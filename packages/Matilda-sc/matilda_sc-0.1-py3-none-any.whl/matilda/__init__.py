02c1b60ec82cdfa3de5fe0f98d35e3f68f219489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:25 +0000

    Add package Matilda-sc/0.1/matilda_sc-0.1-py3-none-any.whl

---
+++ packages/Matilda-sc/matilda_sc-0.1-py3-none-any.whl/matilda/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 06:42:42 2024
+
+@author: FredEvif
+"""
+from . import learn
+from .learn import *
+
+from . import util 
+from . import main_matilda_task
+from . import main_matilda_rna_task
+from . import main_matilda_rna_train 
+from . import main_matilda_train 
+
+# from .learn.predict import *
+# from .learn.model import *
+# from .learn.model_rna import *
+
+
