9ad16158b7ec88f32d8404f041bc8053db799530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:18 +0000

    Add package simple-log-helper/1.4/simple_log_helper-1.4.tar.gz

---
+++ packages/simple-log-helper/simple_log_helper-1.4.tar.gz/simple_log_helper-1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_log_helper',
+    version='1.4',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
---
+++ packages/simple-log-helper/simple_log_helper-1.4.tar.gz/simple_log_helper-1.4/simple_log_helper/__init__.py
@@ -0,0 +1,1 @@
+from .simplerlogger import CustomLogger
