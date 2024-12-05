65ce71817023c9cdbf00e907e143880a7c9219e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:51 +0000

    Add package navground/0.3.0/navground-0.3.0-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/navground/navground-0.3.0-cp312-cp312-manylinux_2_31_x86_64.whl/navground/sim/main.py
@@ -0,0 +1,42 @@
+import argparse
+
+from . import (echo, info, list_plugins, print_schema, record_video, replay,
+               run, run_rt, sample, validate)
+
+
+def config_parser(parsers, name, module):
+    parser = parsers.add_parser(name, help=module.description())
+    module.init_parser(parser)
+    parser.set_defaults(func=module._main)
+
+
+def init_parser(parser: argparse.ArgumentParser) -> None:
+    parsers = parser.add_subparsers(dest='cmd',
+                                    title='Subcommands',
+                                    metavar="{info,run,...}")
+    config_parser(parsers, 'info', info)
+    config_parser(parsers, 'echo', echo)
+    config_parser(parsers, 'schema', print_schema)
+    config_parser(parsers, 'validate', validate)
+    config_parser(parsers, 'plugins', list_plugins)
+    config_parser(parsers, 'run', run)
+    config_parser(parsers, 'run_rt', run_rt)
+    config_parser(parsers, 'sample', sample)
+    config_parser(parsers, 'record_video', record_video)
+    config_parser(parsers, 'replay', replay)
+
+
+def parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog='navground_py')
+    init_parser(parser)
+    return parser
+
+
+def main():
+    ps = parser()
+    args = ps.parse_args()
+    if args.cmd is not None:
+        args.func(args)
+    else:
+        print("Welcome to navground!\n")
+        ps.print_help()
---
+++ packages/navground/navground-0.3.0-cp312-cp312-manylinux_2_31_x86_64.whl/navground/sim/state_estimations/__init__.py
@@ -0,0 +1,5 @@
+from .._navground_sim import (BoundarySensor, BoundedStateEstimation,
+                              SensorCombination, DiscsStateEstimation,
+                              LidarStateEstimation, Sensor,
+                              OdometryStateEstimation)
+from .lidar import PyLidarStateEstimation
