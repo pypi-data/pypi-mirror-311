4cd24ee02864c0752b6325f12794f508cf329816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:03 +0000

    Add package novokshanovLatexGen/0.2.0/novokshanovLatexGen-0.2.0.tar.gz

---
+++ packages/novokshanovLatexGen/novokshanovLatexGen-0.2.0.tar.gz/novokshanovLatexGen-0.2.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='novokshanovLatexGen',
+    version='0.2.0',
+    description='Library to generate LaTeX code for tables and images.',
+    author='novokshanov.e',
+    author_email='novokshanov.eugene@yandex.ru',
+    packages=find_packages(),
+    install_requires=[],
+)
