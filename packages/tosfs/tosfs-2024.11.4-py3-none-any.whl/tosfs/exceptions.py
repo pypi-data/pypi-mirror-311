a2e7e13d1cec770734c72ce93e5a6a7d8c778b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:16 +0000

    Add package tosfs/2024.11.4/tosfs-2024.11.4-py3-none-any.whl

---
+++ packages/tosfs/tosfs-2024.11.4-py3-none-any.whl/tosfs/exceptions.py
@@ -0,0 +1,31 @@
+# ByteDance Volcengine EMR, Copyright 2024.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""It contains exceptions definition for the tosfs package."""
+
+
+class TosfsError(Exception):
+    """Base class for all tosfs exceptions."""
+
+    def __init__(self, message: str):
+        """Initialize the base class for all exceptions in the tosfs package."""
+        super().__init__(message)
+
+
+class TosfsCertificationError(TosfsError):
+    """Exception class for certification related exception."""
+
+    def __init__(self, message: str):
+        """Initialize the exception class for certification related exception."""
+        super().__init__(message)
