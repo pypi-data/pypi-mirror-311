5fd332ec58addad500f4bcfb77537171bef35ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:26 +0000

    Add package pinpointPy/1.4.1/pinpointPy-1.4.1-cp312-cp312-win_amd64.whl

---
+++ packages/pinpointPy/pinpointPy-1.4.1-cp312-cp312-win_amd64.whl/pinpointPy/__init__.py
@@ -0,0 +1,47 @@
+# ******************************************************************************
+#   Copyright  2020. NAVER Corp.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ******************************************************************************
+
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by eeliu at 2/4/21
+
+from pinpointPy.libs import monkey_patch_for_pinpoint
+from pinpointPy.pinpoint import set_agent, app_id, app_name, gen_tid, get_logger
+from pinpointPy.TraceContext import set_trace_context, thread_local_context, asyncio_local_context
+from pinpointPy.Common import PinTransaction, GenPinHeader, PinHeader, enable_experiment_plugins
+
+
+def use_thread_local_context():
+    get_logger().debug("use_thread_local_context")
+    set_trace_context(thread_local_context())
+
+
+def use_asyncio_local_context():
+    get_logger().debug("use_asyncio_local_context")
+    set_trace_context(asyncio_local_context())
+
+
+__all__ = ['monkey_patch_for_pinpoint', 'use_thread_local_context', 'use_asyncio_local_context',
+           'set_agent', 'app_id', 'app_name', 'gen_tid', 'get_logger', 'PinTransaction', 'GenPinHeader', 'PinHeader', 'enable_experiment_plugins']
+__version__ = "1.4.1"
+__author__ = 'liu.mingyi@navercorp.com'
+
+
+# Changes
+# 1.4.1 11-19-2024
+# - add celery plugins and testcase
+# 1.4.0
+# - use_asyncio_local_context
