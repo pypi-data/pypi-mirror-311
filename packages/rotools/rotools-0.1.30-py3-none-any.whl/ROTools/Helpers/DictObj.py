b8094351102eb8ca219412bcfb9d0dcd3f06a211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:26 +0000

    Add package rotools/0.1.30/rotools-0.1.30-py3-none-any.whl

---
+++ packages/rotools/rotools-0.1.30-py3-none-any.whl/ROTools/Helpers/DictObj.py
@@ -0,0 +1,36 @@
+import copy
+
+from ROTools.Helpers.DumpBase import DumpBase
+
+class DictObj(DumpBase):
+    def __init__(self, d=None):
+        if isinstance(d, DictObj):
+            for k, v in d.__dict__.items():
+                self.__dict__[k] = copy.deepcopy(v)
+            return
+
+        if isinstance(d, dict):
+            self._build_dict(d)
+            return
+
+        raise Exception("Flow")
+
+    def _build_dict(self, d):
+        for a, b in d.items():
+            if isinstance(b, (list, tuple)):
+                setattr(self, a, [DictObj(x) if isinstance(x, dict) else x for x in b])
+                continue
+            setattr(self, a, DictObj(b) if isinstance(b, dict) else b)
+
+    def get(self, name, default=None):
+        return getattr(self, name, default)
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def clone(self):
+        import copy
+        return copy.deepcopy(self)
