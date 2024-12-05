e0a73a45c7958b700c016e0c2f1fac7bd9551437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:02 +0000

    Add package mindee/4.14.2/mindee-4.14.2-py3-none-any.whl

---
+++ packages/mindee/mindee-4.14.2-py3-none-any.whl/mindee/input/__init__.py
@@ -0,0 +1,11 @@
+from mindee.input.local_response import LocalResponse
+from mindee.input.page_options import PageOptions
+from mindee.input.sources import (
+    Base64Input,
+    BytesInput,
+    FileInput,
+    InputType,
+    LocalInputSource,
+    PathInput,
+    UrlInputSource,
+)
