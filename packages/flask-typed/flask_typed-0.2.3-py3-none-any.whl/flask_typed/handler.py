ee7ceadf90dfd7af47aafa436665b9900f7d1e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:33 +0000

    Add package flask-typed/0.2.3/flask_typed-0.2.3-py3-none-any.whl

---
+++ packages/flask-typed/flask_typed-0.2.3-py3-none-any.whl/flask_typed/handler.py
@@ -0,0 +1,161 @@
+import inspect
+from inspect import isclass
+from typing import Any, get_origin, Annotated, get_args, Type
+
+import openapi_pydantic as openapi
+from flask import request, current_app
+from pydantic import BaseModel
+
+from flask_typed.docs.responses import ResponsesDocsBuilder
+from flask_typed.docs.utils import Docstring
+from .errors import HttpError
+from .parameter import ParameterLocation, Parameter, ParameterValidationError, ValidationError
+from .parsers import RequestParser
+from .response import BaseResponse
+
+
+class HttpHandler:
+
+    def __init__(self, path, resource_cls, handler):
+        self.resource_cls = resource_cls
+        self.path = path
+        self.handler = handler
+        docstring = getattr(handler, "__doc__", None)
+        self.docstring = Docstring(docstring) if docstring else None
+        self.docs_metadata = getattr(handler, "docs_metadata", None)
+        self.responses = None
+        self.parameters: list[Parameter] = []
+        self.request_parsers: dict[str, Type[RequestParser]] = {}
+
+        self._process_annotations()
+
+    def _process_annotations(self):
+        handler_signature = inspect.signature(self.handler)
+        for parameter in handler_signature.parameters.values():
+            if parameter.name == "self":
+                continue
+
+            source_name = parameter.name
+            param_type = parameter.annotation
+            if param_type is None:
+                raise TypeError(f"No type annotation is provided for parameter: {parameter.name}")
+
+            if isclass(param_type) and issubclass(param_type, RequestParser):
+                self.request_parsers[source_name] = param_type
+                continue
+
+            if get_origin(param_type) == Annotated:
+                metadata = get_args(param_type)
+                param_type = metadata[0]
+                location = metadata[1]
+
+                if location == ParameterLocation.HEADER:
+                    source_name = "-".join(
+                        s.capitalize() for s in parameter.name.split("_")
+                    )
+                if len(metadata) > 2:
+                    source_name = metadata[2]
+
+            elif isclass(param_type) and issubclass(param_type, BaseModel):
+                location = ParameterLocation.BODY
+            elif parameter.name in self.path.path_parameters:
+                location = ParameterLocation.PATH
+            else:
+                location = ParameterLocation.QUERY
+
+            match parameter.default:
+                case parameter.empty:
+                    default_value = ...
+                case other:
+                    default_value = other
+
+            parameter = Parameter(
+                name=parameter.name,
+                source=source_name,
+                location=location,
+                param_type=param_type,
+                description=self._get_parameter_description(parameter.name),
+                default_value=default_value
+            )
+
+            self.parameters.append(parameter)
+
+        self.responses = ResponsesDocsBuilder(
+            return_type=handler_signature.return_annotation,
+            docstring=self.docstring,
+            docs=self.docs_metadata
+        ).build()
+
+    def generate_operation(self) -> openapi.Operation:
+        doc_parameters = []
+        request_body = None
+        for param in self.parameters:
+            match param.location:
+                case ParameterLocation.QUERY | ParameterLocation.PATH | ParameterLocation.HEADER:
+                    doc_parameters.extend(param.to_openapi_parameters())
+                case ParameterLocation.BODY:
+                    request_body = param.to_openapi_request_body()
+
+        return openapi.Operation(
+            parameters=doc_parameters,
+            responses=self.responses,
+            requestBody=request_body,
+            summary=self.docstring.short_description if self.docstring else "",
+            description=self.docstring.long_description if self.docstring else "",
+        )
+
+    def get_handler(self):
+        parameters = self.parameters
+        parsers = self.request_parsers
+        handler = self.handler
+        resource_cls = self.resource_cls
+
+        def perform_validation(kwargs) -> dict[str, Any]:
+            validated_args = {}
+            validation_errors = []
+
+            for param in parameters:
+                try:
+                    validated_args[param.name] = param.validate(request, kwargs)
+                except ParameterValidationError as e:
+                    validation_errors.append(e)
+
+            for name, parser in parsers.items():
+                try:
+                    validated_args[name] = parser.parse_request(request)
+                except ParameterValidationError as e:
+                    validation_errors.append(e)
+
+            if validation_errors:
+                raise ValidationError(errors=[
+                    err.to_model() for err in validation_errors
+                ])
+
+            return validated_args
+
+        def validated(*_args, **kwargs):
+            try:
+                validated_args = perform_validation(kwargs)
+            except HttpError as e:
+                return e.flask_response()
+
+            try:
+                response_value = handler(resource_cls(), **validated_args)
+            except HttpError as e:
+                return e.flask_response()
+
+            if isinstance(response_value, BaseResponse):
+                return response_value.flask_response()
+            if isinstance(response_value, BaseModel):
+                return current_app.response_class(
+                    response=response_value.model_dump_json(by_alias=True),
+                    status=response_value.model_config.get("status_code", 200),
+                    mimetype='application/json',
+                )
+            else:
+                return response_value
+
+        return validated
+
+    def _get_parameter_description(self, param_name) -> str:
+        return "" if self.docstring is None else self.docstring.get_parameter_description(param_name)
---
+++ packages/flask-typed/flask_typed-0.2.3-py3-none-any.whl/flask_typed/parameter.py
@@ -0,0 +1,201 @@
+import json
+from enum import IntEnum
+from inspect import isclass
+from types import UnionType, NoneType
+from typing import Type, Any, get_origin, get_args, Sequence
+
+import openapi_pydantic as openapi
+import pydantic
+from openapi_pydantic.util import PydanticSchema
+from pydantic import BaseModel
+
+from flask_typed.docs.utils import get_builtin_type
+from .errors import HttpError
+from .parsers import QueryParser, HeaderParser
+from .validators import VALIDATORS
+
+
+class ParameterLocation(IntEnum):
+    PATH = 1
+    QUERY = 2
+    BODY = 3
+    HEADER = 4
+
+
+class Parameter:
+
+    def __init__(
+            self,
+            name: str,
+            source: str,
+            location: ParameterLocation,
+            param_type: Type,
+            description: str,
+            default_value: Any,
+    ):
+        self.name = name
+        self.source = source
+        self.location = location
+        self.description = description
+        self.is_optional = False
+        self.type = param_type
+        self.default_value = default_value
+
+        self._init_types(param_type)
+        self._init_data_getter()
+        self._init_validator(self.type)
+
+    def _init_types(self, param_type: Type):
+        actual_type = None
+        origin_type = get_origin(param_type)
+        if origin_type is UnionType:
+            for alternate_type in get_args(param_type):
+                if alternate_type is NoneType:
+                    continue
+                if actual_type is not None:
+                    raise Exception("Multiple argument types are provided")
+                actual_type = alternate_type
+        else:
+            actual_type = param_type
+
+        self.type = actual_type
+
+        if self.default_value is Ellipsis:
+            self.is_optional = False
+        else:
+            self.is_optional = True
+
+    def _init_data_getter(self):
+        match self.location:
+            case ParameterLocation.QUERY:
+                def get_query_param(request, _path_params):
+                    return request.args.get(self.source)
+                self.get_data = get_query_param
+            case ParameterLocation.HEADER:
+                def get_header_param(request, _path_params):
+                    return request.headers.get(self.source)
+                self.get_data = get_header_param
+            case ParameterLocation.PATH:
+                def get_path_param(_request, path_params):
+                    return path_params.get(self.source)
+                self.get_data = get_path_param
+            case ParameterLocation.BODY:
+                def get_body_param(request, _path_params):
+                    return request.data
+                self.get_data = get_body_param
+            case _:
+                raise ValueError(f"Invalid parameter location: {self.location}")
+
+    def _init_validator(self, param_type):
+        if issubclass(param_type, BaseModel):
+            def model_validator(value):
+                return param_type.model_validate(
+                    json.loads(value)
+                )
+            self.validator = model_validator
+        elif validator := VALIDATORS.get(param_type):
+            self.validator = validator
+        else:
+            self.validator = param_type
+
+    def validate(self, request, path_params):
+        value = self.get_data(request, path_params)
+        if value is None:
+            if self.is_optional is True:
+                return self.default_value
+            else:
+                raise ParameterValidationError(self, errors=["Parameter is not optional"])
+        try:
+            return self.validator(value)
+        except pydantic.ValidationError as e:
+            raise ParameterValidationError(self, errors=[e])
+        except Exception as e:
+            raise ParameterValidationError(self, errors=[str(e)])
+
+    def to_openapi_parameters(self) -> list[openapi.Parameter]:
+        location = self.location.name.lower()
+        parameters = []
+        if isclass(self.type) and issubclass(self.type, BaseModel):
+            query_schema = self.type.schema_json()
+            for name, prop in query_schema["properties"].items():
+                parameters.append(
+                    openapi.Parameter(
+                        name=name,
+                        param_in=location,
+                        param_schema=openapi.Schema.model_validate(prop)
+                    )
+                )
+        elif isclass(self.type) and issubclass(self.type, (QueryParser, HeaderParser)):
+            parameters.extend(self.type.schema())
+        else:
+            schema = get_builtin_type(self.type)
+            if schema is None:
+                raise TypeError(f"Unsupported type for parameter '{self.name}': {self.type}")
+
+            if self.default_value is not Ellipsis:
+                schema.default = self.default_value
+            parameters.append(
+                openapi.Parameter(
+                    name=self.source,
+                    description=self.description,
+                    param_in=location,
+                    param_schema=schema,
+                    required=not self.is_optional
+                )
+            )
+        return parameters
+
+    def to_openapi_request_body(self) -> openapi.RequestBody:
+        if isclass(self.type) and issubclass(self.type, BaseModel):
+            schema = PydanticSchema(schema_class=self.type)
+        else:
+            schema = get_builtin_type(self.type)
+            if schema is None:
+                raise TypeError(f"Unsupported type for parameter '{self.name}': {self.type}")
+
+        return openapi.RequestBody(
+            content={
+                "application/json": openapi.MediaType(
+                    schema=schema
+                )
+            }
+        )
+
+
+def repr_pydantic_validation_error(err: pydantic.ValidationError) -> Sequence[str]:
+    for error in err.errors():
+        yield f"{error['msg']}: {'.'.join(str(loc) for loc in error['loc'])}"
+
+
+class ParameterValidationErrorModel(BaseModel):
+    parameter: str
+    location: str
+    details: list[str]
+
+
+class ParameterValidationError(Exception):
+
+    def __init__(self, parameter: Parameter, errors: list[str | pydantic.ValidationError]):
+        self.parameter = parameter
+        self.errors = errors
+
+    def to_model(self):
+        details = []
+        for error in self.errors:
+            if isinstance(error, pydantic.ValidationError):
+                details.extend(repr_pydantic_validation_error(error))
+            else:
+                details.append(error)
+
+        return ParameterValidationErrorModel(
+            parameter=self.parameter.source,
+            location=self.parameter.location.name.lower(),
+            details=details
+        )
+
+
+class ValidationError(HttpError):
+    status_code = 422
+
+    class ResponseModel(BaseModel):
+        errors: list[ParameterValidationErrorModel]
