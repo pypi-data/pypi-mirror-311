3f255d87781a26d4bb94f257ae16beb1302d5111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:25 +0000

    Add package maxim-py/2.0.0/maxim_py-2.0.0.tar.gz

---
+++ packages/maxim-py/maxim_py-2.0.0.tar.gz/maxim_py-2.0.0/maxim/cache/cache.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+
+class MaximCache():
+    def getAllKeys(self) -> List[str]:
+        return []
+
+    def get(self, key: str) -> Optional[str]:
+        pass
+
+    def set(self, key: str, value: str) -> None:
+        pass
+
+    def delete(self, key: str) -> None:
+        pass
