a7629904eb38d65ba9a0f01cff021d0ba17e967a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:17 +0000

    Add package remdex/0.31/remdex-0.31.tar.gz

---
+++ packages/remdex/remdex-0.31.tar.gz/remdex-0.31/setup.py
@@ -0,0 +1,56 @@
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='remdex',
+    version='0.31',
+    description='Fast and lightweight federated learning framework',
+    packages=find_packages(), 
+    install_requires=[
+        'cloudpickle',
+        'numpy'
+    ],
+    license='MIT'
+)
+
+'''
+
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import os
+
+# Gather all .pyx files in the package
+def find_pyx_files(package_dir):
+    pyx_files = []
+    for root, _, files in os.walk(package_dir):
+        for file in files:
+            if file.endswith('.pyx'):
+                pyx_files.append(os.path.join(root, file))
+    return pyx_files
+
+package_name = "remdex"  # Replace with your package's name
+package_dir = os.path.abspath(package_name)  # Set to the directory containing the package
+
+pyx_files = find_pyx_files(package_dir)
+
+# Convert file paths to Python module names
+extensions = []
+for pyx_file in pyx_files:
+    # Get the module name in dot notation relative to the package root
+    module_name = os.path.relpath(pyx_file, os.path.dirname(package_dir)).replace(os.sep, ".")[:-4]
+    extensions.append(Extension(module_name, [pyx_file]))
+
+setup(
+    name=package_name,
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={"language_level": "3"},  # Use Python 3 syntax
+    ),
+    packages=find_packages(),
+)
+'''
+
+
+
