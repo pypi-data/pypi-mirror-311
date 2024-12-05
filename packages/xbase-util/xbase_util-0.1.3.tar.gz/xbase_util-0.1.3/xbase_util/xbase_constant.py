ea349c18a84e9780c821d881fbe36243385a648c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:20 +0000

    Add package xbase-util/0.1.3/xbase_util-0.1.3.tar.gz

---
+++ packages/xbase-util/xbase_util-0.1.3.tar.gz/xbase_util-0.1.3/xbase_util/xbase_constant.py
@@ -0,0 +1,5 @@
+import os
+
+current_dir = os.path.dirname(__file__)
+parse_path = os.path.join(current_dir, '..', 'xbase_util_assets', 'arkimeparse.js')
+geo_path = os.path.join(current_dir, '..', 'xbase_util_assets', 'GeoLite2-City.mmdb')
