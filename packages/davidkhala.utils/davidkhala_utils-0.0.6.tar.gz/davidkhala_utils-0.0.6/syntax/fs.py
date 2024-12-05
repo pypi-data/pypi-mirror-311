ac9e597a80318344b176926ff83fa6261afa5a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:44 +0000

    Add package davidkhala.utils/0.0.6/davidkhala_utils-0.0.6.tar.gz

---
+++ packages/davidkhala.utils/davidkhala_utils-0.0.6.tar.gz/davidkhala_utils-0.0.6/syntax/fs.py
@@ -0,0 +1,33 @@
+import os
+
+from syntax.format import Serializable, JSONReadable
+
+
+def read(path: str) -> str:
+    with open(path, encoding="utf-8") as file:
+        return file.read()
+
+
+def write(path: str, data):
+    with open(path, mode='w') as file:
+        return file.write(data)
+
+
+def write_json(data, name=None):
+    if not name:
+        name = data.__str__()
+    if isinstance(data, Serializable):
+        data = data.as_dict()
+    write(f"{name}.json", JSONReadable(data))
+
+
+def append(path: str, data):
+    with open(path, mode='a') as file:
+        return file.write(data)
+
+
+from pathlib import Path
+
+
+def resolve(*path_tokens):
+    return Path(os.path.join(*path_tokens)).__str__()
