c517a269e1532368e016d317568463c09dcc657c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:37 +0000

    Add package maya-rabbitmq-package/0.1.0/maya_rabbitmq_package-0.1.0.tar.gz

---
+++ packages/maya-rabbitmq-package/maya_rabbitmq_package-0.1.0.tar.gz/maya_rabbitmq_package-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maya_rabbitmq_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pika',
+    ],
+    author='Asifaa Sulthana',
+    author_email='asifaa_sulthanan@trimble.com',
+    description='A package for handling RabbitMQ operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Trimble-Construction/project-maya/tree/dev/Cloud/RabbitmqHandler',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
