19e113882177ea894943ff4433eed0f0f44de26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:45 +0000

    Add package Cocotst/0.0.3/cocotst-0.0.3-py3-none-any.whl

---
+++ packages/Cocotst/cocotst-0.0.3-py3-none-any.whl/cocotst/all.py
@@ -0,0 +1,4 @@
+# This file is used to import all the classes and functions from the cocotst package
+from cocotst.message.element import File, Image, Video, Voice
+from cocotst.network.model import (Author, Content, FileServerConfig, Group,
+                                   Member, Target, WebHookConfig)
