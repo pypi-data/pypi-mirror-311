8f99214cb8d152517b54390e0fa1065461947352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:26 +0000

    Add package api-x-trace-logger/0.0.1/api_x_trace_logger-0.0.1.tar.gz

---
+++ packages/api-x-trace-logger/api_x_trace_logger-0.0.1.tar.gz/api_x_trace_logger-0.0.1/README.md
@@ -0,0 +1,13 @@
+# trace-logger
+
+install all dependent packages
+`pip install -r requirements.txt`
+
+Install build tools:
+`pip install build twine`
+
+Build the package:
+`python -m build`
+
+Clean Previous Builds
+`rm -rf dist build *.egg-info`
---
+++ packages/api-x-trace-logger/api_x_trace_logger-0.0.1.tar.gz/api_x_trace_logger-0.0.1/trace_logger/utils.py
@@ -0,0 +1,13 @@
+from contextvars import ContextVar
+from uuid import UUID
+
+trace_id_var: ContextVar[str] = ContextVar("trace_id")
+
+def set_trace_id(trace_id: str):
+    trace_id_var.set(trace_id)
+
+def get_trace_id() -> str:
+    try:
+        return trace_id_var.get()
+    except LookupError:
+        return "unknown"
