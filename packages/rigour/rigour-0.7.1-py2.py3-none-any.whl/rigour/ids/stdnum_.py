7c4c026beb645d7338c353c03bef7b1a7c30f069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:26 +0000

    Add package rigour/0.7.1/rigour-0.7.1-py2.py3-none-any.whl

---
+++ packages/rigour/rigour-0.7.1-py2.py3-none-any.whl/rigour/ids/stdnum_.py
@@ -0,0 +1,108 @@
+from typing import Optional
+from stdnum import imo, isin, iban, figi, bic, lei  # type: ignore
+from stdnum.ru import inn  # type: ignore
+from stdnum.br import cpf, cnpj  # type: ignore
+
+from rigour.ids.common import StdnumFormat
+
+
+class IMO(StdnumFormat):
+    """An IMO number for a ship."""
+
+    TITLE = "IMO"
+
+    impl = imo
+
+
+class ISIN(StdnumFormat):
+    """An ISIN number for a security."""
+
+    TITLE = "ISIN"
+
+    impl = isin
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return value.upper()
+
+
+class IBAN(StdnumFormat):
+    """An IBAN number for a bank account."""
+
+    TITLE = "IBAN"
+
+    impl = iban
+
+
+class FIGI(StdnumFormat):
+    """A FIGI number for a security, as managed by OpenFIGI."""
+
+    TITLE = "FIGI"
+
+    impl = figi
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return value.upper()
+
+
+class BIC(StdnumFormat):
+    """BIC (ISO 9362 Business identifier codes)."""
+
+    TITLE = "BIC"
+
+    impl = bic
+
+    @classmethod
+    def normalize(cls, value: str) -> Optional[str]:
+        norm = super().normalize(value)
+        if norm is not None:
+            norm = norm[:8]
+        return norm
+
+
+class INN(StdnumFormat):
+    """Russian tax identification number."""
+
+    TITLE = "INN"
+
+    impl = inn
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return value
+
+
+class LEI(StdnumFormat):
+    """Legal Entity Identifier (ISO 17442)"""
+
+    TITLE = "LEI"
+
+    impl = lei
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return value.upper()
+
+
+class CPF(StdnumFormat):
+    """Cadastro de Pessoas Físicas, Brazilian national identifier"""
+
+    TITLE = "CPF"
+
+    impl = cpf
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return str(cpf.format(value))
+
+class CNPJ(StdnumFormat):
+    """Cadastro Nacional de Pessoas Jurídicas, Brazilian national companies identifier"""
+
+    TITLE = "CNPJ"
+
+    impl = cnpj
+
+    @classmethod
+    def format(cls, value: str) -> str:
+        return str(cnpj.format(value))
