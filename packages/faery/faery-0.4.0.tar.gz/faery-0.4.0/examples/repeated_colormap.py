58cf9de4157fd18dfa21b5418d709cf8fad77eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:56 +0000

    Add package faery/0.4.0/faery-0.4.0.tar.gz

---
+++ packages/faery/faery-0.4.0.tar.gz/faery-0.4.0/examples/repeated_colormap.py
@@ -0,0 +1,26 @@
+import faery
+
+colormap = faery.colormaps.batlow.repeated(count=10, flip_odd_indices=True)
+colormap.name = "batlow repeated"
+colormap.to_file(
+    faery.dirname.parent / "tests" / "data_generated" / "repeated_colormap.png"
+)
+
+(
+    faery.events_stream_from_file(
+        faery.dirname.parent / "tests" / "data" / "dvs.es",
+    )
+    .regularize(frequency_hz=60.0)
+    .envelope(
+        decay="linear",
+        tau="00:00:00.100000",
+    )
+    .colorize(colormap=colormap)
+    .to_files(
+        faery.dirname.parent
+        / "tests"
+        / "data_generated"
+        / "dvs_frames_repeated_colormap"
+        / "{index:04}.png"
+    )
+)
