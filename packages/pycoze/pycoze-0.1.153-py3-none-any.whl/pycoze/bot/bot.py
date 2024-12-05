6b64e89800ec4fcae877fee612fd20b8a7867393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:59 +0000

    Add package pycoze/0.1.153/pycoze-0.1.153-py3-none-any.whl

---
+++ packages/pycoze/pycoze-0.1.153-py3-none-any.whl/pycoze/bot/bot.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import HumanMessage
+from .agent import INPUT_MESSAGE, output
+from .agent_chat import agent_chat
+import json
+import queue
+import threading
+
+
+def chat(bot_setting_file: str):
+    history = []
+    while True:
+        input_text = input()
+        if not input_text.startswith(INPUT_MESSAGE):
+            raise ValueError("Invalid message")
+        message = json.loads(input_text[len(INPUT_MESSAGE) :])
+        history.append(HumanMessage(message["content"]))
+        result_queue = queue.Queue()
+        agent_chat_thread = threading.Thread(target=agent_chat, args=(bot_setting_file, history, result_queue))
+
+        result = agent_chat(bot_setting_file, history)
+        history.append(output("assistant", result))
+
+
+
+def get_chat_response(bot_setting_file: str, input_text: str):
+    history = [HumanMessage(input_text)]
+    result_queue = queue.Queue()
+    result = agent_chat(bot_setting_file, history, result_queue)
+    return result
