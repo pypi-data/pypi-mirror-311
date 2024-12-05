2ac016cc57d5dc1d7fbc23de2b888c92a8d6f7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:39 +0000

    Add package djpress/0.13.3/djpress-0.13.3.tar.gz

---
+++ packages/djpress/djpress-0.13.3.tar.gz/djpress-0.13.3/example/config/urls.py
@@ -0,0 +1,10 @@
+"""URL configuration for config project."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("", include("djpress.urls")),
+]
