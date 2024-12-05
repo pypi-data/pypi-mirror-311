003a3b0bf6b3ec0332c14ad3fcb83c04778c7ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:12 +0000

    Add package GscComponent/0.1.0/gsccomponent-0.1.0.tar.gz

---
+++ packages/GscComponent/gsccomponent-0.1.0.tar.gz/gsccomponent-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GscComponent',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author='Gustave',
+    description='the component for GSC',
+    url='https://https://github.com/GustaveGSC/GscComponent',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
