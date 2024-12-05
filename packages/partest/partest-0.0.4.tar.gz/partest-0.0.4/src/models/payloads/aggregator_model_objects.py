f674f491a0741c7b92b961d18eb6dcda155e6133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:04 +0000

    Add package partest/0.0.4/partest-0.0.4.tar.gz

---
+++ packages/partest/partest-0.0.4.tar.gz/partest-0.0.4/src/models/payloads/aggregator_model_objects.py
@@ -0,0 +1,5 @@
+from src.models.payloads.model_get_article import ValidateGetArticle, ParamsGetArticle
+
+class Models:
+    ValidateGetArticle = ValidateGetArticle
+    ParamsGetArticle = ParamsGetArticle
---
+++ packages/partest/partest-0.0.4.tar.gz/partest-0.0.4/src/models/processings/headers.py
@@ -0,0 +1,13 @@
+
+class RequestLenguage:
+
+    header_en_one = {'Accept-Language': 'en'}
+    header_en = {'Accept-Language': 'en-US'}
+    header_ru_one = {'Accept-Language': 'ru'}
+    header_ru = {'Accept-Language': 'ru-Ru'}
+    header_default = {}
+    header_error = {'Accept-Language': 'zu-Ze'}
+    header_preference_ru = {'Accept-Language': 'ru-RU,ru;q=0.9,en-GB;q=0.8,en;q=0.7,be-BY;q=0.6,be;q=0.5,en-US;q=0.4'}
+    header_preference_en = {'Accept-Language': 'ru-RU,ru;q=0.7,en-GB;q=0.8,en;q=0.9,be-BY;q=0.6,be;q=0.5,en-US;q=0.4'}
+
+
