802dbdaa80897e1a12502a6d79848d52b1588ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:24 +0000

    Add package fastcodedog/2.0.15/fastcodedog-2.0.15.tar.gz

---
+++ packages/fastcodedog/fastcodedog-2.0.15.tar.gz/fastcodedog-2.0.15/fastcodedog/context/setting/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from fastcodedog.context.contextbase import ContextBase
+
+
+class Config(ContextBase):
+    class Database(ContextBase):
+        def __init__(self):
+            super().__init__()
+            self.url = 'sqlite:///fastcodedog.db'
+
+    class Logging(ContextBase):
+        def __init__(self):
+            super().__init__()
+            self.filename = 'fastcodedog.log'
+            self.level = 'INFO'
+            self.encoding = 'utf-8'
+            self.datefmt = '%Y-%m-%d %H:%M:%S'
+            self.format = '%(asctime)s - %(levelname)s - %(message)s - %(filename)s:%(lineno)d'
+
+    def __init__(self):
+        super().__init__()
+        self.database = Config.Database()
+        self.logging = Config.Logging()
+        self.user_configs = {}
+        self._types['user_configs'] = dict
