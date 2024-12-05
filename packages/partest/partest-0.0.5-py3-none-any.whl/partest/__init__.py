2abd9c4447f80f5b3fa6cbd91f9e9b1fe595b70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:31 +0000

    Add package partest/0.0.5/partest-0.0.5-py3-none-any.whl

---
+++ packages/partest/partest-0.0.5-py3-none-any.whl/partest/__init__.py
@@ -0,0 +1,7 @@
+from .coverage import *
+from .api_call_storage import *
+from .client import *
+from .parser import *
+from .test_types import *
+from .allure_graph import *
+from .methods import *
