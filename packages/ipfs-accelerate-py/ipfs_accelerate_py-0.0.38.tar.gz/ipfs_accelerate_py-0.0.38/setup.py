4e9bca854ac4e21071ceafdc56dc3b80bb3d9bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:29 +0000

    Add package ipfs-accelerate-py/0.0.38/ipfs_accelerate_py-0.0.38.tar.gz

---
+++ packages/ipfs-accelerate-py/ipfs_accelerate_py-0.0.38.tar.gz/ipfs_accelerate_py-0.0.38/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ipfs_accelerate_py',
+	version='0.0.38',
+    packages=find_packages(),
+	install_requires=[
+		'ipfs_kit_py',
+		'sentence_transformers'
+        'transformers',
+        'ipfs_transformers_py',
+        'ipfs_model_manager_py',
+		'torch',
+        'torchvision',
+        'numpy',
+        'torchtext',
+		'urllib3',
+		'requests',
+		'boto3',
+		'trio',
+		'InstructorEmbedding',
+		'FlagEmbedding',
+		'llama-cpp-python',
+		'gguf',
+		"optimum",
+		"optimum[openvino]",
+	]
+)
