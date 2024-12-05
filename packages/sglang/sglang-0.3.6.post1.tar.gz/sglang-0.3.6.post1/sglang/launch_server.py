8c8a8f4352a9af83b1adf27dcf003924d3ff4a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:28 +0000

    Add package sglang/0.3.6.post1/sglang-0.3.6.post1.tar.gz

---
+++ packages/sglang/sglang-0.3.6.post1.tar.gz/sglang-0.3.6.post1/sglang/launch_server.py
@@ -0,0 +1,18 @@
+"""Launch the inference server."""
+
+import os
+import sys
+
+from sglang.srt.server import launch_server
+from sglang.srt.server_args import prepare_server_args
+from sglang.srt.utils import kill_child_process
+
+if __name__ == "__main__":
+    server_args = prepare_server_args(sys.argv[1:])
+
+    try:
+        launch_server(server_args)
+    except Exception as e:
+        raise e
+    finally:
+        kill_child_process()
