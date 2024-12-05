eddf613b9183fd2474d624e2725947bff0472a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:54 +0000

    Add package dynamofl/0.0.92/dynamofl-0.0.92.tar.gz

---
+++ packages/dynamofl/dynamofl-0.0.92.tar.gz/dynamofl-0.0.92/dynamofl/entities/billing.py
@@ -0,0 +1,19 @@
+"""DynamoFL Model"""
+import logging
+from dataclasses import dataclass
+from typing import Optional
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class BillingReport:
+    """Data class for billing report"""
+
+    id: int
+    from_time: str
+    to_time: str
+    data_store_path: Optional[str]
+    is_billing_data_corrupted: bool
+    job_id: str
+    status: str
