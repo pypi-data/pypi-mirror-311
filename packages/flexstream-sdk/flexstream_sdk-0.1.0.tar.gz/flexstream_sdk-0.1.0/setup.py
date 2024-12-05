247a8dfbcf5eef5ad77efd805f70e33e6316e2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:27 +0000

    Add package flexstream-sdk/0.1.0/flexstream_sdk-0.1.0.tar.gz

---
+++ packages/flexstream-sdk/flexstream_sdk-0.1.0.tar.gz/flexstream_sdk-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flexstream-sdk',
+    version='0.1.0',
+    description='Modbus SDK for automated control and monitoring of Kin-Tek FlexStream device settings and parameters',
+    author='Nir Dagan',
+    author_email='Nirdagan22@gmail.com',
+    packages=find_packages(),
+)
