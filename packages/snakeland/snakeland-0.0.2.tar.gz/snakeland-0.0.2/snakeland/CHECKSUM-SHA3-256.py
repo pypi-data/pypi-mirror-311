0003992878351dea4ba7c57cf588c50d200a8914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:14 +0000

    Add package snakeland/0.0.2/snakeland-0.0.2.tar.gz

---
+++ packages/snakeland/snakeland-0.0.2.tar.gz/snakeland-0.0.2/snakeland/CHECKSUM-SHA3-256.py
@@ -0,0 +1,5 @@
+'''
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/snakeland_guru/head/public/script/snakeland.py)= c639faa5441a6eaed45e8d3ef01cc13ca52cbf3a05ea103425ced34cd8f4aab6
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/snakeland_guru/head/public/pypi/snakeland/__init__.py)= 1650226c6b5fc33d8565ea1db0dd00759959f1947fa88efee43f4e571789b7fc
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/snakeland_guru/head/public/pypi/snakeland/__main__.py)= dfcb2a704ab590a94193e06f367fbeaa6d1b667d20c31a99e3224a39b92a9b96
+'''
