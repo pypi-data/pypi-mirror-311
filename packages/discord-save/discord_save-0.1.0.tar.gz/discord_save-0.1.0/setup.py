5ff89706b1ac1960b46b6040739df3dcb2d4e81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:01 +0000

    Add package discord-save/0.1.0/discord_save-0.1.0.tar.gz

---
+++ packages/discord-save/discord_save-0.1.0.tar.gz/discord_save-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord_save",
+    version="0.1.0",
+    description="A library for managing save files for Discord bots.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/discord_save",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
