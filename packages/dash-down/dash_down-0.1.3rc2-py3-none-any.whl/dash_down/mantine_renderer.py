c1424dbd1e49716e79e290f91feb23b127e8d3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:01 +0000

    Add package dash-down/0.1.3rc2/dash_down-0.1.3rc2-py3-none-any.whl

---
+++ packages/dash-down/dash_down-0.1.3rc2-py3-none-any.whl/dash_down/mantine_renderer.py
@@ -0,0 +1,42 @@
+import dash_mantine_components as dmc
+
+from dash_down.html_renderer import DashHtmlRenderer
+
+
+class DmcRenderer(DashHtmlRenderer):
+    """
+    Render markdown into Dash Mantine components.
+    """
+
+    def link(self, link, children=None, title=None):
+        return dmc.Anchor(children, href=link)
+
+    def image(self, src, alt="", title=None):
+        # TODO: Should we add a caption?
+        return dmc.Image(src=src, alt=alt)  # , caption=title)
+
+    def heading(self, children, level):
+        return dmc.Title(super().add_header_anchor(children), order=level)
+
+    def thematic_break(self):
+        return dmc.Divider()
+
+    def block_code(self, children, info=None):
+        lang = None
+        if info is not None:
+            info = info.strip()
+        if info:
+            lang = info.split(None, 1)[0]
+        return dmc.CodeHighlight(children, language=lang)
+
+    def block_quote(self, text):
+        return dmc.Blockquote(text)
+
+    def list(self, children, ordered, level, start=None):
+        return dmc.List(children, type="ordered" if ordered else "unordered")
+
+    def list_item(self, text, level):
+        return dmc.ListItem(text)
+
+    def table(self, text):
+        return dmc.Table(text, striped=True, highlightOnHover=True)
