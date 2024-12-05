e2ba6e02ee33fbd38ae5ce873046e3c8036e99b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:56 +0000

    Add package config-encryptor/1.2.1/config_encryptor-1.2.1-py3-none-any.whl

---
+++ packages/config-encryptor/config_encryptor-1.2.1-py3-none-any.whl/config_encryptor/__init__.py
@@ -0,0 +1,14 @@
+# config_encryptor/__init__.py
+
+"""
+Config Encryptor Package
+This package provides encryption and decryption functionalities for config.json files.
+"""
+__author__ = "Ahmad Shabir"
+__copyright__ = "Copyright 2024, , CAIMI."
+
+__license__ = "Private"
+__version__ = "1.2.0001"
+__maintainer__ = "CAIMI"
+__email__ = "shabir@caimi.co.kr"
+__status__ = "version 1.2 인증 진행중"
