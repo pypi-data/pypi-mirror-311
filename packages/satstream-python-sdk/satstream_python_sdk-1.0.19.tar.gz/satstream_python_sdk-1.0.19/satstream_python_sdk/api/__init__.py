5eceb2bd579e4b5b35b7dbe2bfac28aad66f3337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:56 +0000

    Add package satstream-python-sdk/1.0.19/satstream_python_sdk-1.0.19.tar.gz

---
+++ packages/satstream-python-sdk/satstream_python_sdk-1.0.19.tar.gz/satstream_python_sdk-1.0.19/satstream_python_sdk/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from satstream_python_sdk.api.bitcoin_api import BitcoinApi
+from satstream_python_sdk.api.blocks_api import BlocksApi
+from satstream_python_sdk.api.inscriptions_api import InscriptionsApi
