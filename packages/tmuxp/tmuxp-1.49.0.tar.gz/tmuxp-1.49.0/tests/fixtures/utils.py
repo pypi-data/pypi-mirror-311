5d0eb47a301088d0a8c04b9a6696c6a5d66f9ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:20 +0000

    Add package tmuxp/1.49.0/tmuxp-1.49.0.tar.gz

---
+++ packages/tmuxp/tmuxp-1.49.0.tar.gz/tmuxp-1.49.0/tests/fixtures/utils.py
@@ -0,0 +1,37 @@
+"""Utility functions for tmuxp fixtures."""
+
+import pathlib
+import typing as t
+
+from tests.constants import FIXTURE_PATH
+
+
+def get_workspace_file(
+    _file: t.Union[str, pathlib.Path],
+) -> pathlib.Path:
+    """Return fixture data, relative to __file__."""
+    if isinstance(_file, str):
+        _file = pathlib.Path(_file)
+
+    return FIXTURE_PATH / _file
+
+
+def read_workspace_file(
+    _file: t.Union[pathlib.Path, str],
+) -> str:
+    """Return fixture data, relative to __file__."""
+    if isinstance(_file, str):
+        _file = pathlib.Path(_file)
+
+    return get_workspace_file(_file).open().read()
+
+
+def write_config(
+    config_path: pathlib.Path,
+    filename: str,
+    content: str,
+) -> pathlib.Path:
+    """Write configuration content to file."""
+    config = config_path / filename
+    config.write_text(content, encoding="utf-8")
+    return config
