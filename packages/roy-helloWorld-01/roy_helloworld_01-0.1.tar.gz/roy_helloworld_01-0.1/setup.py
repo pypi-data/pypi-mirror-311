5677eeae55918c9bde60ae2dbbc2605b7f5b1e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:46 +0000

    Add package roy-helloWorld-01/0.1/roy_helloworld_01-0.1.tar.gz

---
+++ packages/roy-helloWorld-01/roy_helloworld_01-0.1.tar.gz/roy_helloworld_01-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roy_helloWorld_01',
+    version='0.1',
+    packages=find_packages(),
+    author='ZBJ',
+    author_email="wangkangluo1@gmail.com"
+)
