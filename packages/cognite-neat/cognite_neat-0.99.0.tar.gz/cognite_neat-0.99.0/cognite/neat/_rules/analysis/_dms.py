f917cf896aa4e87ea76ba4c24381460b1f2c431d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:46 +0000

    Add package cognite-neat/0.99.0/cognite_neat-0.99.0.tar.gz

---
+++ packages/cognite-neat/cognite_neat-0.99.0.tar.gz/cognite_neat-0.99.0/cognite/neat/_rules/analysis/_dms.py
@@ -0,0 +1,37 @@
+from cognite.neat._constants import DMS_LISTABLE_PROPERTY_LIMIT
+from cognite.neat._rules.models.dms import DMSProperty, DMSRules, DMSView
+from cognite.neat._rules.models.entities import ViewEntity
+
+from ._base import BaseAnalysis
+
+
+class DMSAnalysis(BaseAnalysis[DMSRules, DMSView, DMSProperty, ViewEntity, str]):
+    """Assumes analysis over only the complete schema"""
+
+    def _get_classes(self) -> list[DMSView]:
+        return list(self.rules.views)
+
+    def _get_properties(self) -> list[DMSProperty]:
+        return list(self.rules.properties)
+
+    def _get_cls_entity(self, class_: DMSView | DMSProperty) -> ViewEntity:
+        return class_.view
+
+    def _get_cls_parents(self, class_: DMSView) -> list[ViewEntity] | None:
+        return list(class_.implements) if class_.implements else None
+
+    @classmethod
+    def _set_cls_entity(cls, property_: DMSProperty, class_: ViewEntity) -> None:
+        property_.view = class_
+
+    def _get_object(self, property_: DMSProperty) -> ViewEntity | None:
+        return property_.value_type if isinstance(property_.value_type, ViewEntity) else None
+
+    def _get_max_occurrence(self, property_: DMSProperty) -> int | float | None:
+        return DMS_LISTABLE_PROPERTY_LIMIT if property_.is_list else 1
+
+    def subset_rules(self, desired_classes: set[ViewEntity]) -> DMSRules:
+        raise NotImplementedError()
+
+    def _get_prop_entity(self, property_: DMSProperty) -> str:
+        return property_.view_property
---
+++ packages/cognite-neat/cognite_neat-0.99.0.tar.gz/cognite_neat-0.99.0/cognite/neat/_rules/models/dms/_rules.py
@@ -0,0 +1,433 @@
+import warnings
+from collections.abc import Hashable
+from typing import Any, ClassVar, Literal
+
+import pandas as pd
+from cognite.client import data_modeling as dm
+from pydantic import Field, field_serializer, field_validator, model_validator
+from pydantic_core.core_schema import SerializationInfo, ValidationInfo
+from rdflib import URIRef
+
+from cognite.neat._client.data_classes.schema import DMSSchema
+from cognite.neat._constants import COGNITE_SPACES
+from cognite.neat._issues import MultiValueError
+from cognite.neat._issues.errors import NeatValueError
+from cognite.neat._issues.warnings import (
+    PrincipleMatchingSpaceAndVersionWarning,
+)
+from cognite.neat._rules.models._base_rules import (
+    BaseMetadata,
+    BaseRules,
+    ContainerProperty,
+    DataModelAspect,
+    RoleTypes,
+    SheetList,
+    SheetRow,
+    ViewProperty,
+    ViewRef,
+)
+from cognite.neat._rules.models._types import (
+    ClassEntityType,
+    ContainerEntityType,
+    DmsPropertyType,
+    StrListType,
+    ViewEntityType,
+)
+from cognite.neat._rules.models.data_types import DataType
+from cognite.neat._rules.models.entities import (
+    ContainerEntityList,
+    DMSEntity,
+    DMSNodeEntity,
+    DMSUnknownEntity,
+    EdgeEntity,
+    Entity,
+    HasDataFilter,
+    NodeTypeFilter,
+    RawFilter,
+    ReverseConnectionEntity,
+    ViewEntity,
+    ViewEntityList,
+)
+
+_DEFAULT_VERSION = "1"
+
+
+class DMSMetadata(BaseMetadata):
+    role: ClassVar[RoleTypes] = RoleTypes.dms
+    aspect: ClassVar[DataModelAspect] = DataModelAspect.physical
+    logical: str | None = None
+
+    def as_space(self) -> dm.SpaceApply:
+        return dm.SpaceApply(
+            space=self.space,
+        )
+
+    def as_data_model_id(self) -> dm.DataModelId:
+        return dm.DataModelId(space=self.space, external_id=self.external_id, version=self.version)
+
+    def as_data_model(self) -> dm.DataModelApply:
+        suffix = f"Creator: {', '.join(self.creator)}"
+        if self.description:
+            description = f"{self.description} Creator: {', '.join(self.creator)}"
+        else:
+            description = suffix
+
+        return dm.DataModelApply(
+            space=self.space,
+            external_id=self.external_id,
+            name=self.name or None,
+            version=self.version or "missing",
+            description=description,
+            views=[],
+        )
+
+    def as_identifier(self) -> str:
+        return repr(self.as_data_model_id())
+
+    def get_prefix(self) -> str:
+        return self.space
+
+
+def _metadata(context: Any) -> DMSMetadata | None:
+    if isinstance(context, dict) and isinstance(context.get("metadata"), DMSMetadata):
+        return context["metadata"]
+    return None
+
+
+class DMSProperty(SheetRow):
+    view: ViewEntityType = Field(alias="View")
+    view_property: DmsPropertyType = Field(alias="View Property")
+    name: str | None = Field(alias="Name", default=None)
+    description: str | None = Field(alias="Description", default=None)
+    connection: Literal["direct"] | ReverseConnectionEntity | EdgeEntity | None = Field(None, alias="Connection")
+    value_type: DataType | ViewEntity | DMSUnknownEntity = Field(alias="Value Type")
+    nullable: bool | None = Field(default=None, alias="Nullable")
+    immutable: bool | None = Field(default=None, alias="Immutable")
+    is_list: bool | None = Field(default=None, alias="Is List")
+    default: str | int | dict | None = Field(None, alias="Default")
+    container: ContainerEntityType | None = Field(None, alias="Container")
+    container_property: DmsPropertyType | None = Field(None, alias="Container Property")
+    index: StrListType | None = Field(None, alias="Index")
+    constraint: StrListType | None = Field(None, alias="Constraint")
+    logical: URIRef | None = Field(
+        None,
+        alias="Logical",
+        description="Used to make connection between physical and logical data model aspect",
+    )
+
+    def _identifier(self) -> tuple[Hashable, ...]:
+        return self.view, self.view_property
+
+    @field_validator("nullable")
+    def direct_relation_must_be_nullable(cls, value: Any, info: ValidationInfo) -> None:
+        if info.data.get("connection") == "direct" and value is False:
+            raise ValueError("Direct relation must be nullable")
+        return value
+
+    @field_validator("value_type", mode="after")
+    def connections_value_type(
+        cls, value: EdgeEntity | ViewEntity | DMSUnknownEntity, info: ValidationInfo
+    ) -> DataType | EdgeEntity | ViewEntity | DMSUnknownEntity:
+        if (connection := info.data.get("connection")) is None:
+            return value
+        if connection == "direct" and not isinstance(value, ViewEntity | DMSUnknownEntity):
+            raise ValueError(f"Direct relation must have a value type that points to a view, got {value}")
+        elif isinstance(connection, EdgeEntity) and not isinstance(value, ViewEntity):
+            raise ValueError(f"Edge connection must have a value type that points to a view, got {value}")
+        elif isinstance(connection, ReverseConnectionEntity) and not isinstance(value, ViewEntity):
+            raise ValueError(f"Reverse connection must have a value type that points to a view, got {value}")
+        return value
+
+    @field_validator("container", "container_property", mode="after")
+    def container_set_correctly(cls, value: Any, info: ValidationInfo) -> Any:
+        if (connection := info.data.get("connection")) is None:
+            return value
+        if connection == "direct" and value is None:
+            raise ValueError(
+                "You must provide a container and container property for where to store direct connections"
+            )
+        elif isinstance(connection, EdgeEntity) and value is not None:
+            raise ValueError(
+                "Edge connections are not stored in a container, please remove the container and container property"
+            )
+        elif isinstance(connection, ReverseConnectionEntity) and value is not None:
+            raise ValueError(
+                "Reverse connection are not stored in a container, please remove the container and container property"
+            )
+        return value
+
+    @field_serializer("value_type", when_used="always")
+    def as_dms_type(self, value_type: DataType | EdgeEntity | ViewEntity, info: SerializationInfo) -> str:
+        if isinstance(value_type, DataType):
+            return value_type._suffix_extra_args(value_type.dms._type)
+        elif isinstance(value_type, EdgeEntity | ViewEntity) and (metadata := _metadata(info.context)):
+            return value_type.dump(space=metadata.space, version=metadata.version)
+        return str(value_type)
+
+    @field_serializer("view", "container", when_used="unless-none")
+    def remove_default_space(self, value: str, info: SerializationInfo) -> str:
+        if (metadata := _metadata(info.context)) and isinstance(value, Entity):
+            if info.field_name == "container" and info.context.get("as_reference") is True:
+                # When dumping as reference, the container should keep the default space for easy copying
+                # over to user sheets.
+                return value.dump()
+            return value.dump(prefix=metadata.space, version=metadata.version)
+        return str(value)
+
+    @field_serializer("connection", when_used="unless-none")
+    def remove_defaults(self, value: Any, info: SerializationInfo) -> str:
+        if isinstance(value, Entity) and (metadata := _metadata(info.context)):
+            default_type = f"{self.view.external_id}.{self.view_property}"
+            if isinstance(value, EdgeEntity) and value.edge_type and value.edge_type.space != metadata.space:
+                default_type = f"{metadata.space}{default_type}"
+            return value.dump(space=metadata.space, version=metadata.version, type=default_type)
+        return str(value)
+
+    def as_container_reference(self) -> ContainerProperty:
+        if self.container is None or self.container_property is None:
+            raise NeatValueError("Accessing container reference without container and container property set")
+        return ContainerProperty(container=self.container, property_=self.container_property)
+
+    def as_view_reference(self) -> ViewProperty:
+        return ViewProperty(view=self.view, property_=self.view_property)
+
+
+class DMSContainer(SheetRow):
+    container: ContainerEntityType = Field(alias="Container")
+    name: str | None = Field(alias="Name", default=None)
+    description: str | None = Field(alias="Description", default=None)
+    constraint: ContainerEntityList | None = Field(None, alias="Constraint")
+    used_for: Literal["node", "edge", "all"] | None = Field("all", alias="Used For")
+
+    def _identifier(self) -> tuple[Hashable, ...]:
+        return (self.container,)
+
+    def as_container(self) -> dm.ContainerApply:
+        container_id = self.container.as_id()
+        constraints: dict[str, dm.Constraint] = {}
+        for constraint in self.constraint or []:
+            requires = dm.RequiresConstraint(constraint.as_id())
+            constraints[f"{constraint.space}_{constraint.external_id}"] = requires
+
+        return dm.ContainerApply(
+            space=container_id.space,
+            external_id=container_id.external_id,
+            name=self.name or None,
+            description=self.description,
+            constraints=constraints or None,
+            properties={},
+            used_for=self.used_for,
+        )
+
+    @field_serializer("container", when_used="unless-none")
+    def remove_default_space(self, value: Any, info: SerializationInfo) -> str:
+        if metadata := _metadata(info.context):
+            if isinstance(value, DMSEntity):
+                return value.dump(space=metadata.space, version=metadata.version)
+            elif isinstance(value, Entity):
+                return value.dump(prefix=metadata.space, version=metadata.version)
+        return str(value)
+
+    @field_serializer("constraint", when_used="unless-none")
+    def remove_default_spaces(self, value: Any, info: SerializationInfo) -> str:
+        if isinstance(value, list) and (metadata := _metadata(info.context)):
+            return ",".join(
+                constraint.dump(space=metadata.space, version=metadata.version)
+                if isinstance(constraint, DMSEntity)
+                else str(constraint)
+                for constraint in value
+            )
+        return ",".join(str(value) for value in value)
+
+
+class DMSView(SheetRow):
+    view: ViewEntityType = Field(alias="View")
+    name: str | None = Field(alias="Name", default=None)
+    description: str | None = Field(alias="Description", default=None)
+    implements: ViewEntityList | None = Field(None, alias="Implements")
+    filter_: HasDataFilter | NodeTypeFilter | RawFilter | None = Field(None, alias="Filter")
+    in_model: bool = Field(True, alias="In Model")
+    logical: URIRef | None = Field(
+        None,
+        alias="Logical",
+        description="Used to make connection between physical and logical data model aspect",
+    )
+
+    def _identifier(self) -> tuple[Hashable, ...]:
+        return (self.view,)
+
+    @field_serializer("view", when_used="unless-none")
+    def remove_default_space(self, value: Any, info: SerializationInfo) -> str:
+        if (metadata := _metadata(info.context)) and isinstance(value, Entity):
+            return value.dump(prefix=metadata.space, version=metadata.version)
+        return str(value)
+
+    @field_serializer("implements", when_used="unless-none")
+    def remove_default_spaces(self, value: Any, info: SerializationInfo) -> str:
+        if isinstance(value, list) and (metadata := _metadata(info.context)):
+            return ",".join(
+                parent.dump(space=metadata.space, version=metadata.version)
+                if isinstance(parent, DMSEntity)
+                else str(parent)
+                for parent in value
+            )
+        return ",".join(str(value) for value in value) if isinstance(value, list) else value
+
+    def as_view(self) -> dm.ViewApply:
+        view_id = self.view.as_id()
+        implements = [parent.as_id() for parent in self.implements or []] or None
+
+        return dm.ViewApply(
+            space=view_id.space,
+            external_id=view_id.external_id,
+            version=view_id.version or _DEFAULT_VERSION,
+            name=self.name or None,
+            description=self.description,
+            filter=None if self.filter_ is None else self.filter_.as_dms_filter(),
+            implements=implements,
+            properties={},
+        )
+
+    def as_view_reference(self) -> ViewRef:
+        return ViewRef(view=self.view)
+
+
+class DMSNode(SheetRow):
+    node: DMSNodeEntity = Field(alias="Node")
+    usage: Literal["type", "collection"] = Field(alias="Usage")
+    name: str | None = Field(alias="Name", default=None)
+    description: str | None = Field(alias="Description", default=None)
+
+    def _identifier(self) -> tuple[Hashable, ...]:
+        return (self.node,)
+
+    def as_node(self) -> dm.NodeApply:
+        if self.usage == "type":
+            return dm.NodeApply(space=self.node.space, external_id=self.node.external_id)
+        elif self.usage == "collection":
+            raise NotImplementedError("Collection nodes are not supported yet")
+        else:
+            raise ValueError(f"Unknown usage {self.usage}")
+
+    @field_serializer("node", when_used="unless-none")
+    def remove_default_space(self, value: Any, info: SerializationInfo) -> str:
+        if isinstance(value, DMSEntity) and (metadata := _metadata(info.context)):
+            return value.dump(space=metadata.space, version=metadata.version)
+        return str(value)
+
+
+class DMSEnum(SheetRow):
+    collection: ClassEntityType = Field(alias="Collection")
+    value: str = Field(alias="Value")
+    name: str | None = Field(alias="Name", default=None)
+    description: str | None = Field(alias="Description", default=None)
+
+    def _identifier(self) -> tuple[Hashable, ...]:
+        return self.collection, self.value
+
+    @field_serializer("collection", when_used="unless-none")
+    def remove_default_space(self, value: Any, info: SerializationInfo) -> str:
+        if isinstance(value, DMSEntity) and (metadata := _metadata(info.context)):
+            return value.dump(space=metadata.space, version=metadata.version)
+        return str(value)
+
+
+class DMSRules(BaseRules):
+    metadata: DMSMetadata = Field(alias="Metadata")
+    properties: SheetList[DMSProperty] = Field(alias="Properties")
+    views: SheetList[DMSView] = Field(alias="Views")
+    containers: SheetList[DMSContainer] | None = Field(None, alias="Containers")
+    enum: SheetList[DMSEnum] | None = Field(None, alias="Enum")
+    nodes: SheetList[DMSNode] | None = Field(None, alias="Nodes")
+    # This is a hack to allow the post_validation to be turned off when needed
+    # Will likely be moved completely out of the rules in the future
+    post_validate: bool = Field(default=True, exclude=True, repr=False)
+
+    @field_validator("views")
+    def matching_version_and_space(cls, value: SheetList[DMSView], info: ValidationInfo) -> SheetList[DMSView]:
+        if not (metadata := info.data.get("metadata")):
+            return value
+        model_version = metadata.version
+        if different_version := [
+            view.view.as_id()
+            for view in value
+            if view.view.version != model_version and view.view.space not in COGNITE_SPACES
+        ]:
+            for view_id in different_version:
+                warnings.warn(
+                    PrincipleMatchingSpaceAndVersionWarning(
+                        f"The view {view_id!r} has a different version than the data model version, {model_version}",
+                    ),
+                    stacklevel=2,
+                )
+        if different_space := [
+            view.view.as_id()
+            for view in value
+            if view.view.space != metadata.space and view.view.space not in COGNITE_SPACES
+        ]:
+            for view_id in different_space:
+                warnings.warn(
+                    PrincipleMatchingSpaceAndVersionWarning(
+                        f"The view {view_id!r} is in a different space than the data model space, {metadata.space}",
+                    ),
+                    stacklevel=2,
+                )
+        return value
+
+    @model_validator(mode="after")
+    def post_validation(self) -> "DMSRules":
+        from ._validation import DMSPostValidation
+
+        if not self.post_validate:
+            return self
+        issue_list = DMSPostValidation(self).validate()
+        if issue_list.warnings:
+            issue_list.trigger_warnings()
+        if issue_list.has_errors:
+            raise MultiValueError(issue_list.errors)
+        return self
+
+    def as_schema(self, include_pipeline: bool = False, instance_space: str | None = None) -> DMSSchema:
+        from ._exporter import _DMSExporter
+
+        return _DMSExporter(self, instance_space).to_schema()
+
+    def _repr_html_(self) -> str:
+        summary = {
+            "aspect": self.metadata.aspect,
+            "intended for": "DMS Architect",
+            "name": self.metadata.name,
+            "space": self.metadata.space,
+            "external_id": self.metadata.external_id,
+            "version": self.metadata.version,
+            "views": len(self.views),
+            "containers": len(self.containers) if self.containers else 0,
+            "properties": len(self.properties),
+        }
+
+        return pd.DataFrame([summary]).T.rename(columns={0: ""})._repr_html_()  # type: ignore
+
+    def imported_views_and_containers_ids(
+        self, include_model_views_with_no_properties: bool = True
+    ) -> tuple[set[dm.ViewId], set[dm.ContainerId]]:
+        existing_views = {view.view for view in self.views}
+        imported_views: set[dm.ViewId] = set()
+        for view in self.views:
+            for parent in view.implements or []:
+                if parent not in existing_views:
+                    imported_views.add(parent.as_id())
+        existing_containers = {container.container for container in self.containers or []}
+        imported_containers: set[dm.ContainerId] = set()
+        view_with_properties: set[ViewEntity] = set()
+        for prop in self.properties:
+            if prop.container and prop.container not in existing_containers:
+                imported_containers.add(prop.container.as_id())
+            if prop.view not in existing_views:
+                imported_views.add(prop.view.as_id())
+            view_with_properties.add(prop.view)
+
+        if include_model_views_with_no_properties:
+            extra_views = existing_views - view_with_properties
+            imported_views.update({view.as_id() for view in extra_views})
+
+        return imported_views, imported_containers
