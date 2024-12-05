6c3d7da3905d3e4c462b3bc5542c22eb2b198341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:37 +0000

    Add package ubo-app/1.0.1.dev20241126103559910199559854/ubo_app-1.0.1.dev20241126103559910199559854.tar.gz

---
+++ packages/ubo-app/ubo_app-1.0.1.dev20241126103559910199559854.tar.gz/ubo_app-1.0.1.dev20241126103559910199559854/ubo_app/services/040-camera/reducer.py
@@ -0,0 +1,200 @@
+# ruff: noqa: D100, D101, D102, D103, D104, D107
+from __future__ import annotations
+
+import datetime
+import re
+from dataclasses import replace
+
+from redux import (
+    CompleteReducerResult,
+    InitAction,
+    InitializationActionError,
+    ReducerResult,
+)
+
+from ubo_app.store.input.types import (
+    InputCancelAction,
+    InputDemandAction,
+    InputProvideAction,
+    InputResolveAction,
+)
+from ubo_app.store.services.camera import (
+    CameraAction,
+    CameraEvent,
+    CameraReportBarcodeAction,
+    CameraStartViewfinderAction,
+    CameraStartViewfinderEvent,
+    CameraState,
+    CameraStopViewfinderEvent,
+)
+from ubo_app.store.services.keypad import Key, KeypadKeyPressAction
+from ubo_app.store.services.notifications import (
+    Notification,
+    NotificationDispatchItem,
+    NotificationDisplayType,
+    NotificationsAddAction,
+    NotificationsClearByIdAction,
+)
+
+Action = InitAction | CameraAction | InputDemandAction | KeypadKeyPressAction
+DispatchAction = (
+    NotificationsAddAction | NotificationsClearByIdAction | InputResolveAction
+)
+
+
+def pop_queue(state: CameraState) -> CameraState:
+    if len(state.queue) > 0:
+        input_description, *queue = state.queue
+        return replace(state, current=input_description, queue=queue)
+    return replace(
+        state,
+        is_viewfinder_active=False,
+        current=None,
+    )
+
+
+def reducer(
+    state: CameraState | None,
+    action: Action,
+) -> ReducerResult[
+    CameraState,
+    DispatchAction,
+    CameraEvent,
+]:
+    if state is None:
+        if isinstance(action, InitAction):
+            return CameraState(is_viewfinder_active=False, queue=[])
+        raise InitializationActionError(action)
+
+    if isinstance(action, InputDemandAction):
+        if state.is_viewfinder_active:
+            return replace(
+                state,
+                queue=[
+                    *state.queue,
+                    action.description,
+                ],
+            )
+        return CompleteReducerResult(
+            state=replace(
+                state,
+                current=action.description,
+            ),
+            actions=[
+                NotificationsAddAction(
+                    notification=Notification(
+                        id='camera:qrcode',
+                        icon='󰄀󰐲',
+                        title='QR Code',
+                        content=f'[size=18dp]{action.description.prompt}[/size]',
+                        display_type=NotificationDisplayType.STICKY,
+                        is_read=True,
+                        extra_information=action.description.extra_information,
+                        expiration_timestamp=datetime.datetime.now(tz=datetime.UTC),
+                        color='#ffffff',
+                        actions=[
+                            NotificationDispatchItem(
+                                store_action=CameraStartViewfinderAction(
+                                    pattern=action.description.pattern,
+                                ),
+                                icon='󰄀',
+                                dismiss_notification=True,
+                            ),
+                        ],
+                        dismissable=False,
+                        dismiss_on_close=True,
+                    ),
+                ),
+            ],
+        )
+
+    if isinstance(action, InputResolveAction):
+        if state.current and state.current.id == action.id:
+            return CompleteReducerResult(
+                state=pop_queue(state),
+                actions=[NotificationsClearByIdAction(id='camera:qrcode')],
+                events=[CameraStopViewfinderEvent(id=state.current.id)],
+            )
+        return replace(
+            state,
+            queue=[
+                description
+                for description in state.queue
+                if description.id != action.id
+            ],
+        )
+
+    if isinstance(action, CameraStartViewfinderAction):
+        return CompleteReducerResult(
+            state=replace(
+                state,
+                is_viewfinder_active=True,
+            ),
+            events=[CameraStartViewfinderEvent(pattern=action.pattern)],
+        )
+
+    if isinstance(action, CameraReportBarcodeAction) and state.current:
+        for code in action.codes:
+            if state.current.pattern:
+                match = re.match(state.current.pattern, code)
+                if match:
+                    return CompleteReducerResult(
+                        state=pop_queue(state),
+                        actions=[
+                            InputProvideAction(
+                                id=state.current.id,
+                                value=code,
+                                data={
+                                    key.rstrip('_'): value
+                                    for key, value in match.groupdict().items()
+                                },
+                            ),
+                        ],
+                        events=[
+                            CameraStopViewfinderEvent(id=None),
+                        ],
+                    )
+            else:
+                return CompleteReducerResult(
+                    state=pop_queue(state),
+                    actions=[
+                        InputProvideAction(
+                            id=state.current.id,
+                            value=code,
+                            data=None,
+                        ),
+                    ],
+                    events=[
+                        CameraStopViewfinderEvent(id=None),
+                    ],
+                )
+
+            return state
+
+    if isinstance(action, KeypadKeyPressAction):  # noqa: SIM102
+        if action.key in [Key.BACK, Key.HOME]:
+            actions: list[DispatchAction] = []
+            events: list[CameraEvent] = []
+
+            if state.current:
+                actions.extend(
+                    [
+                        InputCancelAction(id=state.current.id),
+                        NotificationsClearByIdAction(id='camera:qrcode'),
+                    ],
+                )
+
+            if state.is_viewfinder_active:
+                events.append(
+                    CameraStopViewfinderEvent(
+                        id=state.current.id if state.current else None,
+                    ),
+                )
+
+            return CompleteReducerResult(
+                state=pop_queue(state),
+                actions=actions,
+                events=events,
+            )
+
+    return state
---
+++ packages/ubo-app/ubo_app-1.0.1.dev20241126103559910199559854.tar.gz/ubo_app-1.0.1.dev20241126103559910199559854/ubo_app/utils/input.py
@@ -0,0 +1,197 @@
+"""Imperative input."""
+
+from __future__ import annotations
+
+import asyncio
+import uuid
+from asyncio import Future
+from typing import TYPE_CHECKING, TypeAlias, overload
+
+from typing_extensions import TypeVar
+
+from ubo_app.constants import WEB_UI_LISTEN_PORT
+from ubo_app.store.input.types import (
+    InputCancelEvent,
+    InputDemandAction,
+    InputDescription,
+    InputFieldDescription,
+    InputProvideEvent,
+)
+from ubo_app.store.main import store
+from ubo_app.store.services.camera import CameraStopViewfinderEvent
+from ubo_app.store.services.notifications import NotificationExtraInformation
+from ubo_app.store.services.rgb_ring import RgbRingBlinkAction
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+
+InputResultGroupDict: TypeAlias = dict[str, str | None] | None
+
+
+ReturnType = TypeVar('ReturnType', infer_variance=True)
+
+
+@overload
+async def ubo_input(
+    *,
+    prompt: str | None = None,
+    qr_code_generation_instructions: NotificationExtraInformation | None = None,
+    title: str | None = None,
+    pattern: str,
+    fields: list[InputFieldDescription] | None = None,
+) -> tuple[str, InputResultGroupDict]: ...
+@overload
+async def ubo_input(
+    *,
+    prompt: str | None = None,
+    qr_code_generation_instructions: NotificationExtraInformation | None = None,
+    title: str | None = None,
+    fields: list[InputFieldDescription],
+) -> tuple[str, InputResultGroupDict]: ...
+@overload
+async def ubo_input(
+    *,
+    prompt: str | None = None,
+    qr_code_generation_instructions: NotificationExtraInformation | None = None,
+    title: str | None = None,
+    pattern: str,
+    fields: list[InputFieldDescription] | None = None,
+    resolver: Callable[[str, InputResultGroupDict], ReturnType],
+) -> ReturnType: ...
+@overload
+async def ubo_input(
+    *,
+    prompt: str | None = None,
+    qr_code_generation_instructions: NotificationExtraInformation | None = None,
+    title: str | None = None,
+    fields: list[InputFieldDescription],
+    resolver: Callable[[str, InputResultGroupDict], ReturnType],
+) -> ReturnType: ...
+async def ubo_input(  # noqa: PLR0913
+    *,
+    prompt: str | None = None,
+    qr_code_generation_instructions: NotificationExtraInformation | None = None,
+    title: str | None = None,
+    pattern: str | None = None,
+    fields: list[InputFieldDescription] | None = None,
+    resolver: Callable[[str, InputResultGroupDict], ReturnType] | None = None,
+) -> tuple[str, InputResultGroupDict] | ReturnType:
+    """Input the user in an imperative way."""
+    prompt_id = uuid.uuid4().hex
+    loop = asyncio.get_running_loop()
+
+    subscriptions: set[Callable[[], None]] = set()
+    future: Future[tuple[str, InputResultGroupDict]] = loop.create_future()
+
+    def unsubscribe() -> None:
+        for subscription in subscriptions:
+            subscription()
+
+    def handle_input_cancel_event(event: InputCancelEvent) -> None:
+        if event.id == prompt_id:
+            unsubscribe()
+            loop.call_soon_threadsafe(future.cancel)
+
+    def handle_input_provide_event(event: InputProvideEvent) -> None:
+        if event.id == prompt_id:
+            unsubscribe()
+            from kivy.utils import get_color_from_hex
+
+            loop.call_soon_threadsafe(
+                future.set_result,
+                (event.value, event.data),
+            )
+            kivy_color = get_color_from_hex('#21E693')
+            color = tuple(round(c * 255) for c in kivy_color[:3])
+            store.dispatch(
+                RgbRingBlinkAction(
+                    color=color,
+                    repetitions=1,
+                    wait=200,
+                ),
+            )
+
+    def handle_cancel(event: CameraStopViewfinderEvent) -> None:
+        if event.id == prompt_id:
+            unsubscribe()
+            loop.call_soon_threadsafe(future.cancel)
+
+    subscriptions.add(
+        store.subscribe_event(
+            InputProvideEvent,
+            handle_input_provide_event,
+            keep_ref=False,
+        ),
+    )
+    subscriptions.add(
+        store.subscribe_event(
+            InputCancelEvent,
+            handle_input_cancel_event,
+            keep_ref=False,
+        ),
+    )
+    subscriptions.add(
+        store.subscribe_event(
+            CameraStopViewfinderEvent,
+            handle_cancel,
+            keep_ref=False,
+        ),
+    )
+    subscriptions.add(
+        store.subscribe_event(
+            CameraStopViewfinderEvent,
+            handle_cancel,
+            keep_ref=False,
+        ),
+    )
+    web_dashboard_instructions = f"""
+Web dashboard is served on port {WEB_UI_LISTEN_PORT} and it provides an interface for \
+entering this input."""
+    store.dispatch(
+        InputDemandAction(
+            description=InputDescription(
+                title=title or 'Untitled input',
+                prompt=prompt,
+                extra_information=NotificationExtraInformation(
+                    text=(
+                        (
+                            qr_code_generation_instructions.text
+                            if qr_code_generation_instructions
+                            else None
+                        )
+                        or ''
+                    )
+                    + web_dashboard_instructions,
+                    piper_text=(
+                        (
+                            qr_code_generation_instructions.piper_text
+                            if qr_code_generation_instructions
+                            else None
+                        )
+                        or ''
+                    )
+                    + web_dashboard_instructions,
+                    picovoice_text=(
+                        (
+                            qr_code_generation_instructions.picovoice_text
+                            if qr_code_generation_instructions
+                            else None
+                        )
+                        or ''
+                    )
+                    + web_dashboard_instructions,
+                ),
+                id=prompt_id,
+                pattern=pattern,
+                fields=fields,
+            ),
+        ),
+    )
+
+    result = await future
+
+    if not resolver:
+        return result
+
+    return resolver(*result)
