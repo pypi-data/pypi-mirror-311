b6a927f8f78e63a7a83f58fd1560c81ec2bbef6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:27 +0000

    Add package fundrive/1.2.36/fundrive-1.2.36-py3-none-any.whl

---
+++ packages/fundrive/fundrive-1.2.36-py3-none-any.whl/fundrive/__init__.py
@@ -0,0 +1,23 @@
+from .drives import (
+    AlipanDrive,
+    LanZouDrive,
+    LanZouSnapshot,
+    OpenDataLabDrive,
+    OSSDrive,
+    TianChiDrive,
+    TSingHuaDrive,
+)
+from .fungit import GiteeDrive, GithubDrive
+
+__all__ = [
+    "LanZouDrive",
+    "OSSDrive",
+    "TianChiDrive",
+    "TSingHuaDrive",
+    "OpenDataLabDrive",
+    "GithubDrive",
+    "LanZouDrive",
+    "LanZouSnapshot",
+    "GiteeDrive",
+    "AlipanDrive",
+]
