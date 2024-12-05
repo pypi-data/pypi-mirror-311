844bddb026ee251d8e343c908bd66dae4b5136cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:51 +0000

    Add package latex-generator-hw2/0.2.0/latex_generator_hw2-0.2.0.tar.gz

---
+++ packages/latex-generator-hw2/latex_generator_hw2-0.2.0.tar.gz/latex_generator_hw2-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latex_generator_hw2',
+    version='0.2.0',
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[],
+    entry_points={},
+)
