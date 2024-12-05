a003785721abb2431bee4f8fef8dc3290860fc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:39 +0000

    Add package easytuio/0.1.1/easytuio-0.1.1-py3-none-any.whl

---
+++ packages/easytuio/easytuio-0.1.1-py3-none-any.whl/easytuio/__init__.py
@@ -0,0 +1,10 @@
+from .client import TUIOClient
+from .profiles import TUIOProfile, TUIO2DCursor, TUIO2DObject, TUIO2DBlob
+
+__all__ = [
+    "TUIOClient",
+    "TUIOProfile",
+    "TUIO2DCursor",
+    "TUIO2DObject",
+    "TUIO2DBlob",
+]
