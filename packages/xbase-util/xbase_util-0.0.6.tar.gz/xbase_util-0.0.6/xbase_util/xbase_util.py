6b1da1d6979e13f710882e5df633cf7cba29acca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:55 +0000

    Add package xbase-util/0.0.6/xbase_util-0.0.6.tar.gz

---
+++ packages/xbase-util/xbase_util-0.0.6.tar.gz/xbase_util-0.0.6/xbase_util/xbase_util.py
@@ -0,0 +1,21 @@
+import os
+
+import execjs
+import geoip2.database
+
+current_dir = os.path.dirname(__file__)
+parse_path = os.path.join(current_dir, '..', 'assets', 'arkimeparse.js')
+geo_path = os.path.join(current_dir, '..', 'assets', 'GeoLite2-City.mmdb')
+
+
+def parse_expression(expression):
+    if expression:
+        with open(parse_path, "r") as f:
+            ctx = execjs.compile(f.read())
+            return ctx.call("parse_exp", expression)
+    else:
+        return None
+
+
+def geo_reader():
+    return geoip2.database.Reader(geo_path)
