d872126f2b7773810aeb597d3923b249a42b0e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:18 +0000

    Add package EcosDataReader/0.1.4/ecosdatareader-0.1.4.tar.gz

---
+++ packages/EcosDataReader/ecosdatareader-0.1.4.tar.gz/ecosdatareader-0.1.4/EcosDataReader/__init__.py
@@ -0,0 +1,16 @@
+from .reader import EcosDataReader
+from .fetcher import EcosFetcher
+# from .data_reader.economic_index import EconomicIndexDataReader
+# from .data_reader.price_index import PriceIndexDataReader
+# from .data_reader.interest import InterestDataReader
+
+# from .data_reader.global_index import GlobalIndexDataReader
+# from .data_reader.foreign_currency import ForeignCurrencyDataReader
+
+# from .data_reader.bank import BankDataReader
+# from .data_reader.monetary import MonetaryDataReader
+
+# from .data_reader.market import MarketDataReader
+
+# from .data_reader.payment import PaymentDataReader
+# from .data_reader.trade import TradeDataReader
