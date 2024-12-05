c9fc513177bf485c72656350868e6d32849199e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:49 +0000

    Add package wiliot-tools/4.8.1/wiliot_tools-4.8.1-py3-none-any.whl

---
+++ packages/wiliot-tools/wiliot_tools-4.8.1-py3-none-any.whl/wiliot_tools/shipment_approval_gui/shipment_approval_app.py
@@ -0,0 +1,13 @@
+from shipment_approval_main import ShipmentApprovalApp
+from PIL import Image, ImageTk
+import json
+import customtkinter as ctk
+import pandas as pd
+import os
+import subprocess
+import threading
+import customtkinter as ctk
+
+if __name__ == "__main__":
+    app = ShipmentApprovalApp()
+    app.mainloop()
