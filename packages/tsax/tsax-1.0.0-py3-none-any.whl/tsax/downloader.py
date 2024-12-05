03a4476af1c3dba1126edce0b068eeaeedf8dfd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:42 +0000

    Add package tsax/1.0.0/tsax-1.0.0-py3-none-any.whl

---
+++ packages/tsax/tsax-1.0.0-py3-none-any.whl/tsax/downloader.py
@@ -0,0 +1,29 @@
+import getpass
+import requests
+import os
+
+def download_file():
+    CORRECT_PASSCODE = "2346" 
+    FILE_URL = "https://drive.google.com/uc?export=download&id=1ic7TWOf2ylksDNbNkqavyNlul4Dxf2rl"
+    FILE_NAME = "TSAX.zip"
+
+    passcode = getpass.getpass("Enter the passcode: ")
+
+    if passcode == CORRECT_PASSCODE:
+        try:
+            print("Downloading the file...")
+            response = requests.get(FILE_URL, stream=True)
+            response.raise_for_status()
+
+            total_size = int(response.headers.get('content-length', 0))
+            desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", FILE_NAME)
+
+            with open(desktop_path, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+            print(f"File downloaded successfully to {desktop_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error downloading the file: {e}")
+    else:
+        print("Incorrect passcode. Access denied.")
