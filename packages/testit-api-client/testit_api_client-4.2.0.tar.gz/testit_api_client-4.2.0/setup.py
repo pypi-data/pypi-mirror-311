0d3e10ef5a211117afae67369ba1b08b66c990bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:25 +0000

    Add package testit-api-client/4.2.0/testit_api_client-4.2.0.tar.gz

---
+++ packages/testit-api-client/testit_api_client-4.2.0.tar.gz/testit_api_client-4.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "testit-api-client"
+
+VERSION = "4.2.0"
+
+REQUIRES = [
+  "urllib3 >= 1.25.3",
+  "python-dateutil",
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+
+    description='API-client for Test IT',
+    long_description=open('README.md', "r").read(),
+    long_description_content_type="text/markdown",
+    author='Integration team',
+    author_email='integrations@testit.software',
+    url='https://pypi.org/project/testit-api-client/',
+    python_requires=">=3.6",
+    install_requires=REQUIRES,
+    py_modules=['testit_api_client'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True
+)
