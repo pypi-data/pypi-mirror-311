6ae6f841e68cf5923fbd704f3370522f4d7a7cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:24 +0000

    Add package kroky/0.1.0/kroky-0.1.0.tar.gz

---
+++ packages/kroky/kroky-0.1.0.tar.gz/kroky-0.1.0/kroky/__init__.py
@@ -0,0 +1,12 @@
+# /home/jon/Namizje/Projekti/kroky-library/kroky/__init__.py
+
+# This file indicates that the kroky directory is a Python package.
+# You can initialize package-level variables or import submodules here.
+
+# Example of importing a submodule
+# from . import submodule_name
+
+# Example of initializing a package-level variable
+# package_variable = "This is a package-level variable"
+
+from .main import Kroky
