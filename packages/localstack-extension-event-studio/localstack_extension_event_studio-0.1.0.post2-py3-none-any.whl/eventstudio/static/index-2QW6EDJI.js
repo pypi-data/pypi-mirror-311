a8099a98b828da04ea209933722ea649028f7865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:27:20 +0000

    Add package localstack-extension-event-studio/0.1.0.post2/localstack_extension_event_studio-0.1.0.post2-py3-none-any.whl

---
+++ packages/localstack-extension-event-studio/localstack_extension_event_studio-0.1.0.post2-py3-none-any.whl/eventstudio/static/index-2QW6EDJI.js
@@ -0,0 +1,5 @@
+(() => {
+  // html:/home/thomas/workspace/localstack/localstack-extension-event-studio/frontend/src/index.html
+  var src_default = "./index-UUISF6VF.html";
+})();
+//# sourceMappingURL=index-2QW6EDJI.js.map
