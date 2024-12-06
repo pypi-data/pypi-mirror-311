9f7244cc14651106ad29c482b231d8b061ed0d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:47 +0000

    Add package boto3-stubs/1.35.71/boto3_stubs-1.35.71-py3-none-any.whl

---
+++ packages/boto3-stubs/boto3_stubs-1.35.71-py3-none-any.whl/boto3-stubs/docs/resource.pyi
@@ -0,0 +1,14 @@
+from typing import Any
+
+from botocore.docs.bcdoc.restdoc import DocumentStructure
+from botocore.session import Session
+
+from .base import BaseDocumenter
+
+class ResourceDocumenter(BaseDocumenter):
+    def __init__(self, resource: Any, botocore_session: Session, root_docs_path: str) -> None: ...
+    def document_resource(self, section: DocumentStructure) -> None: ...
+
+class ServiceResourceDocumenter(ResourceDocumenter):
+    @property
+    def class_name(self) -> str: ...
