3eeb4421eed4bb289c35f08e071bb6a11b273a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:34 +0000

    Add package slack-airflow-dag-alert/1.0.0/slack_airflow_dag_alert-1.0.0.tar.gz

---
+++ packages/slack-airflow-dag-alert/slack_airflow_dag_alert-1.0.0.tar.gz/slack_airflow_dag_alert-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='slack_airflow_dag_alert',
+      version='1.0.0',
+      description='Send airflow dag alerts in slack',
+      packages=['slack_airflow_dag_alert'],
+      author_email='suyogkokaje6@gmail.com',
+      zip_safe=False)
