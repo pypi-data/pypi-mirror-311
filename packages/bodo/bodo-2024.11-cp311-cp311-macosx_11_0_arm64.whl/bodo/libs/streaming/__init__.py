85a38e18f1c15432742186e3b3ec6d169363b420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:28 +0000

    Add package bodo/2024.11/bodo-2024.11-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/bodo/bodo-2024.11-cp311-cp311-macosx_11_0_arm64.whl/bodo/libs/streaming/__init__.py
@@ -0,0 +1,1 @@
+# Copyright (C) 2024 Bodo Inc. All rights reserved.
---
+++ packages/bodo/bodo-2024.11-cp311-cp311-macosx_11_0_arm64.whl/bodo/submit/worker_state.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 Bodo Inc. All rights reserved.
+"""Distinguish Spawner from Worker at runtime"""
+
+_is_worker = False
+
+
+def is_worker():
+    """Returns true if process is a worker"""
+    return _is_worker
+
+
+def set_is_worker():
+    """Marks the current process as a worker"""
+    global _is_worker
+    _is_worker = True
