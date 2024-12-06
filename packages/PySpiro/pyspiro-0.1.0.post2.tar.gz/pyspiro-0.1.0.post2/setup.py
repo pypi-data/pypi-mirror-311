2533c74666eb6240d298c927053e033019a0e8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:12 +0000

    Add package PySpiro/0.1.0.post2/pyspiro-0.1.0.post2.tar.gz

---
+++ packages/PySpiro/pyspiro-0.1.0.post2.tar.gz/pyspiro-0.1.0.post2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0-02'
+DESCRIPTION = 'PySpiro'
+LONG_DESCRIPTION = 'Spirometry reference correction'
+
+setup(
+        name="PySpiro",
+        version=VERSION,
+        author="Roman Martin, Hendrik Pott",
+        author_email="roman.martin@hhu.de, hendrik.pott@uni-marburg.de",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        package_data={'': ['data/gli_2012_splines.csv', 'gli_2012_coefficients.csv']},
+        include_package_data=True,
+        install_requires=["pandas", "numpy"],
+        keywords=['python', 'respirology', 'spirometry'],
+        classifiers= [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Healthcare Industry",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Education",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: Unix",
+        ]
+)
