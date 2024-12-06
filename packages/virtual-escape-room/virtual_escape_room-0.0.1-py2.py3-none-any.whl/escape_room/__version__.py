b92e2420a7182030c0ad76deac565d13b1f97ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:30 +0000

    Add package virtual-escape-room/0.0.1/virtual_escape_room-0.0.1-py2.py3-none-any.whl

---
+++ packages/virtual-escape-room/virtual_escape_room-0.0.1-py2.py3-none-any.whl/escape_room/__version__.py
@@ -0,0 +1,4 @@
+__title__ = "Virtual Escape Room"
+__description__ = "Flask based microframework to generate virtual escape room."
+__version__ = "0.0.1"
+__author__ = "Yuqiu (Ian) Yang"
---
+++ packages/virtual-escape-room/virtual_escape_room-0.0.1-py2.py3-none-any.whl/escape_room/auth.py
@@ -0,0 +1,105 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from escape_room.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif (not password) or (not confirm_password):
+            error = 'Password is required and please reenter your password.'
+        
+        if password != confirm_password:
+            error = 'Passwords do not match. Please try again.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Username does not exist. Please register first.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password. Please try again.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+        
+@bp.route('/logout')
+def logout():
+    db = get_db()
+    db.execute(
+        "DELETE FROM post"
+        " WHERE author_id = ?",
+        (g.user["id"],),
+    )
+    db.commit()
+    session.pop("puzzle_id_", None)
+    session.pop("puzzles_seen_list_", None)
+    session.clear()
+    session.permanent = False
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+
+    return wrapped_view
