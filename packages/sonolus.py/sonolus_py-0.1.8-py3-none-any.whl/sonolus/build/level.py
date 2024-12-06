6e4638e8649fa51a0036cb7a1f7b9095a7c33c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:09 +0000

    Add package sonolus.py/0.1.8/sonolus_py-0.1.8-py3-none-any.whl

---
+++ packages/sonolus.py/sonolus_py-0.1.8-py3-none-any.whl/sonolus/build/level.py
@@ -0,0 +1,24 @@
+from sonolus.build.engine import package_output
+from sonolus.script.level import LevelData
+
+
+def package_level_data(
+    level_data: LevelData,
+):
+    return package_output(build_level_data(level_data))
+
+
+def build_level_data(
+    level_data: LevelData,
+):
+    level_refs = {entity: i for i, entity in enumerate(level_data.entities)}
+    return {
+        "bgmOffset": level_data.bgm_offset,
+        "entities": [
+            {
+                "archetype": entity.name,
+                "data": entity._level_data_entries(level_refs),
+            }
+            for entity in level_data.entities
+        ],
+    }
