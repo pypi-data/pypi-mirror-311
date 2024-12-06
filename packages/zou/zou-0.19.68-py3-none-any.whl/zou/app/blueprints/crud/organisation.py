8cc7653643f438b14f309e46bd661130e601a8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:58 +0000

    Add package zou/0.19.68/zou-0.19.68-py3-none-any.whl

---
+++ packages/zou/zou-0.19.68-py3-none-any.whl/zou/app/blueprints/crud/organisation.py
@@ -0,0 +1,29 @@
+from zou.app.models.organisation import Organisation
+from zou.app.blueprints.crud.base import BaseModelResource, BaseModelsResource
+
+from zou.app.services import persons_service
+
+
+class OrganisationsResource(BaseModelsResource):
+    def __init__(self):
+        BaseModelsResource.__init__(self, Organisation)
+
+    def check_read_permissions(self, options=None):
+        return True
+
+
+class OrganisationResource(BaseModelResource):
+    def __init__(self):
+        BaseModelResource.__init__(self, Organisation)
+
+    def check_read_permissions(self, instance):
+        return True
+
+    def pre_update(self, instance_dict, data):
+        if "hours_by_day" in data:
+            data["hours_by_day"] = float(data["hours_by_day"])
+        return data
+
+    def post_update(self, instance_dict, data):
+        persons_service.clear_oranisation_cache()
+        return instance_dict
