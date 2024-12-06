a43bc9413cd0582bf0f1248ad38631cb864af6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:39 +0000

    Add package example-package-shahbaz-majeed/0.3/example_package_shahbaz_majeed-0.3.tar.gz

---
+++ packages/example-package-shahbaz-majeed/example_package_shahbaz_majeed-0.3.tar.gz/example_package_shahbaz_majeed-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'example_package_shahbaz_majeed',
+    version = '0.3',
+    pakages=find_packages(),
+    install_requires=[
+
+    ],
+)
