8a385b2be197c49ef5276479bef0a5092ba1a404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:39 +0000

    Add package conviso-cli/2.2.13rc3/conviso_cli-2.2.13rc3-py3-none-any.whl

---
+++ packages/conviso-cli/conviso_cli-2.2.13rc3-py3-none-any.whl/convisoappsec/flow/graphql_api/beta/schemas/mutations/__init__.py
@@ -0,0 +1,39 @@
+CREATE_SAST_FINDING_INPUT = """ 
+mutation createSastFinding($input: CreateSastFindingInput!) {
+  createSastFinding(input: $input) {
+    issue {
+      id
+    }
+  }
+}
+"""
+
+CREATE_SCA_FINDING_INPUT = """ 
+mutation createScaFinding($input: CreateScaFindingInput!) {
+    createScaFinding(input: $input) {
+        issue {
+            id
+        }
+    }
+}
+"""
+
+UPDATE_ISSUE_STATUS = """
+mutation (
+  $issueId: ID!,
+  $status: IssueStatusLabel!,
+  $reason: String
+) {
+  changeIssueStatus (
+    input: {
+      id: $issueId
+      status: $status
+      reason: $reason
+    }
+  ) {
+    issue {
+      id
+    }
+  }
+}
+"""
