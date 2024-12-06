4d20baf8c45fbdff83806e6cd5f821d4a199047a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:22 +0000

    Add package MORTM/1.1b14/MORTM-1.1b14-py3-none-any.whl

---
+++ packages/MORTM/MORTM-1.1b14-py3-none-any.whl/mortm/generate.py
@@ -0,0 +1,33 @@
+import torch
+from torch import Tensor
+
+from .mortm import MORTM
+
+
+def generate_note(note_max: int, input_seq, model: MORTM, t=1.0):
+    model.eval()
+    if not isinstance(input_seq, torch.Tensor):
+        input_seq = torch.tensor(input_seq, dtype=torch.long, device=model.progress.get_device())
+
+    generated = input_seq.tolist()
+    for _ in range(note_max):
+        for i in range(3):
+            logits = model(input_seq.unsqueeze(0))
+            logits = logits[-1, -1, :]
+            if i == 0:
+                token = model.top_p_sampling(logits, p=0.95, temperature=0.8) #S
+            elif i == 1:
+                token = model.top_p_sampling(logits, p=0.90, temperature=1.2) #P
+            else:
+                token = model.top_p_sampling(logits, p=0.95, temperature=0.8) #D
+            generated.append(token)
+    return torch.tensor(generated, dtype=torch.long, device=model.progress.get_device())
+
+def generate_measure(measure: int, input_seq, model: MORTM):
+    model.eval()
+    if not isinstance(input_seq, torch.Tensor):
+        input_seq = torch.tensor(input_seq, dtype=torch.long, device=model.progress.get_device())
+
+    generated = input_seq.tolist()
+    for _ in range(measure):
+        pass
