f32e9ffd250449d21a143f148c1067db81842f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:47 +0000

    Add package omnidocs/0.1.0/omnidocs-0.1.0-py3-none-any.whl

---
+++ packages/omnidocs/omnidocs-0.1.0-py3-none-any.whl/omnidocs/tasks/__init__.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+# from .layout_analysis import YOLOLayoutDetector
+# from .layout_analysis import FlorenceLayoutDetector
+# from .layout_analysis import PaddleLayoutDetector
+# from .layout_analysis import RTDETRLayoutDetector
+# from .layout_analysis import SuryaLayoutDetector
+
+# __all__ = [
+#     "YOLOLayoutDetector","FlorenceLayoutDetector", "PaddleLayoutDetector", "RTDETRLayoutDetector", "SuryaLayoutDetector"
+# ]
