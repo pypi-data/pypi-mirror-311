0b4c594f7032ec38620ba2294a498b5888121194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:35 +0000

    Add package PatrowlEnginesUtils/1.2.2/PatrowlEnginesUtils-1.2.2-py2.py3-none-any.whl

---
+++ packages/PatrowlEnginesUtils/PatrowlEnginesUtils-1.2.2-py2.py3-none-any.whl/PatrowlEnginesUtils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""PatrowlEnginesUtils init file."""
+
+
+__title__ = 'patrowl_engine_utils'
+__version__ = '1.2.2'
+__author__ = 'Nicolas MATTIOCCO'
+__license__ = 'AGPLv3'
+__copyright__ = 'Copyright (C) 2018-2021 Nicolas Mattiocco - @MaKyOtOx'
