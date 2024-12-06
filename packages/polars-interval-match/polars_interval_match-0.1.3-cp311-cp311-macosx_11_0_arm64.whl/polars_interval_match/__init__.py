dc0604aeb7232f42c9b1be80e2385a2769e357bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:53 +0000

    Add package polars-interval-match/0.1.3/polars_interval_match-0.1.3-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/polars-interval-match/polars_interval_match-0.1.3-cp311-cp311-macosx_11_0_arm64.whl/polars_interval_match/__init__.py
@@ -0,0 +1,25 @@
+# expression_lib/__init__.py
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import polars as pl
+from polars.plugins import register_plugin_function
+from polars._typing import IntoExpr
+
+PLUGIN_PATH = Path(__file__).parent
+
+
+def match_intervals(values: IntoExpr, intervals: pl.Series) -> pl.Expr:
+    """Match values to intervals."""
+    try:
+        values = values.cast(pl.Float64)
+    except:
+        raise ValueError("values must be castable to Float64")
+    intervals = ";".join(intervals.to_list())
+    return register_plugin_function(
+        plugin_path=PLUGIN_PATH,
+        function_name="match_intervals",
+        args=values,
+        kwargs={"intervals": intervals},
+        is_elementwise=True,
+    )
