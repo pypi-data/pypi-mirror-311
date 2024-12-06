d34bb0db3706f802c16fd44d1fc68c951ccdcaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:43 +0000

    Add package cbr-web-components/0.12.120/cbr_web_components-0.12.120.tar.gz

---
+++ packages/cbr-web-components/cbr_web_components-0.12.120.tar.gz/cbr_web_components-0.12.120/cbr_web_components/js/cbr/CBR_Events.mjs
@@ -0,0 +1,19 @@
+export const CBR_Events = {
+    CBR__FILE__CANCEL          : 'cbr::file:cancel'         ,
+    CBR__FILE__CHANGED         : 'cbr::file:changed'        ,
+    CBR__FILE__EDIT            : 'cbr::file:edit'           ,
+    CBR__FILE__EDIT_MODE       : 'cbr::file:edit-mode'      ,
+    CBR__FILE__GET_CONTENT     : 'cbr::file:get-content'    ,
+    CBR__FILE__VIEW_MODE       : 'cbr::file:view-mode'      ,
+    CBR__FILE__LOAD            : 'cbr::file:load'           ,
+    CBR__FILE__LOADED          : 'cbr::file:loaded'         ,
+    CBR__FILE__SAVED           : 'cbr::file:saved'          ,
+    CBR__FILE__SAVE            : 'cbr::file:save'           ,
+    CBR__FILE__SHOW_HISTORY    : 'cbr::file:show-history'   ,
+    CBR__FILE__HIDE_HISTORY    : 'cbr::file:hide-history'   ,
+
+    CBR__UI__LEFT_MENU_TOGGLE  : 'cbr::ui::left-menu:toggle'
+
+}
+
+export default CBR_Events;
