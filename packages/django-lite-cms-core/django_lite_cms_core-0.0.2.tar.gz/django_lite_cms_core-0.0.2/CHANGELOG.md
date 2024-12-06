735d0e3fdf424e7b85882a9600a6e88dc348ac7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:26 +0000

    Add package django-lite-cms-core/0.0.2/django_lite_cms_core-0.0.2.tar.gz

---
+++ packages/django-lite-cms-core/django_lite_cms_core-0.0.2.tar.gz/django_lite_cms_core-0.0.2/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+<!-- GENERATOR_PLACEHOLDER -->
+
+# 0.0.2
+
+### Fixed
+- Rename forgotten "core" static directory to "lite_cms_core"
+
+## 0.0.1
+
+### Initial
+- Initial version
---
+++ packages/django-lite-cms-core/django_lite_cms_core-0.0.2.tar.gz/django_lite_cms_core-0.0.2/lite_cms_core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from lite_cms_core import views
+
+app_name = 'lite_cms_core'
+
+urlpatterns = [
+    path('search/', views.search, name='search'),
+    path('extsearch/', views.ext_search_form, name='extsearch'),
+]
