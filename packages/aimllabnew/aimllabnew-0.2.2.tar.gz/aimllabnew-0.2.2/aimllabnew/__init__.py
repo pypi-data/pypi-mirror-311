c1d657e753c14d262728e090794272650251eddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:22 +0000

    Add package aimllabnew/0.2.2/aimllabnew-0.2.2.tar.gz

---
+++ packages/aimllabnew/aimllabnew-0.2.2.tar.gz/aimllabnew-0.2.2/aimllabnew/__init__.py
@@ -0,0 +1,16 @@
+from .codes import a_star, eight_puzzle, ttt, sudoku, csp
+
+
+
+
+
+def list_functions():
+   print('''
+a_star,
+eight_puzzle, 
+ttt,
+sudoku, 
+csp 
+''')
+
+
---
+++ packages/aimllabnew/aimllabnew-0.2.2.tar.gz/aimllabnew-0.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aimllabnew",
+    version="0.2.2",
+    description="A Python library with various useful AI/ML code snippets.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="mdimado",
+    author_email="mdimad005@gmail.com",
+    url="https://github.com/mdimado/aimllab",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
