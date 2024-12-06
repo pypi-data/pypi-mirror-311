6c364f3f86d865e3058022693e22f0e1011543fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:55 +0000

    Add package SubjData/0.1.2/SubjData-0.1.2-py3-none-any.whl

---
+++ packages/SubjData/SubjData-0.1.2-py3-none-any.whl/SubjData/__init__.py
@@ -0,0 +1,7 @@
+import download
+import process
+
+from main import get_target_info
+from main import get_category_info
+from main import create_target_dataset
+from main import create_category_dataset
