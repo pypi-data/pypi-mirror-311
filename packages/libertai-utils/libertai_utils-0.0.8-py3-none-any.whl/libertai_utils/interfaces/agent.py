0e69d1a4a6d87f054c2c49f786fcfc9fd678259a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:44 +0000

    Add package libertai-utils/0.0.8/libertai_utils-0.0.8-py3-none-any.whl

---
+++ packages/libertai-utils/libertai_utils-0.0.8-py3-none-any.whl/libertai_utils/interfaces/agent.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+from libertai_utils.interfaces.subscription import SubscriptionAccount
+
+
+class BaseDeleteAgentBody(BaseModel):
+    subscription_id: str
+    password: str
+
+
+class BaseSetupAgentBody(BaseDeleteAgentBody):
+    account: SubscriptionAccount
