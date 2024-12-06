fd7ea36442d6bb84983a1be0139460c7210a3814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:52 +0000

    Add package example-pkg-berry0303/0.0.1/example_pkg_berry0303-0.0.1.tar.gz

---
+++ packages/example-pkg-berry0303/example_pkg_berry0303-0.0.1.tar.gz/example_pkg_berry0303-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="example-pkg-berry0303",
+  version="0.0.1",
+  author="berry0303",
+  author_email="berry_0403@163.com",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+  install_requires=[
+    'pandas>=1.3.5',
+  ],
+)
