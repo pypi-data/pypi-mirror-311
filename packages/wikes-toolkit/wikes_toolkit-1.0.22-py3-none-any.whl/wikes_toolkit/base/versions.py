b154c512e29e2c52faa544701d48c3a71d5b6a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:10 +0000

    Add package wikes-toolkit/1.0.22/wikes_toolkit-1.0.22-py3-none-any.whl

---
+++ packages/wikes-toolkit/wikes_toolkit-1.0.22-py3-none-any.whl/wikes_toolkit/base/versions.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class DatasetVersion:
+    base_url: str
+
+
+class DatasetName(Enum):
+
+    @abstractmethod
+    def get_dataset_url(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_version(self) -> str:
+        pass
