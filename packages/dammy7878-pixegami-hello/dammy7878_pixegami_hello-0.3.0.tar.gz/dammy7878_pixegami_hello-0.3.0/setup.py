7f6a8ec3e62b1574e8fe357cae7841b489408251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:54 +0000

    Add package dammy7878-pixegami-hello/0.3.0/dammy7878_pixegami_hello-0.3.0.tar.gz

---
+++ packages/dammy7878-pixegami-hello/dammy7878_pixegami_hello-0.3.0.tar.gz/dammy7878_pixegami_hello-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='dammy7878_pixegami_hello',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "pixegami-hello = pixegami_hello:hello",
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
