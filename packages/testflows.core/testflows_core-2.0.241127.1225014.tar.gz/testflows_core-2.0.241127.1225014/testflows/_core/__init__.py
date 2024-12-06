0470bea04130bf1b8b4548326ae430dee24db260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:14 +0000

    Add package testflows.core/2.0.241127.1225014/testflows_core-2.0.241127.1225014.tar.gz

---
+++ packages/testflows.core/testflows_core-2.0.241127.1225014.tar.gz/testflows_core-2.0.241127.1225014/testflows/_core/__init__.py
@@ -0,0 +1,39 @@
+# Copyright 2019-2021 Katteli Inc.
+# TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+__author__ = "Vitaliy Zakaznikov"
+__version__ = "2.0.241127.1225014"
+__repository__ = "https://github.com/testflows/TestFlows-Core"
+__commit__ = "66035d195d3556e866ad7b4fc23254ab1f17a59b"
+__branch__ = "master"
+__license__ = f"""
+Copyright 2019-{datetime.now().year} Katteli Inc.
+TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+See testflows/_core/contrib folder in sources for licenses of each third-party module.
+"""
