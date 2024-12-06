53a96dabdc17792e0f591aa77e76fbf1cfbeab1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:39 +0000

    Add package naeural-client/2.3.1/naeural_client-2.3.1-py3-none-any.whl

---
+++ packages/naeural-client/naeural_client-2.3.1-py3-none-any.whl/naeural_client/cli/cli.py
@@ -0,0 +1,48 @@
+import argparse
+
+from naeural_client.utils.config import maybe_init_config, log_with_color
+from naeural_client.cli.cli_commands import CLI_COMMANDS
+
+from naeural_client import version
+
+def build_parser():
+  """
+  Dynamically builds the argument parser based on CLI_COMMANDS.
+
+  Returns
+  -------
+  argparse.ArgumentParser
+      Configured argument parser.
+  """
+  descr = f"nepctl v{version} - CLI for Naeural Edge Protocol SDK package"
+  
+ 
+  parser = argparse.ArgumentParser(description=descr)
+  subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+  # Iterate over top-level commands
+  for command, subcommands in CLI_COMMANDS.items():
+    command_parser = subparsers.add_parser(command, help=f"{command} commands")
+    if isinstance(subcommands, dict):  # Nested subcommands
+      command_subparsers = command_parser.add_subparsers(dest="subcommand")
+      for subcommand, func in subcommands.items():
+        subcommand_parser = command_subparsers.add_parser(
+            subcommand, help=f"{subcommand} command"
+        )
+        subcommand_parser.set_defaults(func=func)
+    else:
+      command_parser.set_defaults(func=subcommands)
+
+  return parser
+
+def main():
+  maybe_init_config()
+  parser = build_parser()
+  args = parser.parse_args()
+  if hasattr(args, "func"):
+    args.func()  # Call the dynamically loaded function
+  else:
+    parser.print_help()
+
+if __name__ == "__main__":
+  main()
