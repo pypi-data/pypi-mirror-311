264f611ba6d30617dc79980a191658f3418b328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:49 +0000

    Add package vectocore/0.1.7/vectocore-0.1.7.tar.gz

---
+++ packages/vectocore/vectocore-0.1.7.tar.gz/vectocore-0.1.7/vectocore/vectocore.py
@@ -0,0 +1,24 @@
+import os
+import json
+import requests
+from components.common_logs import _logger as logger
+
+
+class Vectocore:
+    def __init__(self, tenantKey=""):
+        # 변수 은닉화
+        self.__VECTOR_URL = "https://api.vectocore.com/api"
+        self.__tenant_key = os.environ.get("VECTOCORE_TENANT_KEY", tenantKey)
+        if self.__tenant_key == "" or self.__tenant_key is None:
+            raise ValueError("Tenant key is none")
+
+    def put_index(self, index_name: str, desc=""):
+        headers = {"Content-Type": "application/json", "x-api-key": self.__tenant_key}
+        data = {"command": "put_index", "index_name": index_name, "index_desc": desc}
+
+        logger.info("data : {}".format(data))
+        response = requests.post(url=self.__VECTOR_URL, headers=headers, json=data)
+
+        logger.info("response : {}".format(response))
+
+        return response
