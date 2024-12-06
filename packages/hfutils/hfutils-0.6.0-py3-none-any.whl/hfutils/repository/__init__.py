3631dec880b1b4231cf5dcf3932d8e65e623bd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:36 +0000

    Add package hfutils/0.6.0/hfutils-0.6.0-py3-none-any.whl

---
+++ packages/hfutils/hfutils-0.6.0-py3-none-any.whl/hfutils/repository/__init__.py
@@ -0,0 +1,8 @@
+"""
+Overview:
+    Some git-based operations.
+"""
+from .base import hf_hub_repo_url
+from .clone import hf_hub_clone
+from .rollback import hf_hub_rollback
+from .url import hf_hub_repo_path_url, hf_hub_repo_file_url
