66b821a9e7a7f5b7891eb63921385f220a161b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:17 +0000

    Add package plutous-trade-crypto/0.0.83/plutous_trade_crypto-0.0.83-py3-none-any.whl

---
+++ packages/plutous-trade-crypto/plutous_trade_crypto-0.0.83-py3-none-any.whl/plutous/trade/crypto/exchanges/bitmex.py
@@ -0,0 +1,25 @@
+from ccxt.pro import bitmex
+
+from plutous.trade.crypto.utils.paginate import paginate
+
+
+class Bitmex(bitmex):
+    @paginate(
+        max_limit=1000,
+        end_time_arg="until",
+    )
+    async def fetch_ohlcv(
+        self,
+        symbol,
+        timeframe,
+        since=None,
+        limit=None,
+        params={},
+    ):
+        return await super().fetch_ohlcv(
+            symbol,
+            timeframe,
+            since,
+            limit,
+            params,
+        )
