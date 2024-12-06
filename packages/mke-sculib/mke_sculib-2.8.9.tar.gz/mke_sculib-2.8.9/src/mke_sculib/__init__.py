a8a525b983f460ecd3b74f5faf9e1279dabae8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:36 +0000

    Add package mke-sculib/2.8.9/mke_sculib-2.8.9.tar.gz

---
+++ packages/mke-sculib/mke_sculib-2.8.9.tar.gz/mke_sculib-2.8.9/src/mke_sculib/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '2.8.9'
+
+import mke_sculib.scu 
+from mke_sculib.scu import scu as scu_api
+from mke_sculib.scu import load as _load
+from mke_sculib.scu import plot_tt, print_color, colors, log, link_stellarium, activate_logging_mattermost
+from mke_sculib.sim import scu_sim
+from mke_sculib.stellarium_api import stellarium_api as stellar_api
+from mke_sculib.sim import plot_motion_pyplot as plot_motion
+from mke_sculib.helpers import get_utcnow, make_zulustr, parse_zulutime
+
+from astropy.time import Time
+import astropy.units as u
+from astropy.coordinates import EarthLocation, get_sun, AltAz, get_moon
+
+def load(antenna_id='', readonly=False, use_socket=True, debug=False, url_qry = 'http://10.98.76.45:8990/antennas', **kwargs):
+
+    if not "requests" in locals():
+        import requests
+    if not "json" in locals():
+        import json    
+
+    if antenna_id == 'test_antenna' or antenna_id == 'sim':
+        return scu_sim(str(antenna_id), debug=debug, **kwargs)
+    else:
+        return _load(antenna_id, readonly=readonly, use_socket=use_socket, debug=debug, url_qry=url_qry, **kwargs)
