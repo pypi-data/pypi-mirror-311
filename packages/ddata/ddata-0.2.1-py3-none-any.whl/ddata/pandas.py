b3723973c96d93403a1a071ccba2c36356ec8f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:10 +0000

    Add package ddata/0.2.1/ddata-0.2.1-py3-none-any.whl

---
+++ packages/ddata/ddata-0.2.1-py3-none-any.whl/ddata/pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# 显示所有列
+pd.set_option('display.max_columns', None)
+# 显示所有行
+pd.set_option('display.max_rows', None)
+# 显示宽度
+pd.set_option('display.width', 5000)
+# 设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth', 100)
+
+
+class Pandas():
+    def __init__(self):
+        pass
+
+    def get_pandas(self):
+        return pd
+
