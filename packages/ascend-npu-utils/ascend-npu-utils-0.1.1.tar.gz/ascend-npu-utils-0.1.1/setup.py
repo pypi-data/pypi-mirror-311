ce6dd771117cd514eba7306ff556bed2e2c23131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:24 +0000

    Add package ascend-npu-utils/0.1.1/ascend-npu-utils-0.1.1.tar.gz

---
+++ packages/ascend-npu-utils/ascend-npu-utils-0.1.1.tar.gz/ascend-npu-utils-0.1.1/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ascend-npu-utils',  # Name of your package
+    version='0.1.1',  # Initial version number
+    author='Harsh',  # Your name
+    description='A utility library for interacting with Ascend NPUs via the npu-smi command options, to directly get information from the terminal commands parsed into python',  # Brief description of your package
+    long_description=open('README.md').read(),  # Detailed description read from README.md
+    long_description_content_type='text/markdown',  # Format of the long description
+    packages=find_packages(),  # Automatically find packages in your project
+    install_requires=[  # List of dependencies required for your package
+        'psutil',  # Dependency for system and process utilities
+    ],
+    classifiers=[  # Metadata to categorize your package
+        'Programming Language :: Python :: 3',  # Supported Python version
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',  # OS compatibility
+    ],
+    python_requires='>=3.6',  # Minimum required Python version
+)
