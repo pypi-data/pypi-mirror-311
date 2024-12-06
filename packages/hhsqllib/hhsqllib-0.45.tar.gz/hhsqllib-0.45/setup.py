6b6c73d086b38c10987038cc52d2e85c5c2b11d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:15 +0000

    Add package hhsqllib/0.45/hhsqllib-0.45.tar.gz

---
+++ packages/hhsqllib/hhsqllib-0.45.tar.gz/hhsqllib-0.45/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name="hhsqllib",
+	  version="0.45",
+	  packages=find_packages(),
+	  install_requires=[
+'pandas==2.2.2',
+'SQLAlchemy==2.0.30',
+	  ],
+	  author="hh",
+	  author_email="hehuang0717@outlook.com",
+	  description="sqllib",
+	  long_description=open('README.md').read(),
+	  long_description_content_type="text/markdown",
+	  url="https://your.project.url",
+	  classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
+				   "Operating System :: OS Independent", ],
+	  python_requires='>=3.9', )
