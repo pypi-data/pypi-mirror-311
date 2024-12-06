bc6e0286ef40c42b09ae9361b32c52a9746b2711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:36 +0000

    Add package Mensajes-Luis-David/5.0/mensajes_luis_david-5.0.tar.gz

---
+++ packages/Mensajes-Luis-David/mensajes_luis_david-5.0.tar.gz/mensajes_luis_david-5.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+#Definimos el distribuible y su configuración
+setup(
+	name='Mensajes-Luis_David',
+	version='5.0',
+	description='Un paquete para saludar y despedir',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	author='Luis David López-Roberts Luzón',
+	author_email='luisdavid.lrlmp@gmail.com',
+	url='https://noesmidireccion.com',	
+	license_files=['LICENSE'],
+	packages=find_packages(),
+	scripts=[],	
+	test_suite='tests',
+	install_requires=[paquete.strip() 
+		for paquete in open("requirements.txt").readlines()],
+	classifiers=[
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.12',
+		'Topic :: Utilities'
+	]
+)
