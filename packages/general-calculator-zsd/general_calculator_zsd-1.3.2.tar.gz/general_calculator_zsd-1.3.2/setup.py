6b0b80126c8b3c9a86be9d81eabf15e9e9572914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:38 +0000

    Add package general-calculator-zsd/1.3.2/general_calculator_zsd-1.3.2.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.3.2.tar.gz/general_calculator_zsd-1.3.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.3.2',
+    author='colin zhang',
+    author_email='zsd0830@163.com',
+    description='通用脚本及数据导入脚本，不定期按需更新。绍东添加文件',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
