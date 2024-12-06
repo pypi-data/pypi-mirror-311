09d1e5928e13f3cc891fc7349ff47297974badb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:30 +0000

    Add package textual-tree-sitter-languages/0.0.0a0/textual_tree_sitter_languages-0.0.0a0.tar.gz

---
+++ packages/textual-tree-sitter-languages/textual_tree_sitter_languages-0.0.0a0.tar.gz/textual_tree_sitter_languages-0.0.0a0/setup.py
@@ -0,0 +1,42 @@
+import pathlib
+import re
+import setuptools
+
+from Cython.Build import cythonize
+
+init = (pathlib.Path("tree_sitter_languages") / "__init__.py").read_text()
+match = re.search(r"^__version__ = \"(.+)\"$", init, re.MULTILINE)
+version = match.group(1)
+
+with open("README.rst") as reader:
+    readme = reader.read()
+
+setuptools.setup(
+    name="textual_tree_sitter_languages",
+    version=version,
+    description="Binary Python wheels for all tree sitter languages.",
+    long_description=readme,
+    author="Textualize",
+    author_email="will@textualize.io",
+    url="https://github.com/Textualize/py-tree-sitter-languages",
+    license="Apache 2.0",
+    ext_modules=cythonize("tree_sitter_languages/core.pyx", language_level="3"),
+    packages=["tree_sitter_languages"],
+    package_data={"tree_sitter_languages": ["languages.so", "languages.dll"]},
+    install_requires=["tree-sitter<0.22.0"],
+    python_requires=">=3.8.1",
+    project_urls={
+        "Documentation": "https://github.com/Textualize/py-tree-sitter-languages",
+        "Source": "https://github.com/Textualize/py-tree-sitter-languages",
+        "Tracker": "https://github.com/Textualize/py-tree-sitter-languages/issues",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
---
+++ packages/textual-tree-sitter-languages/textual_tree_sitter_languages-0.0.0a0.tar.gz/textual_tree_sitter_languages-0.0.0a0/tests/test_tree_sitter_languages.py
@@ -0,0 +1,91 @@
+from tree_sitter_languages import get_language, get_parser
+
+LANGUAGES = [
+    "bash",
+    "c",
+    "c_sharp",  # c-sharp
+    "commonlisp",
+    "cpp",
+    "css",
+    "dockerfile",
+    "dot",
+    "elisp",
+    "elixir",
+    "elm",
+    "embedded_template",  # embedded-template
+    "erlang",
+    "fixed_form_fortran",
+    "fortran",
+    "go",
+    "gomod",  # go-mod
+    "hack",
+    "haskell",
+    "hcl",
+    "html",
+    "java",
+    "javascript",
+    "jsdoc",
+    "json",
+    "julia",
+    "kotlin",
+    "lua",
+    "make",
+    "markdown",
+    "objc",
+    "ocaml",
+    "perl",
+    "php",
+    "python",
+    "ql",
+    "r",
+    "regex",
+    "rst",
+    "ruby",
+    "rust",
+    "scala",
+    "sql",
+    "sqlite",
+    "toml",
+    "tsq",
+    "typescript",
+    "yaml",
+]
+
+PYTHON_CODE = """
+    def foo():
+        if bar:
+            baz()
+""".encode()
+
+PYTHON_QUERY = """
+(function_definition
+  name: (identifier) @function.def)
+
+(call
+  function: (identifier) @function.call)
+"""
+
+
+def test_python():
+    language = get_language("python")
+    query = language.query(PYTHON_QUERY)
+    parser = get_parser("python")
+    tree = parser.parse(PYTHON_CODE)
+    captures = query.captures(tree.root_node)
+    assert len(captures) == 2
+    assert captures[0][1] == "function.def"
+    assert captures[0][0].text.decode() == "foo"
+    assert captures[1][1] == "function.call"
+    assert captures[1][0].text.decode() == "baz"
+
+
+def test_get_parser():
+    for language in LANGUAGES:
+        parser = get_parser(language)
+        assert parser
+
+
+def test_get_language():
+    for language in LANGUAGES:
+        language = get_language(language)
+        assert language
