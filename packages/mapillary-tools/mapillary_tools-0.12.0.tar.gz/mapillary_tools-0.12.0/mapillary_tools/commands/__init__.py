a9ce1000feb715ff2b618803c94e5995f2bd117a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:51 +0000

    Add package mapillary-tools/0.12.0/mapillary_tools-0.12.0.tar.gz

---
+++ packages/mapillary-tools/mapillary_tools-0.12.0.tar.gz/mapillary_tools-0.12.0/mapillary_tools/commands/__init__.py
@@ -0,0 +1,11 @@
+# ruff: noqa: F401
+from . import (
+    authenticate,
+    process,
+    process_and_upload,
+    sample_video,
+    upload,
+    upload_camm,
+    video_process,
+    video_process_and_upload,
+)
