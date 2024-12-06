55a17c7c04393ea6dd6524447f77848866beb2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:10 +0000

    Add package tangled-agent/1.2.1/tangled_agent-1.2.1-py3-none-any.whl

---
+++ packages/tangled-agent/tangled_agent-1.2.1-py3-none-any.whl/tangled_agent/__init__.py
@@ -0,0 +1,5 @@
+from tangled_game_engine import Game, GameAgentBase
+from .random_agent import RandomRandyAgent
+from .remote_game_player import RemoteGamePlayer
+from .basic_game import play_local_game, play_remote_game
+from .get_builtin_agents import get_builtin_agents, create_agent
