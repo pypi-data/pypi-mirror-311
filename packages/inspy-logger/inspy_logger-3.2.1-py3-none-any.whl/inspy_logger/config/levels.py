2953fbfff37ce90c0c15ae3492824aa7c1c958b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:37 +0000

    Add package inspy-logger/3.2.1/inspy_logger-3.2.1-py3-none-any.whl

---
+++ packages/inspy-logger/inspy_logger-3.2.1-py3-none-any.whl/inspy_logger/config/levels.py
@@ -0,0 +1,29 @@
+"""
+
+
+Author: 
+    Inspyre Softworks
+
+Project:
+    inSPy-Logger
+
+File: 
+    inspy_logger/config/levels.py
+ 
+
+Description:
+    
+
+"""
+import logging
+
+
+LEVEL_MAP = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+    'fatal': logging.FATAL,
+}
+"""A mapping of level names to their corresponding logging levels."""
