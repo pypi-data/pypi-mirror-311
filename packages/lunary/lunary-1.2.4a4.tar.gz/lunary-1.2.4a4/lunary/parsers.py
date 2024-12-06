6581e9ce1701d208e7cf195ef35ee7d006cfe0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:20 +0000

    Add package lunary/1.2.4a4/lunary-1.2.4a4.tar.gz

---
+++ packages/lunary/lunary-1.2.4a4.tar.gz/lunary-1.2.4a4/lunary/parsers.py
@@ -0,0 +1,69 @@
+from typing import Any, Dict
+
+def default_input_parser(*args, **kwargs):
+    def serialize(args, kwargs):
+        if not args and not kwargs:
+            return None
+
+        if len(args) == 1 and not kwargs:
+            return args[0]
+
+        input = list(args)
+        if kwargs:
+            input.append(kwargs)
+
+        return input
+
+    return {"input": serialize(args, kwargs)}
+
+def method_input_parser(*args, **kwargs):
+    def serialize(args, kwargs):
+        args = args[1:]
+
+        if not args and not kwargs:
+            return None
+
+        if len(args) == 1 and not kwargs:
+            return args[0]
+
+        input_list = list(args)
+        if kwargs:
+            input_list.append(kwargs)
+
+        return input_list
+
+    return {"input": serialize(args, kwargs)}
+
+
+def default_output_parser(output, *args, **kwargs):
+    return {"output": getattr(output, "content", output), "tokensUsage": None}
+
+
+PARAMS_TO_CAPTURE = [
+  "frequency_penalty",
+  "function_call",
+  "functions",
+  "logit_bias",
+  "logprobs",
+  "max_tokens",
+  "n",
+  "presence_penalty",
+  "response_format",
+  "seed",
+  "stop",
+  "temperature",
+  "tool_choice",
+  "tools",
+  "top_logprobs",
+  "top_p",        
+  # Additional params
+  "extra_headers",
+  "extra_query",
+  "extra_body",
+  "timeout"
+]
+
+def filter_params(params: Dict[str, Any]) -> Dict[str, Any]:
+    filtered_params = {key: value for key, value in params.items() if key in PARAMS_TO_CAPTURE}
+    return filtered_params
+
