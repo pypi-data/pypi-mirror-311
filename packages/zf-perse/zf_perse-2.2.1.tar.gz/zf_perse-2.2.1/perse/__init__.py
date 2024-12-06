0ab25594bc54dc734e2147f86861e5ac4bc53ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:43 +0000

    Add package zf-perse/2.2.1/zf_perse-2.2.1.tar.gz

---
+++ packages/zf-perse/zf_perse-2.2.1.tar.gz/zf_perse-2.2.1/perse/__init__.py
@@ -0,0 +1,19 @@
+from .perse import (
+    reduce_html_content,
+    extract_content_tag,
+    extract_relevant_fields,
+    build_pydantic_model,
+    extract_json_fields,
+    perse,
+    perses,
+)
+
+__all__ = [
+    "reduce_html_content",
+    "extract_content_tag",
+    "extract_relevant_fields",
+    "build_pydantic_model",
+    "extract_json_fields",
+    "perse",
+    "perses",
+]
