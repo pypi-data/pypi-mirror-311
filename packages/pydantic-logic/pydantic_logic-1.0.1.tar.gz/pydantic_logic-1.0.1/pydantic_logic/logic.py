584e6b069ee3ac246d0485175d0dad5154802a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:25 +0000

    Add package pydantic-logic/1.0.1/pydantic_logic-1.0.1.tar.gz

---
+++ packages/pydantic-logic/pydantic_logic-1.0.1.tar.gz/pydantic_logic-1.0.1/pydantic_logic/logic.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+from pydantic_logic.expression import LogicExpression
+
+
+class Logic(BaseModel):
+    expressions: list[LogicExpression]
+
+    def evaluate(self, data: dict[str, Any], *, default_if_empty: bool = False) -> bool:
+        if len(self.expressions) == 0:
+            return default_if_empty
+        return all(expr.evaluate(data) for expr in self.expressions)
