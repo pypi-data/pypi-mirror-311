84a2819890cc63797caac11433e142a22ea06081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:57 +0000

    Add package uploadserver/6.0.0/uploadserver-6.0.0.tar.gz

---
+++ packages/uploadserver/uploadserver-6.0.0.tar.gz/uploadserver-6.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='uploadserver',
+    version='6.0.0',
+    author='Densaugeo',
+    author_email='author@example.com',
+    description='Python\'s http.server extended to include a file upload page',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Densaugeo/uploadserver',
+    packages=['uploadserver'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    entry_points = {
+        'console_scripts': ['uploadserver=uploadserver:main'],
+    }
+)
