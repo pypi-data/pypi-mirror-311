fb0d6ea7c0f8b3bb3cb2a3ab4a3adae0ac902f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:59 +0000

    Add package buco-db-controller/1.0.6/buco_db_controller-1.0.6.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-1.0.6.tar.gz/buco_db_controller-1.0.6/buco_db_controller/services/team_ratings_service.py
@@ -0,0 +1,18 @@
+from buco_db_controller.models.team_ratings import TeamRatings
+from buco_db_controller.repositories.team_ratings_repository import TeamRatingsRepository
+from buco_db_controller.utils import mappers
+
+
+class TeamRatingsService:
+    def __init__(self):
+        self.team_ratings_repository = TeamRatingsRepository()
+
+    def upsert_many_team_ratings(self, team_ratings: list):
+        self.team_ratings_repository.upsert_many_team_ratings(team_ratings)
+
+    def insert_team_ratings(self, team_ratings: dict):
+        self.team_ratings_repository.insert_team_ratings(team_ratings)
+
+    def get_team_ratings(self, season: int) -> TeamRatings:
+        response = self.team_ratings_repository.get_team_ratings(season)
+        return TeamRatings.from_dict(response)
