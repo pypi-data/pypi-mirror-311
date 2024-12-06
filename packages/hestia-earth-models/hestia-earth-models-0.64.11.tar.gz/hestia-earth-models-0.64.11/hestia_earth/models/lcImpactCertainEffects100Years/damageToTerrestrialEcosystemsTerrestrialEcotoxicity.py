b20a3a5bf67267ca987e4f8a41b07d61bf3b3a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:53 +0000

    Add package hestia-earth-models/0.64.11/hestia-earth-models-0.64.11.tar.gz

---
+++ packages/hestia-earth-models/hestia-earth-models-0.64.11.tar.gz/hestia-earth-models-0.64.11/hestia_earth/models/lcImpactCertainEffects100Years/damageToTerrestrialEcosystemsTerrestrialEcotoxicity.py
@@ -0,0 +1,34 @@
+from hestia_earth.models.utils.indicator import _new_indicator
+from hestia_earth.models.utils.pesticideAI import impact_lookup_value, impact_value_set_none
+from . import MODEL
+
+REQUIREMENTS = {
+    "ImpactAssessment": {
+        "cycle": {
+            "@type": "Cycle",
+            "completeness.pesticideVeterinaryDrug": "True",
+            "inputs": [{"@type": "Input", "value": "", "term.termType": "pesticideAI"}]
+        }
+    }
+}
+RETURNS = {
+    "Indicator": {
+        "value": ""
+    }
+}
+LOOKUPS = {
+    "pesticideAI": "pdfYear100YearsTerrestrialEcotoxicityDamageToTerrestrialEcosystemsLCImpact"
+}
+TERM_ID = 'damageToTerrestrialEcosystemsTerrestrialEcotoxicity'
+
+
+def _indicator(value: float):
+    indicator = _new_indicator(TERM_ID, MODEL)
+    indicator['value'] = value
+    return indicator
+
+
+def run(impact_assessment: dict):
+    value = impact_lookup_value(MODEL, TERM_ID, impact_assessment, LOOKUPS['pesticideAI'])
+    should_run = any([value is not None, impact_value_set_none(impact_assessment)])
+    return _indicator(value) if should_run else None
---
+++ packages/hestia-earth-models/hestia-earth-models-0.64.11.tar.gz/hestia-earth-models-0.64.11/tests/models/pooreNemecek2018/test_no3ToGroundwaterInorganicFertiliser.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+import json
+from tests.utils import fixtures_path, fake_new_emission, FLOODED_RICE_TERMS
+
+from hestia_earth.models.pooreNemecek2018.no3ToGroundwaterInorganicFertiliser import MODEL, TERM_ID, run
+
+class_path = f"hestia_earth.models.{MODEL}.{TERM_ID}"
+fixtures_folder = f"{fixtures_path}/{MODEL}/{TERM_ID}"
+
+
+@patch(f"hestia_earth.models.{MODEL}.no3ToGroundwaterSoilFlux.get_rice_paddy_terms", return_value=FLOODED_RICE_TERMS)
+@patch(f"hestia_earth.models.{MODEL}.no3ToGroundwaterSoilFlux.get_max_rooting_depth", return_value=0.9)
+@patch(f"{class_path}._new_emission", side_effect=fake_new_emission)
+def test_run(*args):
+    with open(f"{fixtures_folder}/cycle.jsonld", encoding='utf-8') as f:
+        cycle = json.load(f)
+
+    with open(f"{fixtures_folder}/result.jsonld", encoding='utf-8') as f:
+        expected = json.load(f)
+
+    value = run(cycle)
+    assert value == expected
