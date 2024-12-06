c6848bdafd4d50fe12b2944f2bec17a863f1c7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:27 +0000

    Add package core-universal4/4.24.2/core_universal4-4.24.2-py2.py3-none-macosx_10_7_x86_64.whl

---
+++ packages/core-universal4/core_universal4-4.24.2-py2.py3-none-macosx_10_7_x86_64.whl/applitools/core_universal/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+
+def _version(package_name):
+    try:  # assume python>=3.9
+        from importlib.metadata import version
+
+        return version(package_name)
+    except ImportError:  # older python
+        from pkg_resources import get_distribution
+
+        return get_distribution(package_name).version
+
+
+__version__ = _version("core-universal4")
+
+
+def get_instance():
+    from . import instance
+
+    return instance.instance
+
+
+__all__ = ("get_instance",)
