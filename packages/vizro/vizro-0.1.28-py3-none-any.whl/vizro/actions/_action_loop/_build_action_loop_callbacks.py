0b870644c1984ee186bc70dabd5fa7b3018a37fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:19 +0000

    Add package vizro/0.1.28/vizro-0.1.28-py3-none-any.whl

---
+++ packages/vizro/vizro-0.1.28-py3-none-any.whl/vizro/actions/_action_loop/_build_action_loop_callbacks.py
@@ -0,0 +1,78 @@
+"""Contains utilities to create required dash callbacks for the action loop."""
+
+import logging
+
+from dash import ClientsideFunction, Input, Output, State, clientside_callback
+
+from vizro.actions._action_loop._action_loop_utils import (
+    _get_actions_chains_on_all_pages,
+    _get_actions_on_registered_pages,
+)
+from vizro.managers import model_manager
+from vizro.managers._model_manager import ModelID
+
+logger = logging.getLogger(__name__)
+
+
+def _build_action_loop_callbacks() -> None:
+    """Creates all required dash callbacks for the action loop."""
+    actions_chains = _get_actions_chains_on_all_pages()
+    actions = _get_actions_on_registered_pages()
+
+    if not actions_chains:
+        return
+
+    gateway_inputs: list[Input] = []
+    for actions_chain in actions_chains:
+        # Recalculating the trigger component id to use the underlying callable object as a trigger component if needed.
+        actions_chain_trigger_component_id = actions_chain.trigger.component_id
+        try:
+            actions_chain_trigger_component = model_manager[ModelID(str(actions_chain_trigger_component_id))]
+            # Use underlying callable object as a trigger component.
+            if hasattr(actions_chain_trigger_component, "_input_component_id"):
+                actions_chain_trigger_component_id = actions_chain_trigger_component._input_component_id
+        # Not all action_chain_trigger_components are included in model_manager e.g. on_page_load_action_trigger
+        except KeyError:
+            pass
+
+        # Callback that enables gateway callback to work in the multiple page app
+        clientside_callback(
+            ClientsideFunction(namespace="build_action_loop_callbacks", function_name="trigger_to_global_store"),
+            Output({"type": "gateway_input", "trigger_id": actions_chain.id}, "data"),
+            Input(
+                component_id=actions_chain_trigger_component_id,
+                component_property=actions_chain.trigger.component_property,
+            ),
+            State({"type": "gateway_input", "trigger_id": actions_chain.id}, "data"),
+            prevent_initial_call=True,
+        )
+
+        gateway_inputs.append(
+            Input(
+                component_id={"type": "gateway_input", "trigger_id": actions_chain.id},
+                component_property="data",
+            )
+        )
+
+    # Determines the final sequence of actions to be triggered.
+    clientside_callback(
+        ClientsideFunction(namespace="build_action_loop_callbacks", function_name="gateway"),
+        output=[Output("remaining_actions", "data")]
+        + [Output({"type": "action_trigger", "action_name": action.id}, "data") for action in actions],
+        inputs=[
+            State("remaining_actions", "data"),
+            State("trigger_to_actions_chain_mapper", "data"),
+            State("action_trigger_actions_id", "data"),
+            Input("cycle_breaker_div", "n_clicks"),
+            *gateway_inputs,
+        ],
+        prevent_initial_call=True,
+    )
+
+    # Callback that triggers the next iteration
+    clientside_callback(
+        ClientsideFunction(namespace="build_action_loop_callbacks", function_name="after_action_cycle_breaker"),
+        Output("cycle_breaker_empty_output_store", "data"),
+        Input("action_finished", "data"),
+        prevent_initial_call=True,
+    )
---
+++ packages/vizro/vizro-0.1.28-py3-none-any.whl/vizro/actions/_callback_mapping/_callback_mapping_utils.py
@@ -0,0 +1,147 @@
+"""Contains utilities to create the action_callback_mapping."""
+
+from typing import Any, Callable, Union
+
+from dash import Output, State, dcc
+
+from vizro.actions import _parameter, filter_interaction
+from vizro.managers import model_manager
+from vizro.managers._model_manager import ModelID
+from vizro.models import Action, Page
+from vizro.models._controls import Filter, Parameter
+from vizro.models.types import ControlType
+
+
+# This function can also be reused for all other inputs (filters, parameters).
+# Potentially this could be a way to reconcile predefined with custom actions,
+# and make that predefined actions see and add into account custom actions.
+def _get_matching_actions_by_function(
+    page_id: ModelID, action_function: Callable[[Any], dict[str, Any]]
+) -> list[Action]:
+    """Gets list of `Actions` on triggered `Page` that match the provided `action_function`."""
+    return [
+        action
+        for actions_chain in model_manager._get_page_actions_chains(page_id=page_id)
+        for action in actions_chain.actions
+        if action.function._function == action_function
+    ]
+
+
+# CALLBACK STATES --------------
+def _get_inputs_of_controls(page: Page, control_type: ControlType) -> list[State]:
+    """Gets list of `States` for selected `control_type` of triggered `Page`."""
+    return [
+        State(component_id=control.selector.id, component_property=control.selector._input_property)
+        for control in page.controls
+        if isinstance(control, control_type)
+    ]
+
+
+def _get_inputs_of_figure_interactions(
+    page: Page, action_function: Callable[[Any], dict[str, Any]]
+) -> list[dict[str, State]]:
+    """Gets list of `States` for selected chart interaction `action_function` of triggered `Page`."""
+    figure_interactions_on_page = _get_matching_actions_by_function(
+        page_id=ModelID(str(page.id)), action_function=action_function
+    )
+    inputs = []
+    for action in figure_interactions_on_page:
+        triggered_model = model_manager._get_action_trigger(action_id=ModelID(str(action.id)))
+        required_attributes = ["_filter_interaction_input", "_filter_interaction"]
+        for attribute in required_attributes:
+            if not hasattr(triggered_model, attribute):
+                raise ValueError(f"Model {triggered_model.id} does not have required attribute `{attribute}`.")
+        if "modelID" not in triggered_model._filter_interaction_input:
+            raise ValueError(
+                f"Model {triggered_model.id} does not have required State `modelID` in `_filter_interaction_input`."
+            )
+        inputs.append(triggered_model._filter_interaction_input)
+    return inputs
+
+
+# TODO: Refactor this and util functions once we implement "_get_input_property" method in VizroBaseModel models
+def _get_action_callback_inputs(action_id: ModelID) -> dict[str, list[Union[State, dict[str, State]]]]:
+    """Creates mapping of pre-defined action names and a list of `States`."""
+    page: Page = model_manager[model_manager._get_model_page_id(model_id=action_id)]
+
+    action_input_mapping = {
+        "filters": _get_inputs_of_controls(page=page, control_type=Filter),
+        "parameters": _get_inputs_of_controls(page=page, control_type=Parameter),
+        # TODO: Probably need to adjust other inputs to follow the same structure list[dict[str, State]]
+        "filter_interaction": _get_inputs_of_figure_interactions(
+            page=page, action_function=filter_interaction.__wrapped__
+        ),
+    }
+    return action_input_mapping
+
+
+# CALLBACK OUTPUTS --------------
+def _get_action_callback_outputs(action_id: ModelID) -> dict[str, Output]:
+    """Creates mapping of target names and their `Output`."""
+    action_function = model_manager[action_id].function._function
+
+    # The right solution for mypy here is to not e.g. define new attributes on the base but instead to get mypy to
+    # recognize that model_manager[action_id] is of type Action and hence has the function attribute.
+    # Ideally model_manager.__getitem__ would handle this itself, possibly with suitable use of a cast.
+    # If not then we can do the cast to Action at the point of consumption here to avoid needing mypy ignores.
+
+    try:
+        targets = model_manager[action_id].function["targets"]
+    except KeyError:
+        targets = []
+
+    if action_function == _parameter.__wrapped__:
+        targets = [target.split(".")[0] for target in targets]
+
+    return {
+        target: Output(
+            component_id=target,
+            component_property=model_manager[target]._output_component_property,
+            allow_duplicate=True,
+        )
+        for target in targets
+    }
+
+
+def _get_export_data_callback_outputs(action_id: ModelID) -> dict[str, Output]:
+    """Gets mapping of relevant output target name and `Outputs` for `export_data` action."""
+    action = model_manager[action_id]
+
+    try:
+        targets = action.function["targets"]
+    except KeyError:
+        targets = None
+
+    if not targets:
+        targets = model_manager._get_page_model_ids_with_figure(
+            page_id=model_manager._get_model_page_id(model_id=action_id)
+        )
+
+    return {
+        f"download_dataframe_{target}": Output(
+            component_id={"type": "download_dataframe", "action_id": action_id, "target_id": target},
+            component_property="data",
+        )
+        for target in targets
+    }
+
+
+# CALLBACK COMPONENTS --------------
+def _get_export_data_callback_components(action_id: ModelID) -> list[dcc.Download]:
+    """Creates dcc.Downloads for target components of the `export_data` action."""
+    action = model_manager[action_id]
+
+    try:
+        targets = action.function["targets"]
+    except KeyError:
+        targets = None
+
+    if not targets:
+        targets = model_manager._get_page_model_ids_with_figure(
+            page_id=model_manager._get_model_page_id(model_id=action_id)
+        )
+
+    return [
+        dcc.Download(id={"type": "download_dataframe", "action_id": action_id, "target_id": target})
+        for target in targets
+    ]
