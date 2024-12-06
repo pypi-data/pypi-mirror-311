ef8f63bfe7d59bed5968ab594cb85d8a1d3a3ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:01 +0000

    Add package matrax/0.0.5/matrax-0.0.5.tar.gz

---
+++ packages/matrax/matrax-0.0.5.tar.gz/matrax-0.0.5/README.md
@@ -0,0 +1,121 @@
+<p align="center">
+    <a href="docs/img/matrax_logo.png">
+        <img src="docs/img/matrax_logo.png" alt="Matrax logo" width="50%"/>
+    </a>
+</p>
+
+<h2 align="center">
+    <p>Matrix Games in JAX</p>
+</h2>
+<p align="center">
+    <a href="https://www.python.org/doc/versions/">
+      <img src="https://img.shields.io/badge/python-3.9-blue" alt="Python Versions">
+    </a>
+    <a href="https://badge.fury.io/py/matrax">
+      <img src="https://badge.fury.io/py/matrax.svg" alt="PyPI version" height="18">
+    </a>
+    <a href="https://github.com/instadeepai/matrax/actions/workflows/tests_linters.yml">
+      <img src="https://github.com/instadeepai/matrax/actions/workflows/tests_linters.yml/badge.svg" alt="Tests">
+    </a>
+    <a href="https://github.com/psf/black">
+      <img src="https://img.shields.io/badge/code%20style-black-000000.svg" alt="Code Style">
+    </a>
+    <a href="http://mypy-lang.org/">
+      <img src="http://www.mypy-lang.org/static/mypy_badge.svg" alt="MyPy">
+    </a>
+    <a href="https://opensource.org/licenses/Apache-2.0">
+      <img src="https://img.shields.io/badge/License-Apache%202.0-orange.svg" alt="License">
+    </a>
+</p>
+
+## Enter the Matrax! 😎
+
+<div align="center">
+<h3>
+
+[**Installation**](#installation-) | [**Quickstart**](#quickstart-)
+
+</div>
+
+**Matrax** is a lightweight suite of [2-player matrix game](https://en.wikipedia.org/wiki/Normal-form_game) environments written in [JAX](https://github.com/google/jax). It is a direct re-implementation of the matrix games provided in [this repository](https://github.com/uoe-agents/matrix-games) from the [AARG](https://agents.inf.ed.ac.uk/). It follows the [Jumanji](https://github.com/instadeepai/jumanji) RL environment suite API developed by [InstaDeep](https://www.instadeep.com/).
+
+<h2 name="environments" id="environments">2-Player Matrix Games 🧑‍🤝‍🧑 </h2>
+
+| Category                              | Shape (action space) | Registered Version(s)                                | Source                                                                                           |
+|------------------------------------------|----------|------------------------------------------------------|--------------------------------------------------------------------------------------------------|
+| 🔻 Penalty Game                              | 3 x 3  | `Penalty-{k}-{state}-v0`                                        | [code](https://github.com/instadeepai/matrax/blob/main/matrax/games/penalty.py)   |
+| 🧗‍♀️ Climbing Game                              | 3 x 3  | `Climbing-{state}-v0`                                        | [code](https://github.com/instadeepai/matrax/blob/main/matrax/games/climbing.py)   |
+| 🤝 No-Conflict Games                              | 2 x 2  | `NoConflict-{id}-{state}-v0`                                   | [code](https://github.com/instadeepai/matrax/blob/main/matrax/games/no_conflict.py)   |
+| 💣 Conflict Games                        | 2 x 2    | `Conflict-{id}-{state}-v0`                                     | [code](https://github.com/instadeepai/matrax/blob/main/matrax/games/conflict.py) |
+
+<h2 name="install" id="install">Installation 🎬</h2>
+
+You can install the latest release of **Matrax** from PyPI:
+
+```bash
+pip install matrax
+```
+
+Alternatively, you can install the latest development version directly from GitHub:
+
+```bash
+pip install git+https://github.com/instadeepai/matrax.git
+```
+
+**Matrax** has been tested on Python 3.9.
+Note that because the installation of JAX differs depending on your hardware accelerator,
+we advise users to explicitly install the correct JAX version (see the
+[official installation guide](https://github.com/google/jax#installation)).
+
+<h2 name="quickstart" id="quickstart">Quickstart ⚡</h2>
+
+```python
+import jax
+import matrax
+
+# Instantiate a matrix game environment using the registry
+env = matrax.make("Penalty-25-stateless-v0")
+
+# Reset your (jit-able) environment
+key = jax.random.PRNGKey(0)
+state, timestep = jax.jit(env.reset)(key)
+
+# Interact with the (jit-able) environment
+action = env.action_spec.generate_value()          # Action selection (dummy value here)
+state, timestep = jax.jit(env.step)(state, action)   # Take a step and observe the next state and time step
+```
+
+### Registry and Versioning 📖
+
+Like Jumanji, **Matrax** keeps a strict versioning of its environments for reproducibility reasons.
+We maintain a registry of standard environments with their configuration.
+For each environment, a version suffix is appended, e.g. `EnvironmentName-v1`.
+When changes are made to environments that might impact learning results,
+the version number is incremented by one to prevent potential confusion.
+
+## Citing Matrax 📝
+
+If you use any of the environment implementations in Matrax please consider citing this library.
+
+```
+@software{matrax2023github,
+  author = {Arnu Pretorius},
+  title = {Matrax: Matrix Games in JAX},
+  url = {http://github.com/instadeepai/matrax},
+  version = {v0.0.4-alpha},
+  year = {2023},
+}
+```
+
+## See Also 🔎
+
+Other works have embraced the approach of writing RL environments in JAX.
+In particular, we suggest users check out the following sister repositories:
+
+- 🌴 [Jumanji](https://github.com/instadeepai/jumanji) is a diverse suite of scalable reinforcement learning environments.
+- 🦾 [Brax](https://github.com/google/brax) is a differentiable physics engine that simulates
+environments made up of rigid bodies, joints, and actuators.
+- 🏋️‍ [Gymnax](https://github.com/RobertTLange/gymnax) implements classic environments including
+classic control, bsuite, MinAtar and a collection of meta RL tasks.
+- 🎲 [Pgx](https://github.com/sotetsuk/pgx) provides classic board game environments like
+Backgammon, Shogi, and Go.
---
+++ packages/matrax/matrax-0.0.5.tar.gz/matrax-0.0.5/matrax/env.py
@@ -0,0 +1,201 @@
+# Copyright 2023 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import cached_property, partial
+from typing import Tuple
+
+import chex
+import jax
+import jax.numpy as jnp
+from jumanji import specs
+from jumanji.env import Environment
+from jumanji.types import TimeStep, restart, termination, transition
+
+from matrax.types import Observation, State
+
+
+class MatrixGame(Environment[State, specs.MultiDiscreteArray, Observation]):
+    """JAX implementation of the 2-player matrix game environment:
+    https://github.com/uoe-agents/matrix-games
+
+    A matrix game is a two-player game where each player has a set of actions and a payoff matrix.
+    The payoff matrix is a 2D array where the rows correspond to the actions of player 1 and the
+    columns correspond to the actions of player 2. The entry at row i and column j for player 1 is
+    the reward given to player 1 when playing action i and player 2 plays action j. Similarly, the
+    entry at row i and column j for player 2 is the reward given to player 2 when playing action j
+    and player 1 plays action i.
+
+    ```python
+    from matrax import MatrixGame
+    payoff_matrix = jnp.array([[1, -1], [-1, 1]])
+    env = MatrixGame(payoff_matrix)
+    key = jax.random.PRNGKey(0)
+    state, timestep = jax.jit(env.reset)(key)
+    action = env.action_spec.generate_value()
+    state, timestep = jax.jit(env.step)(state, action)
+    ```
+    """
+
+    def __init__(
+        self,
+        payoff_matrix: chex.Array,
+        keep_state: bool = True,
+        time_limit: int = 500,
+    ):
+        """Instantiates a `MatrixGame` environment.
+
+        Args:
+            payoff_matrix: a 2D array of shape (num_agents, num_agents) containing the payoff
+                matrix of the game.
+            keep_state: whether to keep state by giving agents the actions of all players in
+                the previous round as observations. Defaults to True.
+            time_limit: the maximum step limit allowed within the environment.
+                Defaults to 500.
+        """
+        self.payoff_matrix = payoff_matrix
+        self.keep_state = keep_state
+
+        self.num_agents = self.payoff_matrix.shape[0]
+        self.num_actions = self.payoff_matrix.shape[1]
+
+        self.time_limit = time_limit
+
+    def __repr__(self) -> str:
+        return f"MatrixGame(\n" f"\tpayoff_matrix={self.payoff_matrix!r},\n" ")"
+
+    def reset(self, key: chex.PRNGKey) -> Tuple[State, TimeStep[Observation]]:
+        """Resets the environment.
+
+        Args:
+            key: random key used to reset the environment since it is stochastic.
+
+        Returns:
+            state: State object corresponding to the new state of the environment.
+            timestep: TimeStep object corresponding the first timestep returned by the environment.
+        """
+        # create environment state
+        state = State(
+            step_count=jnp.array(0, int),
+            key=key,
+        )
+        dummy_actions = jnp.ones((self.num_agents,), int) * -1
+
+        # collect first observations and create timestep
+        agent_obs = jax.vmap(partial(self._make_agent_observation, dummy_actions))(
+            jnp.arange(self.num_agents)
+        )
+        observation = Observation(
+            agent_obs=agent_obs,
+            step_count=state.step_count,
+        )
+        timestep = restart(observation=observation, shape=self.num_agents)
+        return state, timestep
+
+    def step(
+        self,
+        state: State,
+        actions: chex.Array,
+    ) -> Tuple[State, TimeStep[Observation]]:
+        """Perform an environment step.
+
+        Args:
+            state: State object containing the dynamics of the environment.
+            actions: Array containing actions of each agent.
+
+        Returns:
+            state: State object corresponding to the next state of the environment.
+            timestep: TimeStep object corresponding the timestep returned by the environment.
+        """
+
+        def compute_reward(
+            actions: chex.Array, payoff_matrix_per_agent: chex.Array
+        ) -> chex.Array:
+            reward_idx = tuple(actions)
+            return payoff_matrix_per_agent[reward_idx].astype(float)
+
+        rewards = jax.vmap(partial(compute_reward, actions))(self.payoff_matrix)
+
+        # construct timestep and check environment termination
+        steps = state.step_count + 1
+        done = steps >= self.time_limit
+
+        # compute next observation
+        agent_obs = jax.vmap(partial(self._make_agent_observation, actions))(
+            jnp.arange(self.num_agents)
+        )
+        next_observation = Observation(
+            agent_obs=agent_obs,
+            step_count=steps,
+        )
+
+        timestep = jax.lax.cond(
+            done,
+            lambda: termination(
+                reward=rewards, observation=next_observation, shape=self.num_agents
+            ),
+            lambda: transition(
+                reward=rewards, observation=next_observation, shape=self.num_agents
+            ),
+        )
+
+        # create environment state
+        next_state = State(
+            step_count=steps,
+            key=state.key,
+        )
+        return next_state, timestep
+
+    def _make_agent_observation(
+        self,
+        actions: chex.Array,
+        agent_id: int,
+    ) -> chex.Array:
+        return jax.lax.cond(
+            self.keep_state,
+            lambda: actions,
+            lambda: jnp.zeros(self.num_agents, int),
+        )
+
+    @cached_property
+    def observation_spec(self) -> specs.Spec[Observation]:
+        """Specification of the observation of the MatrixGame environment.
+        Returns:
+            Spec for the `Observation`, consisting of the fields:
+                - agent_obs: BoundedArray (int32) of shape (num_agents, num_agents).
+                - step_count: BoundedArray (int32) of shape ().
+        """
+
+        obs_shape = (self.num_agents, self.num_agents)
+        low = jnp.zeros(obs_shape)
+        high = jnp.ones(obs_shape) * self.num_actions
+
+        agent_obs = specs.BoundedArray(obs_shape, jnp.int32, low, high, "agent_obs")
+        step_count = specs.BoundedArray((), jnp.int32, 0, self.time_limit, "step_count")
+        return specs.Spec(
+            Observation,
+            "ObservationSpec",
+            agent_obs=agent_obs,
+            step_count=step_count,
+        )
+
+    @cached_property
+    def action_spec(self) -> specs.MultiDiscreteArray:
+        """Returns the action spec.
+        Since this is a multi-agent environment, the environment expects an array of actions.
+        This array is of shape (num_agents,).
+        """
+        return specs.MultiDiscreteArray(
+            num_values=jnp.array([self.num_actions] * self.num_agents, jnp.int32),
+            name="action",
+        )
