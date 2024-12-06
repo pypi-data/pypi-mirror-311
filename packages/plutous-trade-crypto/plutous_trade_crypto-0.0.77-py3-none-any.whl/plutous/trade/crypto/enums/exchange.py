7241a8ab9e3de82ea950a7d8a01203eae9e26846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:38 +0000

    Add package plutous-trade-crypto/0.0.77/plutous_trade_crypto-0.0.77-py3-none-any.whl

---
+++ packages/plutous-trade-crypto/plutous_trade_crypto-0.0.77-py3-none-any.whl/plutous/trade/crypto/enums/exchange.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from plutous.trade.crypto.exchanges import (
+    Binance,
+    BinanceCoinm,
+    BinanceUsdm,
+    Bitget,
+    Bybit,
+    Coinbase,
+    GateIO,
+    Huobi,
+    Hyperliquid,
+    Kucoin,
+    KucoinFutures,
+    LBank,
+    Mexc,
+    Okx,
+    Phemex,
+    Woo,
+)
+
+
+class Exchange(Enum):
+    BINANCE = Binance
+    BINANCE_COINM = BinanceCoinm
+    BINANCE_USDM = BinanceUsdm
+    BITGET = Bitget
+    BYBIT = Bybit
+    COINBASE = Coinbase
+    GATEIO = GateIO
+    HUOBI = Huobi
+    HYPERLIQUID = Hyperliquid
+    KUCOIN = Kucoin
+    KUCOIN_FUTURES = KucoinFutures
+    OKX = Okx
+    PHEMEX = Phemex
+    WOO = Woo
+    MEXC = Mexc
+    LBANK = LBank
