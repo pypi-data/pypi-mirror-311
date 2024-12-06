c922a86a1c5ab2eb3cca29450a4468a87cf94f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:16 +0000

    Add package prefect/3.1.5.dev4/prefect-3.1.5.dev4.tar.gz

---
+++ packages/prefect/prefect-3.1.5.dev4.tar.gz/prefect-3.1.5.dev4/src/prefect/server/ui/assets/404-DCForAWp.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,k as a,o as t,de as r}from"./index-BFro5MYh.js";import{u as s}from"./usePageTitle-Bt2w5lgs.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(t(),o(a(r)))}});export{m as default};
+//# sourceMappingURL=404-DCForAWp.js.map
---
+++ packages/prefect/prefect-3.1.5.dev4.tar.gz/prefect-3.1.5.dev4/src/prefect/server/ui/assets/usePrefectApi-BCy89XZ9.js
@@ -0,0 +1,2 @@
+import{cH as e,cI as r}from"./index-BFro5MYh.js";function t(){return e(r)}export{t as u};
+//# sourceMappingURL=usePrefectApi-BCy89XZ9.js.map
