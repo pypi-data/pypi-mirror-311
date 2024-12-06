3cf57ae6d5c517802a8a5af0ffb65197377169db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:01 +0000

    Add package aryaxai/0.0.77/aryaxai-0.0.77.tar.gz

---
+++ packages/aryaxai/aryaxai-0.0.77.tar.gz/aryaxai-0.0.77/aryaxai/common/types.py
@@ -0,0 +1,46 @@
+from typing import List, Optional, TypedDict, Dict
+
+
+class ProjectConfig(TypedDict):
+    project_type: str
+    unique_identifier: str
+    true_label: str
+    tag: str
+    pred_label: Optional[str]
+    feature_exclude: Optional[List[str]]
+    drop_duplicate_uid: Optional[bool]
+    handle_errors: Optional[bool]
+    feature_encodings: Optional[dict]
+
+class DataConfig(TypedDict):
+    tags: List[str]
+    feature_exclude: List[str]
+    feature_encodings: Dict[str, str]
+    drop_duplicate_uid: bool
+    sample_percentage: float
+    explainability_sample_percentage: float
+    explainability_method: List[str]
+
+class SyntheticDataConfig(TypedDict):
+    model_name: str
+    tags: List[str]
+    feature_exclude: List[str]
+    feature_include: List[str]
+    feature_actual_used: List[str]
+    drop_duplicate_uid: bool
+
+
+class SyntheticModelHyperParams(TypedDict):
+    # GPT2 hyper params
+    batch_size: Optional[int]
+    early_stopping_patience: Optional[int]
+    early_stopping_threshold: Optional[float]
+    epochs: Optional[int]
+    model_type: Optional[str]
+    random_state: Optional[int]
+    tabular_config: Optional[str]
+    train_size: Optional[float]
+
+    # CTGAN hyper params
+    epochs: Optional[int]
+    test_ratio: Optional[float]
