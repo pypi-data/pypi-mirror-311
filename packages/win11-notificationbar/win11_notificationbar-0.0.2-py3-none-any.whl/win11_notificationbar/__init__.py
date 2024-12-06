aeafcaa690e6500452f450075bade41461e8a5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:04 +0000

    Add package win11-notificationbar/0.0.2/win11_notificationbar-0.0.2-py3-none-any.whl

---
+++ packages/win11-notificationbar/win11_notificationbar-0.0.2-py3-none-any.whl/win11_notificationbar/__init__.py
@@ -0,0 +1,31 @@
+__version__ = "0.0.2"
+__auther__ = "uf4"
+
+__all__ = [
+    "notify",
+    "clear_toast",
+    "update_progress",
+    "add_progress",
+    "available_recognizer_languages",
+    "add_audio",
+    "add_button",
+    "add_icon",
+    "add_image",
+    "add_input",
+    "add_selection",
+    "add_text",
+    "toast",
+    "activated_args",
+    "play_sound",
+    "recognize",
+    "speak",
+    "toast_async",
+    "result_wrapper",
+    "set_attribute",
+    "create_actions"
+]
+from packaging_tutorial.win11_notificationbar.win11_notificationbar import add_input, notify, clear_toast, \
+    update_progress, add_progress, available_recognizer_languages, add_audio, add_button, add_icon, add_image, add_text, \
+    add_selection, activated_args, toast, recognize, play_sound, speak, toast_async, result_wrapper, set_attribute, \
+    create_actions
+
