55092fa21ea15e3e727b31ced359d21f034ee8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:53 +0000

    Add package gffanalyzer_m1bioinfo/0.1.0/gffanalyzer_m1bioinfo-0.1.0-py3-none-any.whl

---
+++ packages/gffanalyzer_m1bioinfo/gffanalyzer_m1bioinfo-0.1.0-py3-none-any.whl/gffanalyzer_m1bioinfo/__init__.py
@@ -0,0 +1,23 @@
+from .annotation import Annotation
+
+def plot_and_stats(gff_file : str, stats : bool = False, plot_out :str = None):
+    '''
+    Retourne statistiques et un boxplot à partir d'un fichier GFF, 
+    '''
+    try:
+        # Charger les annotations depuis le fichier
+        annotation = Annotation(gff_file)
+        annotation._parse_gff(gff_file)  # Méthode pour charger les données du fichier
+    except Exception as e:
+        raise IOError(f"Erreur lors du chargement du fichier : {e}")
+    if stats:
+        annotation.stats()
+    elif plot_out:
+        try:
+            annotation.rna_lens(plot_out)  # Appel de la méthode rna_lens avec le chemin du fichier de sortie 
+            print(f"Boxplot sauvegardé sous : {plot_out}")
+        except Exception as e:
+            print(f"Erreur lors de la création du boxplot : {e}")
+    else: 
+        raise ValueError("Aucun argument appelé, ni stats, ni output")
+    
