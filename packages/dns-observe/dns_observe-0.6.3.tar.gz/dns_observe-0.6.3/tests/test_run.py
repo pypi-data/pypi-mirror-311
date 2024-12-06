8a7fe12e5099dc1c769d0fe933bea1172609d66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:47 +0000

    Add package dns-observe/0.6.3/dns_observe-0.6.3.tar.gz

---
+++ packages/dns-observe/dns_observe-0.6.3.tar.gz/dns_observe-0.6.3/tests/test_run.py
@@ -0,0 +1,28 @@
+import unittest
+from dns_observe import DNSQuery
+import os
+from pathlib import Path
+
+test_path = os.path.split(os.path.realpath(__file__))[0]
+
+domains_file = Path(test_path, "./domain_list.txt")
+
+class MyTest(unittest.TestCase):
+    def test_run(self):
+        with open(domains_file, 'r') as f:
+            domains = f.readlines()
+        for d in domains:
+            d = d.strip()
+            print(f"== {d} ==")
+            dns = DNSQuery(listen_time=1, timeout=1)
+            dns.query(d)
+    
+    def test_type_AAAA(self):
+        dns = DNSQuery('223.5.5.5')
+        domains = ['taobao.com', 'data.bilibili.com', 'www.qq.com']
+        for d in domains:
+            print(f"== {d} ==")
+            dns.query(d)
+
+if __name__ == '__main__':
+    unittest.main()            
