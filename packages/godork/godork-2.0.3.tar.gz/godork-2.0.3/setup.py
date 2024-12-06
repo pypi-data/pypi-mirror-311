f8e17636a68a53729b039d0c88c9a24a54e82000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:35 +0000

    Add package godork/2.0.3/godork-2.0.3.tar.gz

---
+++ packages/godork/godork-2.0.3.tar.gz/godork-2.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from v2.__init__ import __version__
+
+setup(
+    name='godork',
+    version=__version__,
+    author='Thunder (@thd3r)',
+    author_email='thd3r@proton.me',
+    description='Scrape Google search quickly',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'rich',
+        'pydub',
+        'aiohttp',
+        'asyncio',
+        'selenium',
+        'SpeechRecognition',
+        'webdriver-manager'
+    ],
+    entry_points={
+        'console_scripts': [
+            'godork = src.godork:main'
+        ]
+    },
+    license='MIT',
+    url='https://github.com/thd3r/godork',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['godork', 'google dorks', 'google dorking']
+)
