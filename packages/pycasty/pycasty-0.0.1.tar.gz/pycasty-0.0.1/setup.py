ba7c98f6fca97c0a12454890cde72dbb9588705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:04 +0000

    Add package pycasty/0.0.1/pycasty-0.0.1.tar.gz

---
+++ packages/pycasty/pycasty-0.0.1.tar.gz/pycasty-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Raycasting Engine using python'
+LONG_DESCRIPTION = 'A package that allows to render a 2D array map into a 3D explorable map, with the help of raycasting technique'
+
+# Setting up
+setup(
+    name="pycasty",
+    version=VERSION,
+    author="tempewda",
+    author_email="tempewda@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python', 'raycast', 'raycaster', 'pixelated', 'doom style', 'wolfenstein style'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
