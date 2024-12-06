bc7ddc389d2fb3dcc35b8a8f530ecebc07e55b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:27 +0000

    Add package koltrast/2.0.6/koltrast-2.0.6-py3-none-any.whl

---
+++ packages/koltrast/koltrast-2.0.6-py3-none-any.whl/koltrast/interval.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from pendulum import DateTime, Duration, now, instance, parse
+from croniter import croniter
+
+
+@dataclass
+class Interval:
+    since: DateTime
+    until: DateTime
+
+    def __init__(self, since: DateTime | str, until: DateTime | str):
+
+        if isinstance(since, str):
+            since=parse(since)
+
+        if isinstance(until, str):
+            until=parse(until)
+
+        if since >= until:
+            raise ValueError("`since` must be before `until`")
+
+        self.since = since
+        self.until = until
+
+    @property
+    def duration(self) -> Duration:
+        """Return the duration of the interval."""
+        return self.until - self.since
+
+    def overlaps_with(self, other_interval: Interval) -> bool:
+        """Check if two intervals overlap."""
+        return not (self.until <= other_interval.since or self.since >= other_interval.until)
+
+    def contains(self, moment: DateTime) -> bool:
+        """Check if a given moment falls within the interval."""
+        return self.since <= moment < self.until
+
+    def split(self, cron_expression: str) -> list[Interval]:
+        """
+        Split the current interval into smaller intervals based on a cron expression.
+        The cron expression defines the times when the interval should be split.
+
+        Args:
+            cron_expression (str): A cron-style expression for the split pattern (e.g., "0 */6 * * *").
+
+        Returns:
+            list[Interval]: A list of smaller intervals.
+        """
+        if not croniter.is_valid(expression=cron_expression):
+            raise Exception(f"{cron_expression} is not a valid cron expression")
+
+        cron = croniter(cron_expression, self.since)
+
+        intervals = []
+        while True:
+            this_time: DateTime = instance(cron.get_current(DateTime))
+            next_time: DateTime = instance(cron.get_next(DateTime))
+
+            if next_time > self.until:
+                break  # Break the loop when the next time exceeds 'until'
+
+            intervals.append(Interval(since=this_time, until=next_time))
+
+        return intervals
+
+
+def last_complete_interval(cron_expression: str, anchor: DateTime = now(), tz: str = 'UTC') -> Interval:
+    """
+    Calculate the last complete interval for a given cron expression.
+
+    Args:
+        cron_expression (str): A valid cron expression defining the desired schedule.
+        anchor (DateTime): The reference point for calculating intervals. Defaults to the current time.
+        tz (str): The timezone for interpreting the `anchor` and calculating the schedule. Defaults to 'UTC'.
+
+    Returns:
+        Interval: An object representing the last completed interval, with `since` and `until` attributes.
+
+    Raises:
+        Exception: If the provided `cron_expression` is invalid.
+    """
+    if not croniter.is_valid(expression=cron_expression):
+        raise Exception(f"{cron_expression} is not a valid cron expression")
+
+    cron = croniter(expr_format=cron_expression, start_time=anchor.in_timezone(tz))
+
+    return Interval(since=instance(cron.get_prev(DateTime)), until=instance(cron.get_next(DateTime)))
