42f8f2504253429e4e48400b054bce2fce95fab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:30 +0000

    Add package pproto-py/2.1.4a1/pproto_py-2.1.4a1-py3-none-any.whl

---
+++ packages/pproto-py/pproto_py-2.1.4a1-py3-none-any.whl/pproto_py/schemas/__init__.py
@@ -0,0 +1,15 @@
+from .message import Priority, Status, Type, BaseMessage, Compression, Serialize, FlagField, ByteField, FlagMessage
+from .exceptions import BasePprotoErrorContent
+
+__all__ = [
+    "Priority",
+    "Status",
+    "Type",
+    "BaseMessage",
+    "Compression",
+    "Serialize",
+    "FlagField",
+    "ByteField",
+    "FlagMessage",
+    "BasePprotoErrorContent",
+]
