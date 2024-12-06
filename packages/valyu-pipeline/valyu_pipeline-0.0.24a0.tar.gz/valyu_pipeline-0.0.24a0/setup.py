aba01a7f308195436a31b9aeee356f72863abcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:33 +0000

    Add package valyu-pipeline/0.0.24a0/valyu_pipeline-0.0.24a0.tar.gz

---
+++ packages/valyu-pipeline/valyu_pipeline-0.0.24a0.tar.gz/valyu_pipeline-0.0.24a0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="valyu-pipeline",
+    version="0.0.24a",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        "PyMuPDF",
+        "tqdm",
+        "Pillow",
+        "IPython",
+        "pydantic"
+    ],
+    author="Valyu.Network",
+    author_email="harvey@valyu.network",
+    description="A Python package for accessing the Valyu Data pipeline infrastructure",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.8",
+)
