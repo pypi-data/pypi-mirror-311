305c26a25e114cddca70d63e9c4cc9c2efe0d179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:43 +0000

    Add package asi-weather/2.2.8/asi_weather-2.2.8.tar.gz

---
+++ packages/asi-weather/asi_weather-2.2.8.tar.gz/asi_weather-2.2.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asi-weather",
+    version="2.2.8",
+    description="A Python package for a weather prediction project",
+    author="Andrei, Tagir, Yan",
+    author_email="s24238@pjwstk.edu.pl",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
