8b0718404ab9ec3fafae2be67f058e512b0cea90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:46 +0000

    Add package oviond-tap-stripe/0.0.1/oviond_tap_stripe-0.0.1.tar.gz

---
+++ packages/oviond-tap-stripe/oviond_tap_stripe-0.0.1.tar.gz/oviond_tap_stripe-0.0.1/tap_stripe/tap.py
@@ -0,0 +1,41 @@
+"""Stripe tap class."""
+
+from __future__ import annotations
+
+from singer_sdk import Tap
+from singer_sdk import typing as th
+from tap_stripe import streams
+
+
+class TapStripe(Tap):
+    """Stripe tap class."""
+
+    name = "tap-stripe"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "secret_key",
+            th.StringType,
+            required=True,
+            secret=True,
+            title="Secret key",
+            description="The Stripe secret key to authenticate against the API service",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            title="Start Date",
+            description="The earliest record date to sync",
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[streams.StripeStream]:
+        """Return a list of discovered streams."""
+        return [
+            streams.SubscriptionsStream(self),
+            streams.BalanceTransactionsStream(self),
+        ]
+
+
+if __name__ == "__main__":
+    TapStripe.cli()
