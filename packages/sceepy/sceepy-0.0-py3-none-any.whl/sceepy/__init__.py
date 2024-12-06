f64d1e35781ec389290f50e788dc6af65f616957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:21 +0000

    Add package sceepy/0.0/sceepy-0.0-py3-none-any.whl

---
+++ packages/sceepy/sceepy-0.0-py3-none-any.whl/sceepy/__init__.py
@@ -0,0 +1,24 @@
+import os
+import importlib
+
+# Папка с заданиями
+TASKS_DIR = os.path.dirname(__file__)
+
+# Функция для загрузки задания и функции
+def load_task_function(task_number):
+    module_name = f"{task_number}_task"  # Модули называются task1, task2, ...
+    try:
+        # Динамический импорт модуля (например, task1.py, task2.py)
+        module = importlib.import_module(f".{module_name}", package="my_library")
+        # Получаем функцию (например, t1, t2 и т.д.)
+        task_function = getattr(module, f"t{task_number}")
+        return task_function
+    except ModuleNotFoundError:
+        return f"Задача {task_number} не найдена."
+    except AttributeError:
+        return f"Функция t{task_number} не найдена в модуле {module_name}."
+
+# Динамическое добавление всех функций t1-t6 в глобальное пространство
+for task_number in range(1, 7):  # Для задач с 1 по 6
+    task_function = load_task_function(task_number)
+    globals()[f"t{task_number}"] = task_function
