d4f5fbb9deb4738d95f007469917367f0f561ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:35 +0000

    Add package art22m-latex-generator/0.1/art22m_latex_generator-0.1.tar.gz

---
+++ packages/art22m-latex-generator/art22m_latex_generator-0.1.tar.gz/art22m_latex_generator-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="art22m_latex_generator",
+    version="0.1",
+    packages=find_packages(),
+    author="art22m",
+    install_requires=[],
+)
