f0924eb83fc0e319a1637ce719cf990141e8f1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:51 +0000

    Add package bayex/0.2.1b0/bayex-0.2.1b0.tar.gz

---
+++ packages/bayex/bayex-0.2.1b0.tar.gz/bayex-0.2.1b0/bayex/__init__.py
@@ -0,0 +1,12 @@
+import .src
+from .src.optimizer import Optimizer, OptimizerState
+from .src import domain
+
+
+__version__ = "0.2.0"
+
+__all__ = [
+        "Optimizer",
+        "OptimizerState",
+        "domain",
+]
