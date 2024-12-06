b38ebc7fd43166038a54bbcbbe5b03f75ee2ed86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:04 +0000

    Add package forkEntropy/0.9.0/forkEntropy-0.9.0.tar.gz

---
+++ packages/forkEntropy/forkEntropy-0.9.0.tar.gz/forkEntropy-0.9.0/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("./ReadMe.md", "r", encoding="utf-8") as f:
+  long_description = f.read()
+
+setup(
+    name='forkEntropy',  # 包名
+    version='0.9.0',  # 版本号
+    author='XiangChenWu',  # 作者
+    author_email='ixc@smail.nju.edu.cn',  # 作者邮箱
+    description='The implementation and test for the measurement method fork entropy.',  # 简要描述
+    long_description=long_description,  # 详细描述
+    long_description_content_type='text/markdown',
+    url='https://github.com/WuXiangChen/ForkEntropy_ESE',  # 项目网址
+    packages=[
+    'forkEntropyTest',
+    'forkEntropyTest.testData',
+    'forkEntropyTest.testResult',
+    'forkEntropyTest.testData.downloaded_PRrawdata',
+    'forkEntropyTest.testData.commitChanges'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # 许可证
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Python版本要求
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    platforms=["all"],
+    include_package_data=True,
+)
