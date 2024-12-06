8220de645c9b170fb91f1203cc47ac3cd10b3c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:47 +0000

    Add package koreanbots/3.0.0/koreanbots-3.0.0.tar.gz

---
+++ packages/koreanbots/koreanbots-3.0.0.tar.gz/koreanbots-3.0.0/koreanbots/__init__.py
@@ -0,0 +1,21 @@
+from typing import Literal, NamedTuple
+
+from .client import Koreanbots as Koreanbots
+from .errors import *
+from .http import KoreanbotsRequester as KoreanbotsRequester
+from .model import KoreanbotsBot as KoreanbotsBot
+from .model import KoreanbotsServer as KoreanbotsServer
+from .model import KoreanbotsUser as KoreanbotsUser
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+versioninfo = VersionInfo(3, 0, 0, "final", 0)
+
+__version__ = "3.0.0"
