1d4561adb05a33f67fbcb5560b91e062cc85e336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:45 +0000

    Add package fracodim/1.4/fracodim-1.4.tar.gz

---
+++ packages/fracodim/fracodim-1.4.tar.gz/fracodim-1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='fracodim',
+    version='1.4',
+    packages=find_packages(),
+    author='Renan Gomes',
+    author_email='renangcfreitas@gmail.com',
+    description='Structural package of the box-counting plot method used in fractal correlation dimension estimation.',
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/renangcf/fracodim',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+)
