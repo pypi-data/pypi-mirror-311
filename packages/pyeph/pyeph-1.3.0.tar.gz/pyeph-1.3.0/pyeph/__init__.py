8e67572864ccc1cebdbd4ebdbdb60915e8dbe87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:24 +0000

    Add package pyeph/1.3.0/pyeph-1.3.0.tar.gz

---
+++ packages/pyeph/pyeph-1.3.0.tar.gz/pyeph-1.3.0/pyeph/__init__.py
@@ -0,0 +1,5 @@
+from pyeph.get.api import *
+
+from pyeph.calc.api import *
+
+from pyeph.tools.api import *
---
+++ packages/pyeph/pyeph-1.3.0.tar.gz/pyeph-1.3.0/pyeph/get/equivalent_adult/__init__.py
@@ -0,0 +1,9 @@
+from ..getter import Getter
+
+class EquivalentAdult(Getter):
+	"""
+		Obtencion del df de adulto equivalente
+	"""
+
+	folder = "adulto_equivalente"
+	filename = "adulto_equivalente.zip"
