b0047c237e96794d02ddb7868bf28f5276b2a4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:36 +0000

    Add package collate-sqllineage/1.5.0/collate_sqllineage-1.5.0-py3-none-any.whl

---
+++ packages/collate-sqllineage/collate_sqllineage-1.5.0-py3-none-any.whl/collate_sqllineage/core/parser/sqlparse/__init__.py
@@ -0,0 +1,34 @@
+import re
+
+from sqlparse import tokens
+from sqlparse.engine import grouping
+from sqlparse.keywords import KEYWORDS, SQL_REGEX
+
+from collate_sqllineage.core.parser.sqlparse.utils import group_function_with_window
+
+
+def _patch_adding_window_function_token() -> None:
+    grouping.group_functions = group_function_with_window
+
+
+def _patch_adding_builtin_type() -> None:
+    KEYWORDS["STRING"] = tokens.Name.Builtin
+    KEYWORDS["DATETIME"] = tokens.Name.Builtin
+
+
+def _patch_updating_lateral_view_lexeme() -> None:
+    for i, (regex, lexeme) in enumerate(SQL_REGEX):
+        rgx = re.compile(regex, re.IGNORECASE | re.UNICODE).match
+        if rgx("LATERAL VIEW EXPLODE(col)"):
+            new_regex = r"(LATERAL\s+VIEW\s+)(OUTER\s+)?(EXPLODE|INLINE|PARSE_URL_TUPLE|POSEXPLODE|STACK|JSON_TUPLE)\b"
+            SQL_REGEX[i] = (new_regex, lexeme)
+            break
+
+
+def _monkey_patch() -> None:
+    _patch_adding_window_function_token()
+    _patch_adding_builtin_type()
+    _patch_updating_lateral_view_lexeme()
+
+
+_monkey_patch()
