e4504baad67dff82c9e1efe2018161d9ad3159d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:54 +0000

    Add package velesresearch/0.4.1/velesresearch-0.4.1-py3-none-any.whl

---
+++ packages/velesresearch/velesresearch-0.4.1-py3-none-any.whl/velesresearch/__init__.py
@@ -0,0 +1,32 @@
+"VelesResearch main functionality."
+from .wrappers import (
+    survey,
+    page,
+    panel,
+    dropdown,
+    text,
+    checkbox,
+    ranking,
+    radio,
+    dropdownMultiple,
+    textLong,
+    rating,
+    yesno,
+    info,
+    matrix,
+    matrixDropdown,
+    matrixDynamic,
+    slider,
+    image,
+    consent,
+    surveyFromJson,
+)
+from .validators import (
+    numberValidator,
+    textValidator,
+    emailValidator,
+    expressionValidator,
+    answerCountValidator,
+    regexValidator,
+)
+from .utils import convertImage, getJS, botSalt
