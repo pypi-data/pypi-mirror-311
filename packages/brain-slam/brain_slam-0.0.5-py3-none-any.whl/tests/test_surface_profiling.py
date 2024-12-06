22620d2004a1f05a25bb17610d73e036222284b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:10 +0000

    Add package brain-slam/0.0.5/brain_slam-0.0.5-py3-none-any.whl

---
+++ packages/brain-slam/brain_slam-0.0.5-py3-none-any.whl/tests/test_surface_profiling.py
@@ -0,0 +1,19 @@
+import unittest
+
+import trimesh
+
+
+def make_sphere(radius=1):
+    """Create a sphere"""
+    mesh_a = trimesh.creation.icosphere(subdivisions=1, radius=radius)
+    return mesh_a
+
+
+class TestSurfaceProfiling(unittest.TestCase):
+    def test_basic(self):
+        print(1)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
