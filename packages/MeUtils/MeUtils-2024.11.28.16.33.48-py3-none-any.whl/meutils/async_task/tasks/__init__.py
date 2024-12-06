ee68148733ecfc94895657178968272dab08004c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:56 +0000

    Add package MeUtils/2024.11.28.16.33.48/MeUtils-2024.11.28.16.33.48-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.11.28.16.33.48-py3-none-any.whl/meutils/async_task/tasks/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2024/11/28 16:03
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.async_task import worker
+from meutils.async_task.tasks.kling import *
---
+++ packages/MeUtils/MeUtils-2024.11.28.16.33.48-py3-none-any.whl/meutils/async_task/tasks/kling.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : kling
+# @Time         : 2024/11/28 16:18
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+from meutils.decorators.retry import retrying
+
+# from meutils.async_task import worker, shared_task
+from meutils.async_task import  shared_task
+
+
+@shared_task()
+def do_task(**kwargs):
+    return kwargs
+
+
+@shared_task
+@retrying(3)
+def proxy_task(**kwargs):
+    method = kwargs.pop('method', '')
+    url = kwargs.pop('url', '')
+
+    logger.debug(kwargs)
+
+    response = requests.request(method, url, **kwargs).json()
+
+    return response
