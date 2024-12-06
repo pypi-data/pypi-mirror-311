82166481de210f0ac031e1b218f39577b1cb6a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:10 +0000

    Add package brain-slam/0.0.5/brain_slam-0.0.5.tar.gz

---
+++ packages/brain-slam/brain_slam-0.0.5.tar.gz/brain_slam-0.0.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+BASE_REQUIREMENTS = ["numpy", "scipy", "trimesh", "nibabel>=2.1", "networkx"]
+TEST_REQUIREMENTS = ["flake8", "autopep8", "pytest", "pytest-cov", "coveralls"]
+DIST = ["tvb-gdist"]
+
+setup(
+    name="brain-slam",
+    version="0.0.5",
+    packages=find_packages(),
+    author="Guillaume Auzias",
+    description="Surface anaLysis And Modeling",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/brain-slam/slam",
+    license="MIT",
+    python_requires=">=3.6",  # enforce Python 3.6 as minimum
+    install_requires=BASE_REQUIREMENTS,
+    extras_require={
+        "full": DIST,
+        "dev": DIST + TEST_REQUIREMENTS,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
