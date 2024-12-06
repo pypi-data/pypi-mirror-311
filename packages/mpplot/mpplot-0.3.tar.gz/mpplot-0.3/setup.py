04ad67eb4598bf87df204126a4a1efe76777ac1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:49 +0000

    Add package mpplot/0.3/mpplot-0.3.tar.gz

---
+++ packages/mpplot/mpplot-0.3.tar.gz/mpplot-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mpplot',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=["h5py>=3.12.1","matplotlib>=3.9.2"],
+    entry_points={
+        'console_scripts': [
+            'my_package_script = my_package.module1:main_function',
+        ],
+    },
+    author='kevin',
+    author_email='kevin2059@163.com',
+    description='mpplot that connect matplotlib and matlab',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/your_username/my_package',
+    license='MIT',
+)
