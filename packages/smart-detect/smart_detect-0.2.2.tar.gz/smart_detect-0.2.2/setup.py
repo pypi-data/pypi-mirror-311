dc067267b9ee4b713142456bc6f70108c5733a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:42 +0000

    Add package smart-detect/0.2.2/smart_detect-0.2.2.tar.gz

---
+++ packages/smart-detect/smart_detect-0.2.2.tar.gz/smart_detect-0.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart_detect",
+    version="0.2.2",
+    description="A smart pose detection package for movement analysis using OpenCV and Mediapipe",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="nandani",
+    author_email="himanshukrjha004@gmail.com",
+    url="https://github.com/himanshu-kr-jha",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python",
+        "mediapipe",
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
