a2a810b82440c0aee3d959b2d800684c88427f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:27 +0000

    Add package py2saber/0.18.6/py2saber-0.18.6.tar.gz

---
+++ packages/py2saber/py2saber-0.18.6.tar.gz/py2saber-0.18.6/py2saber/__init__.py
@@ -0,0 +1,7 @@
+from .py2saber import (AnimaFileWriteException, AnimaNotReadyException,
+                       InvalidSaberResponseException, NoAnimaSaberException,
+                       NotEnoughFreeSpaceException, Saber_Controller)
+
+__all__ = ["py2saber"]
+
+__version__ = "0.18.6"
