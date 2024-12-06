212bda4a856687a13a6665c5f1513934a29b59f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:14 +0000

    Add package mlcs-assignments/0.1.1/mlcs_assignments-0.1.1-py3-none-any.whl

---
+++ packages/mlcs-assignments/mlcs_assignments-0.1.1-py3-none-any.whl/helpers/maths/__init__.py
@@ -0,0 +1,7 @@
+from .latex import (
+    pretty_matrix as pretty_matrix,
+    pretty_vector as pretty_vector,
+    pretty_latex as pretty_latex,
+)
+from .matrix import MatrixPowers as MatrixPowers
+from .types import Matrix as Matrix, Vector as Vector
