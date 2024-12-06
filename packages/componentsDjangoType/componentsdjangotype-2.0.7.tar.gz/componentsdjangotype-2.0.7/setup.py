86e753f3ac63a6fd5192b103e3413ac2dd5b44de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:04:43 +0000

    Add package componentsDjangoType/2.0.7/componentsdjangotype-2.0.7.tar.gz

---
+++ packages/componentsDjangoType/componentsdjangotype-2.0.7.tar.gz/componentsdjangotype-2.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='componentsDjangoType',
+    version='2.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Comandos para crear archivos html, css y js',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jose-CR/componentsDjangoType',
+    author='Alejandro',
+    author_email='hjosealejandro21@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django :: 3.2',
+    ],
+    install_requires=[
+        'Django>=3.2',
+    ],
+)
