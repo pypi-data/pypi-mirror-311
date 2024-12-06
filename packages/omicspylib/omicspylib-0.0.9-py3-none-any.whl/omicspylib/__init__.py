91df10a119541548d475706cb29a7fcb1f9bebea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:46 +0000

    Add package omicspylib/0.0.9/omicspylib-0.0.9-py3-none-any.whl

---
+++ packages/omicspylib/omicspylib-0.0.9-py3-none-any.whl/omicspylib/__init__.py
@@ -0,0 +1,22 @@
+"""
+Starting point for importing libraries main objects.
+"""
+from omicspylib.datasets.proteins import ProteinsDataset
+from omicspylib.datasets.peptides import PeptidesDataset
+from omicspylib.datasets.concat import concat
+from omicspylib.analysis.pairs.statistical import PairwiseComparisonTTestFC
+from omicspylib.analysis.pairs.frequency_based import PairwiseUniqueEntryComparison
+from omicspylib.analysis.clusters import HierarchicallyClusteredHeatmap
+
+
+__all__ = [
+    'concat',
+    'HierarchicallyClusteredHeatmap',
+    'PairwiseComparisonTTestFC',
+    'PairwiseUniqueEntryComparison',
+    'PeptidesDataset',
+    'ProteinsDataset',
+]
+
+
+__version__ = '0.0.9'
