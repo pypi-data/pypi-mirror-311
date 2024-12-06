13b0e73ebc6e253d0301ca9d45df2981ada7db3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:26 +0000

    Add package intentional-text-chat/0.0.1rc1/intentional_text_chat-0.0.1rc1-py3-none-any.whl

---
+++ packages/intentional-text-chat/intentional_text_chat-0.0.1rc1-py3-none-any.whl/intentional_text_chat/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present ZanSara <github@zansara.dev>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""
+Init file for `intentional_text_chat`.
+"""
+
+from intentional_text_chat.bot_structure import TextChatBotStructure
+
+__all__ = ["TextChatBotStructure"]
