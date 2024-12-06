973b3364e654547aef0f85ffffd5cf077f9f7599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:32 +0000

    Add package tensorama/0.1.3/tensorama-0.1.3.tar.gz

---
+++ packages/tensorama/tensorama-0.1.3.tar.gz/tensorama-0.1.3/README.md
@@ -0,0 +1,6 @@
+# MyLib
+A simple Python library for helping people.
+
+## Installation
+```bash
+pip install mylib
---
+++ packages/tensorama/tensorama-0.1.3.tar.gz/tensorama-0.1.3/tensorama/__init__.py
@@ -0,0 +1,2 @@
+from .tensorama import allCMD, TokenizeStopwordPOS, SpellStemLemNER, SentimentScore, DFT_DCT_MS, WalshSlant, AmpEnvLoudness, ChromaMFCC, DFS
+
