1e484f4bfbba5a318b74615bd7caf33f802b8913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:09 +0000

    Add package pp.server/3.4.0/pp.server-3.4.0-py3-none-any.whl

---
+++ packages/pp.server/pp.server-3.4.0-py3-none-any.whl/pp/server/tests/test_utils.py
@@ -0,0 +1,11 @@
+################################################################
+# pp.server - Produce & Publish Server
+# (C) 2013, ZOPYX,  Tuebingen, Germany
+################################################################
+
+from pp.server import util
+
+
+def test_which():
+    assert util.which("vim")
+    assert not util.which("unknown")
