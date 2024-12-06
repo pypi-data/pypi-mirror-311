44d4f34a6d9fa83b328a9c9e2b060d5391d81b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:05 +0000

    Add package DYNMO/1.0.0/DYNMO-1.0.0-py3-none-any.whl

---
+++ packages/DYNMO/DYNMO-1.0.0-py3-none-any.whl/webscraping/__init__.py
@@ -0,0 +1,7 @@
+from webscraping.match_history import MatchHistory
+from webscraping.squads import Squads
+from webscraping.standings import Standings
+from webscraping.process_data import ProcessData
+from webscraping.compile_data import CompileData
+
+__all__ = ['MatchHistory', 'Squads', 'Standings', 'ProcessData', 'CompileData', 'load_data']
