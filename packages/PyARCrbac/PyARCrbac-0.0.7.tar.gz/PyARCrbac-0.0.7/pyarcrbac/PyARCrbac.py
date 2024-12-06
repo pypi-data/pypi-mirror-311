98642c8150ba64a2548703374035d40112c3cff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:38 +0000

    Add package PyARCrbac/0.0.7/PyARCrbac-0.0.7.tar.gz

---
+++ packages/PyARCrbac/PyARCrbac-0.0.7.tar.gz/PyARCrbac-0.0.7/pyarcrbac/PyARCrbac.py
@@ -0,0 +1,39 @@
+import requests
+
+class PyARC:
+    def __init__(self):
+        self.mgmturl = "http://127.0.0.1:40342/metadata/identity/oauth2/token?api-version=2019-11-01&resource=https%3A%2F%2Fmanagement.azure.com"
+        self.graphurl = "http://127.0.0.1:40342/metadata/identity/oauth2/token?api-version=2019-11-01&resource=https%3A%2F%2Fgraph.microsoft.com"
+    def graph_token(self):
+        """
+        Retrieves the challenge token from the metadata service.
+        """
+        headers = {"Metadata": "true"}
+        response = requests.get(self.graphurl, headers=headers)
+        
+        for item, data in response.headers.items():
+            if item == "Www-Authenticate":
+                challenge_token_path = data.split("=")[1]
+        if challenge_token_path is None:
+            raise Exception("Failed to retrieve challenge token")
+        with open(challenge_token_path, "r") as file:
+            header = {"Metadata": "true", "Authorization": f"Basic {file.read()}"}
+        response = requests.get(URL, headers=header)
+        return response.json()['access_token']
+
+    def get_management_token():
+        """
+        Retrieves the challenge token from the metadata service.
+        """
+        headers = {"Metadata": "true"}
+        response = requests.get(self.mgmturl, headers=headers)
+        
+        for item, data in response.headers.items():
+            if item == "Www-Authenticate":
+                challenge_token_path = data.split("=")[1]
+        if challenge_token_path is None:
+            raise Exception("Failed to retrieve challenge token")
+        with open(challenge_token_path, "r") as file:
+            header = {"Metadata": "true", "Authorization": f"Basic {file.read()}"}
+        response = requests.get(URL, headers=header)
+        return response.json()['access_token']
