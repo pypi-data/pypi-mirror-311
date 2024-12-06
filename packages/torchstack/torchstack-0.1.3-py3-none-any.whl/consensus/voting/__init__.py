5699df290dac19758e997384389c64966b109b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:02 +0000

    Add package torchstack/0.1.3/torchstack-0.1.3-py3-none-any.whl

---
+++ packages/torchstack/torchstack-0.1.3-py3-none-any.whl/consensus/voting/__init__.py
@@ -0,0 +1,7 @@
+from .auto import AutoAggregator
+from .weighted import WeightedAggregator
+from .average import AverageAggregator
+
+
+# Define the public API
+__all__ = ["AutoAggregator", "WeightedAggregator", "AverageAggregator"]
