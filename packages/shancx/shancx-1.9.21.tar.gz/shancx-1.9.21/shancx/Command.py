d943b891e9f2c0d9aecf45eb9b1acffc0b753ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:24 +0000

    Add package shancx/1.9.21/shancx-1.9.21.tar.gz

---
+++ packages/shancx/shancx-1.9.21.tar.gz/shancx-1.9.21/shancx/Command.py
@@ -0,0 +1,29 @@
+allDf.dtypes
+
+vim ~/.bash_history
+git checkout main  -- /home/scx/mqpf_pmsc/backup/of_ref_pre.py
+du -sh * | sort -h
+rsync -avid scx@10.1.98.7:/home/scx/test/project   ./
+rsync -avid scx@10.1.98.7:/home/scx/ESRGAN-PyTorch-main /home/scx/test/   整个文件夹以及文件夹下
+find . -type f -name "project1.log*" -exec rm -f {} \;
+find . -type f -name "project*.log*" -exec rm -f {} \;
+sudo nvidia-smi -i 2 -pm 0
+ssh scx@10.1.98.7     cmd 链接
+   
+grep users /etc/group
+sudo groupdel scx
+sudo useradd -u 1015 -g 1015 scx
+id scx
+
+
+more /etc/passwd  
+
+sudo usermod -g users scx
+sudo groupdel users1
+sudo groupadd -g 1015 scx
+sudo usermod -g 1015 scx
+id scx
+grep scx /etc/group
+
+
+sudo chown -R scx:scx /mnt/wtx_weather_forecast/scx/mqpf_0722_wtyN/
