f3c40a6bcac96a5d6c2948d45da93dc8e022b911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:40 +0000

    Add package versatile-audio-upscaler-fixed/0.0.4/versatile-audio-upscaler-fixed-0.0.4.tar.gz

---
+++ packages/versatile-audio-upscaler-fixed/versatile-audio-upscaler-fixed-0.0.4.tar.gz/versatile-audio-upscaler-fixed-0.0.4/README.md
@@ -0,0 +1,26 @@
+# Audio Upscaler (AudioSR)
+
+[![arXiv](https://img.shields.io/badge/arXiv-2309.07314-brightgreen.svg?style=flat-square)](https://arxiv.org/abs/2309.07314)  [![githubio](https://img.shields.io/badge/GitHub.io-Audio_Samples-blue?logo=Github&style=flat-square)](https://audioldm.github.io/audiosr)
+
+## Overview
+
+AudioSR is a powerful tool designed to enhance the fidelity of your audio files, regardless of their type (e.g., music, speech, ambient sounds) or sampling rates. It leverages cutting-edge super-resolution techniques to upscale audio signals, resulting in superior quality output.
+
+## Key Features
+
+- **Versatility**: Works seamlessly with all types of audio content, including music, speech, environmental sounds, and more.
+- **Scale**: Handles audio files of various sampling rates, ensuring compatibility with a wide range of sources.
+- **High Fidelity**: Produces high-quality output with enhanced clarity and detail.
+- **Ease of Use**: Simple and intuitive interface makes it easy to enhance your audio files with just a few clicks.
+
+## Acknowledgments
+Based on the work of https://github.com/haoheliu/versatile_audio_super_resolution/
+
+```bibtex
+@article{liu2023audiosr,
+  title={{AudioSR}: Versatile Audio Super-resolution at Scale},
+  author={Liu, Haohe and Chen, Ke and Tian, Qiao and Wang, Wenwu and Plumbley, Mark D},
+  journal={arXiv preprint arXiv:2309.07314},
+  year={2023}
+}
+```
---
+++ packages/versatile-audio-upscaler-fixed/versatile-audio-upscaler-fixed-0.0.4.tar.gz/versatile-audio-upscaler-fixed-0.0.4/setup.py
@@ -0,0 +1,108 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = "versatile-audio-upscaler-fixed"
+DESCRIPTION = (
+    "Versatile AI-driven audio upscaler to enhance the quality of any audio -- Now"
+    " supporting Numpy +1.26"
+)
+URL = "https://github.com/JackismyShephard/Audio-Upscaler"
+EMAIL = "jackismyshephard@gmail.com"
+AUTHOR = "Jack Is My Shephard"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.0.4"
+
+REQUIRED = [
+    "torch>=1.13.0",
+    "torchaudio>=0.13.0",
+    "torchvision>=0.14.0",
+    "tqdm",
+    "pyyaml",
+    "einops",
+    "chardet",
+    "numpy<=1.26.4",
+    "soundfile",
+    "librosa>=0.9.2",
+    "scipy",
+    "pandas",
+    "tokenizers>=0.14.1",
+    "unidecode",
+    "phonemizer",
+    "torchlibrosa>=0.0.9",
+    "transformers",
+    "wget",
+    "progressbar",
+    "ftfy",
+    "timm",
+    "matplotlib",
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    # packages=find_packages(exclude=[]),
+    # If your package is a single module, use this instead of 'packages':
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    packages=find_packages(),
+    package_data={
+        "audio_upscaler": [
+            "*/*.py",
+            "*/*/*.py",
+            "*/*/*/*.py",
+            "*/*/*/*/*.py",
+            "*/*.npy",
+            "*/*/*.npy",
+            "*/*.gz",
+            "*/*/*.gz",
+            "*/*.json",
+            "*/*/*.json",
+            "*/*/*/*.json",
+        ]
+    },
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
