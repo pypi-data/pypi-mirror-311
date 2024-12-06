9a6dc30e5f5aba0a90af6e330bfc276d434a8271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:59 +0000

    Add package datatrails-scitt-samples/0.0.1/datatrails_scitt_samples-0.0.1.tar.gz

---
+++ packages/datatrails-scitt-samples/datatrails_scitt_samples-0.0.1.tar.gz/datatrails_scitt_samples-0.0.1/unittests/constants.py
@@ -0,0 +1,7 @@
+"""
+Unit Test Data Constants
+"""
+
+KNOWN_STATEMENT = {"author": "fred", "title": "my biography", "reviews": "mixed"}
+
+KNOWN_RECEIPT_FILE: str = "unittests/resources/scitt-receipt.cbor"
