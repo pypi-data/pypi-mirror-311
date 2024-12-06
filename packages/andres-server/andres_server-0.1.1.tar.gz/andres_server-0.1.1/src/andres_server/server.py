309485248dee96faceabfc7dee4209af677f3638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:41 +0000

    Add package andres-server/0.1.1/andres_server-0.1.1.tar.gz

---
+++ packages/andres-server/andres_server-0.1.1.tar.gz/andres_server-0.1.1/src/andres_server/server.py
@@ -0,0 +1,24 @@
+from mcp.server.sse import SseServerTransport
+from mcp.server import Server
+from starlette.applications import Starlette
+from starlette.routing import Route
+
+
+async def main():
+
+    app = Server("example-server")
+    sse = SseServerTransport("/messages")
+
+    async def handle_sse(scope, receive, send):
+        async with sse.connect_sse(scope, receive, send) as streams:
+            await app.run(streams[0], streams[1], app.create_initialization_options())
+
+    async def handle_messages(scope, receive, send):
+        await sse.handle_post_message(scope, receive, send)
+
+    starlette_app = Starlette(
+        routes=[
+            Route("/sse", endpoint=handle_sse),
+            Route("/messages", endpoint=handle_messages, methods=["POST"]),
+        ]
+    )
