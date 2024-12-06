0d7f6599f97b99acc1f781219484fbfe274d33ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:26 +0000

    Add package django-swifty/0.0.23/django_swifty-0.0.23-py3-none-any.whl

---
+++ packages/django-swifty/django_swifty-0.0.23-py3-none-any.whl/swifty/mongodb/connector.py
@@ -0,0 +1,241 @@
+"""MongoDB Connector"""
+
+from typing import Union, List, Dict
+
+from mongoengine import connect
+from mongoengine.base import BaseDocument
+from mongoengine.errors import MongoEngineException
+
+from swifty.logging.logger import SwiftyLoggerMixin
+
+
+def init_mongodb_connections(database_pool):
+    """Init mongo connections
+
+    Args:
+        database_pool (_type_): _description_
+    """
+    for alias, database in database_pool:
+        connect(
+            db=database["db"],
+            alias=alias,
+            host=database["host"],
+            maxPoolSize=database.get("max_pool_size", 10),
+            **database.get("kwargs", {}),
+        )
+
+
+# @contextmanager
+# def connect_to_db(db: str, alias: str, count: int = 3) -> None:  # type: ignore
+#     """
+#     Context manager to connect to the MongoDB database with retry logic.
+
+#     Args:
+#         db (str): The name of the database to connect to.
+#         alias (str): The alias for the connection.
+#         count (int, optional): Number of retry attempts. Defaults to 3.
+
+#     Raises:
+#         MongoEngineException: If unable to connect after specified attempts.
+#     """
+#     retry_count = 0
+#     exception = None
+#     connected = False
+
+#     while retry_count < count:
+#         try:
+#             # Attempt to connect
+#             connect(host=f"{settings.MONGO_URL}", db=db, alias=alias)
+#             connected = True
+#             break  # Exit retry loop on successful connection
+#         except (MongoEngineException, ConnectionError) as ex:
+#             retry_count += 1
+#             exception = six.text_type(ex)
+#             sleep(0.3)  # Wait 300ms before retrying
+
+#     if not connected:
+#         raise MongoEngineException(
+#             f"Unable to connect to database after {count} attempts, specific error: {exception}"
+#         )
+
+#     try:
+#         # Code inside the `with` block can execute here
+#         yield
+#     finally:
+#         # Ensure the connection is closed properly when the context exits
+#         disconnect(alias)
+
+
+# def mongo_connection(func):
+#     """
+#     Decorator which automatically reconnects mongo connection
+#     when connection or timeout errors occur.
+
+#     .. note::
+#         This function expects the wrapped function to
+#         have ``reconnect()`` method.
+#     """
+
+#     def wrapper(self, *args, **kwargs):
+#         """_summary_
+
+#         Raises:
+#             MongoEngineException: _description_
+#             MongoEngineException: _description_
+
+#         Returns:
+#             _type_: _description_
+#         """
+
+#         metadata = getattr(self.mongo_collection, "_meta", {})
+
+#         db = metadata and metadata.get("db_name", None)
+
+#         if not db:
+#             raise MongoEngineException("Mongo DB is not defined")
+
+#         alias = metadata and metadata.get("db_alias", "default")
+
+#         with connect_to_db(db=db, alias=alias):
+#             return func(self, *args, **kwargs)
+
+#     return wrapper
+
+
+class MongoConnector(SwiftyLoggerMixin):
+    """MongoDB Connector class for handling database operations.
+
+    Args:
+        SwiftyLoggerMixin (_type_): _description_
+    """
+
+    def __init__(self, mongo_collection: BaseDocument) -> None:
+        if not mongo_collection or not issubclass(mongo_collection, BaseDocument):
+            raise MongoEngineException("Mongo collection is not defined")
+        self.mongo_collection = mongo_collection
+
+    @property
+    def mongo_objects(self):
+        """Get the objects manager for the MongoDB collection."""
+        return getattr(self.mongo_collection, "objects")
+
+    # @mongo_connection
+    def insert(self, data: dict) -> None:
+        """Insert a single document into the MongoDB collection.
+
+        Args:
+            data (dict): The document data to insert.
+        """
+        self.mongo_objects.insert(self.mongo_collection(**data))
+
+    # @mongo_connection
+    def insert_many(self, list_of_data: List[Dict]) -> None:
+        """Insert multiple documents into the MongoDB collection.
+
+        Args:
+            list_of_data (List[Dict]): A list of document data to insert.
+        """
+        self.mongo_objects.insert(
+            [self.mongo_collection(**data) for data in list_of_data],
+        )
+
+    # @mongo_connection
+    def update(
+        self,
+        data: dict,
+        filters: Union[dict, None] = None,
+        q_combine: Union[None, str] = None,
+    ) -> None:
+        """Update documents in the MongoDB collection.
+
+        Args:
+            data (dict): The data to update.
+            filters (Union[dict, None], optional): Filters to apply. Defaults to None.
+            q_combine (Union[None, str], optional): Query combination. Defaults to None.
+        """
+        self.mongo_objects(q_combine, **(filters or {})).update(__raw__={"$set": data})
+
+    # @mongo_connection
+    def update_many(self, list_of_data: List[Dict]) -> None:
+        """Replace multiple documents in the MongoDB collection.
+
+        Args:
+            list_of_data (List[Dict]): A list of document data to replace.
+        """
+        self.mongo_objects.replace(
+            [self.mongo_collection(**data) for data in list_of_data],
+        )
+
+    # @mongo_connection
+    def aggregate(self, pipeline: List[dict]) -> List[dict]:
+        """Perform aggregation on the MongoDB collection.
+
+        Args:
+            pipeline (List[dict]): The aggregation pipeline.
+
+        Returns:
+            List[dict]: The results of the aggregation.
+        """
+        return self.mongo_objects.aggregate(pipeline)
+
+    def __find(
+        self,
+        filters: Union[dict, None] = None,
+        q_combine: Union[None, str] = None,
+        order_by: str = "-version",
+    ) -> List[dict]:
+        """Find documents in the MongoDB collection.
+
+        Args:
+            filters (Union[dict, None], optional): Filters to apply. Defaults to None.
+            q_combine (Union[None, str], optional): Query combination. Defaults to None.
+            order_by (str, optional): Field to order by. Defaults to "-version".
+
+        Returns:
+            List[dict]: The found documents.
+        """
+        return self.mongo_objects.filter(q_combine, **(filters or {})).order_by(
+            order_by
+        )
+
+    # @mongo_connection
+    def find_one(
+        self,
+        filters: Union[dict, None] = None,
+        q_combine: Union[None, str] = None,
+        order_by: str = "-version",
+    ) -> Union[None, dict]:
+        """Find a single document in the MongoDB collection.
+
+        Args:
+            filters (Union[dict, None], optional): Filters to apply. Defaults to None.
+            q_combine (Union[None, str], optional): Query combination. Defaults to None.
+            order_by (str, optional): Field to order by. Defaults to "-version".
+
+        Returns:
+            Union[None, dict]: The found document or None if not found.
+        """
+        return self.__find(
+            filters=filters, q_combine=q_combine, order_by=order_by
+        ).first()
+
+    # @mongo_connection
+    def find_many(
+        self,
+        filters: Union[dict, None] = None,
+        q_combine: Union[None, str] = None,
+        order_by: str = "-version",
+    ) -> List[dict]:
+        """Find multiple documents in the MongoDB collection.
+
+        Args:
+            filters (Union[dict, None], optional): Filters to apply. Defaults to None.
+            q_combine (Union[None, str], optional): Query combination. Defaults to None.
+            order_by (str, optional): Field to order by. Defaults to "-version".
+
+        Returns:
+            List[dict]: A list of found documents.
+        """
+        return self.__find(
+            filters=filters, q_combine=q_combine, order_by=order_by
+        ).all()
