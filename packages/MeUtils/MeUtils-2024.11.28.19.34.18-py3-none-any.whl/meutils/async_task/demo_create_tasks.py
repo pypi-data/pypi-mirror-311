9645a72290f5434bd9e469ae5bea74bbab35f19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:07 +0000

    Add package MeUtils/2024.11.28.19.34.18/MeUtils-2024.11.28.19.34.18-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.11.28.19.34.18-py3-none-any.whl/meutils/async_task/demo_create_tasks.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : x
+# @Time         : 2024/11/28 15:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+from meutils.async_task import worker
+from meutils.async_task.tasks import test
+from celery.result import AsyncResult
+
+
+# print(worker.conf)
+
+# @shared_task(
+#     autoretry_for=(Exception,),      # 自动重试的异常类型
+#     retry_kwargs={
+#         'max_retries': 3,            # 最大重试次数
+#         'countdown': 60              # 重试等待时间（秒）
+#     },
+#     retry_backoff=True,              # 启用指数退避
+#     retry_backoff_max=600,           # 最大退避时间（秒）
+#     retry_jitter=True                # 添加随机抖动
+# )
+# def my_task():
+#     try:
+#         # 任务逻辑
+#         result = some_operation()
+#         return result
+#     except Exception as exc:
+#         logger.error(f"Task failed: {exc}")
+#         raise  # 触发自动重试
+
+
+if __name__ == '__main__':
+    # r = test.do_sync_task.apply_async(kwargs={'a': 1, 'b': 2})
+    # logger.debug(r.backend)
+    test.ado_sync_task.apply_async(kwargs={'a': 1, 'b': 2})
+
+    # print(AsyncResult(r.id).result)
+    # result.backend
+
