93d05d2b6bd59d9ae1a00ce6dacc8942b8828c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:12:25 +0000

    Add package novokshanovLatexGen/0.3.0/novokshanovLatexGen-0.3.0.tar.gz

---
+++ packages/novokshanovLatexGen/novokshanovLatexGen-0.3.0.tar.gz/novokshanovLatexGen-0.3.0/latex_gen/latex_gen.py
@@ -0,0 +1,40 @@
+
+def generate_latex_table(matrix):
+    """
+    Генерация LaTeX кода таблицы из двойного списка (matrix).
+    
+    Args:
+        matrix: Двойной список значений, которые должны быть включены в таблицу.
+
+    Returns:
+        str: Строка с кодом LaTeX для таблицы.
+    """
+    def format_row(row):
+        return ' & '.join(map(str, row)) + ' \\\\'
+
+    table_header = '\\begin{tabular}{' + 'c' * len(matrix[0]) + '}'
+    table_footer = '\\end{tabular}'
+    formatted_rows = map(format_row, matrix)
+    table_body = '\n'.join(formatted_rows)
+
+    return '\n'.join([table_header, '\\hline', table_body, '\\hline', table_footer])
+
+def generate_latex_image(image_path, width='\\textwidth'):
+    """
+    Генерация LaTeX кода для вставки изображения.
+
+    Args:
+        image_path: Путь к файлу изображения.
+        width: Ширина изображения в LaTeX (например, '\\textwidth').
+
+    Returns:
+        str: Строка с LaTeX кодом для изображения.
+    """
+    return f"""
+\\begin{{figure}}[h!]
+    \\centering
+    \\includegraphics[width={width}]{{{image_path}}}
+    \\caption{{Пример изображения}}
+    \\label{{fig:sample_image}}
+\\end{{figure}}
+    """
