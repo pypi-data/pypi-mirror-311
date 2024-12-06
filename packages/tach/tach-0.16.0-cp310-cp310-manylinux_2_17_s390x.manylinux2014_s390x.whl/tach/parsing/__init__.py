e58ce272225a5ef859decfe29af2de8d56ca369b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:50 +0000

    Add package tach/0.16.0/tach-0.16.0-cp310-cp310-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/tach/tach-0.16.0-cp310-cp310-manylinux_2_17_s390x.manylinux2014_s390x.whl/tach/parsing/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from tach.parsing.config import (
+    dump_project_config_to_toml,
+    parse_project_config,
+)
+
+__all__ = [
+    "parse_project_config",
+    "dump_project_config_to_toml",
+]
