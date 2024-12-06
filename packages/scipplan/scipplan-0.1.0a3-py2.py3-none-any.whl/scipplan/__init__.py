6062266eeaa2a2f41ff76f3c922b17e3234a217d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:17 +0000

    Add package scipplan/0.1.0a3/scipplan-0.1.0a3-py2.py3-none-any.whl

---
+++ packages/scipplan/scipplan-0.1.0a3-py2.py3-none-any.whl/scipplan/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0alpha3"
+print(f"SCIPPlan Version: {__version__}")
+__release__ = "v0.1.0"
+__author__ = "Ari Gestetner, Buser Say"
+__email__ = "ari.gestetner@monash.edu, buser.say@monash.edu"
