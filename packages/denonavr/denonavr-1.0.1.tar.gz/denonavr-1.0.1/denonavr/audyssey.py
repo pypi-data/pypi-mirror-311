ea17276a0ba677fd0b52f29abacb4243ce311130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:05 +0000

    Add package denonavr/1.0.1/denonavr-1.0.1.tar.gz

---
+++ packages/denonavr/denonavr-1.0.1.tar.gz/denonavr-1.0.1/denonavr/audyssey.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module implements the Audyssey settings of Denon AVR receivers.
+
+:copyright: (c) 2020 by Oliver Goetz.
+:license: MIT, see LICENSE for more details.
+"""
+
+import logging
+from collections.abc import Hashable
+from typing import List, Optional
+
+import attr
+
+from .appcommand import AppCommandCmd, AppCommandCmdParam, AppCommands
+from .const import (
+    DENON_ATTR_SETATTR,
+    DYNAMIC_VOLUME_MAP,
+    DYNAMIC_VOLUME_MAP_LABELS_APPCOMMAND,
+    DYNAMIC_VOLUME_MAP_LABELS_TELNET,
+    MULTI_EQ_MAP,
+    MULTI_EQ_MAP_LABELS_APPCOMMAND,
+    MULTI_EQ_MAP_LABELS_TELNET,
+    REF_LVL_OFFSET_MAP,
+    REF_LVL_OFFSET_MAP_LABELS_APPCOMMAND,
+    REF_LVL_OFFSET_MAP_LABELS_TELNET,
+)
+from .exceptions import AvrCommandError, AvrProcessingError
+from .foundation import DenonAVRFoundation, convert_string_int_bool
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@attr.s(auto_attribs=True, on_setattr=DENON_ATTR_SETATTR)
+class DenonAVRAudyssey(DenonAVRFoundation):
+    """Audyssey Settings."""
+
+    _dynamiceq: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(convert_string_int_bool), default=None
+    )
+    _dynamiceq_control: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(convert_string_int_bool), default=None
+    )
+    _reflevoffset: Optional[str] = attr.ib(
+        converter=attr.converters.optional(REF_LVL_OFFSET_MAP.get), default=None
+    )
+    _reflevoffset_control: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(convert_string_int_bool), default=None
+    )
+    _dynamicvol: Optional[str] = attr.ib(
+        converter=attr.converters.optional(DYNAMIC_VOLUME_MAP.get), default=None
+    )
+    _dynamicvol_control: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(convert_string_int_bool), default=None
+    )
+    _multeq: Optional[str] = attr.ib(
+        converter=attr.converters.optional(MULTI_EQ_MAP.get), default=None
+    )
+    _multeq_control: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(convert_string_int_bool), default=None
+    )
+
+    # Update tags for attributes
+    # AppCommand0300.xml interface
+    appcommand0300_attrs = {AppCommands.GetAudyssey: None}
+
+    def setup(self) -> None:
+        """Ensure that the instance is initialized."""
+        # Add tags for a potential AppCommand.xml update
+        for tag in self.appcommand0300_attrs:
+            self._device.api.add_appcommand0300_update_tag(tag)
+
+        self._device.telnet_api.register_callback(
+            "PS", self._async_sound_detail_callback
+        )
+
+        self._is_setup = True
+
+    async def _async_sound_detail_callback(
+        self, zone: str, event: str, parameter: str
+    ) -> None:
+        """Handle a sound detail change event."""
+        if self._device.zone != zone:
+            return
+
+        if parameter[0:6] == "REFLEV":
+            self._reflevoffset = parameter[7:]
+        elif parameter[0:6] == "DYNVOL":
+            self._dynamicvol = parameter[7:]
+        elif parameter[0:6] == "MULTEQ":
+            self._multeq = parameter[7:]
+        elif parameter == "DYNEQ ON":
+            self._dynamiceq = "1"
+        elif parameter == "DYNEQ OFF":
+            self._dynamiceq = "0"
+
+    async def async_update(
+        self, global_update: bool = False, cache_id: Optional[Hashable] = None
+    ) -> None:
+        """Update Audyssey asynchronously."""
+        _LOGGER.debug("Starting Audyssey update")
+        # Ensure instance is setup before updating
+        if not self._is_setup:
+            self.setup()
+
+        # Update state
+        await self.async_update_audyssey(global_update=global_update, cache_id=cache_id)
+        _LOGGER.debug("Finished Audyssey update")
+
+    async def async_update_audyssey(
+        self, global_update: bool = False, cache_id: Optional[Hashable] = None
+    ):
+        """Update Audyssey status of device."""
+        if self._device.use_avr_2016_update is None:
+            raise AvrProcessingError(
+                "Device is not setup correctly, update method not set"
+            )
+
+        # Audyssey is only available for avr 2016 update
+        if self._device.use_avr_2016_update:
+            try:
+                await self.async_update_attrs_appcommand(
+                    self.appcommand0300_attrs,
+                    appcommand0300=True,
+                    global_update=global_update,
+                    cache_id=cache_id,
+                )
+            except AvrProcessingError as err:
+                # Don't raise an error here, because not all devices support it
+                _LOGGER.debug("Updating Audyssey failed: %s", err)
+
+    async def _async_set_audyssey(self, cmd: AppCommandCmd) -> None:
+        """Set Audyssey parameter."""
+        res = await self._device.api.async_post_appcommand(
+            self._device.urls.appcommand0300, (cmd,)
+        )
+
+        try:
+            if res.find("cmd").text != "OK":
+                raise AvrProcessingError(f"SetAudyssey command {cmd.name} failed")
+        except AttributeError as err:
+            raise AvrProcessingError(f"SetAudyssey command {cmd.name} failed") from err
+
+    ##############
+    # Properties #
+    ##############
+    @property
+    def dynamic_eq(self) -> Optional[bool]:
+        """Return value of Dynamic EQ."""
+        return self._dynamiceq
+
+    @property
+    def reference_level_offset(self) -> Optional[str]:
+        """Return value of Reference Level Offset."""
+        return self._reflevoffset
+
+    @property
+    def reference_level_offset_setting_list(self) -> List[str]:
+        """Return a list of available reference level offset settings."""
+        if self._device.telnet_available:
+            return list(REF_LVL_OFFSET_MAP_LABELS_TELNET.keys())
+        return list(REF_LVL_OFFSET_MAP_LABELS_APPCOMMAND.keys())
+
+    @property
+    def dynamic_volume(self) -> Optional[str]:
+        """Return value of Dynamic Volume."""
+        return self._dynamicvol
+
+    @property
+    def dynamic_volume_setting_list(self) -> List[str]:
+        """Return a list of available Dynamic Volume settings."""
+        if self._device.telnet_available:
+            return list(DYNAMIC_VOLUME_MAP_LABELS_TELNET.keys())
+        return list(DYNAMIC_VOLUME_MAP_LABELS_APPCOMMAND.keys())
+
+    @property
+    def multi_eq(self) -> Optional[str]:
+        """Return value of MultiEQ."""
+        return self._multeq
+
+    @property
+    def multi_eq_setting_list(self) -> List[str]:
+        """Return a list of available MultiEQ settings."""
+        if self._device.telnet_available:
+            return list(MULTI_EQ_MAP_LABELS_TELNET.keys())
+        return list(MULTI_EQ_MAP_LABELS_APPCOMMAND.keys())
+
+    ##########
+    # Setter #
+    ##########
+    async def async_dynamiceq_off(self) -> None:
+        """Turn DynamicEQ off."""
+        if self._device.telnet_available:
+            telnet_command = self._device.telnet_commands.command_dynamiceq + "OFF"
+            await self._device.telnet_api.async_send_commands(telnet_command)
+            return
+        cmd = attr.evolve(
+            AppCommands.SetAudysseyDynamicEQ,
+            param_list=(AppCommandCmdParam(name="dynamiceq", text=0),),
+        )
+        await self._async_set_audyssey(cmd)
+
+    async def async_dynamiceq_on(self) -> None:
+        """Turn DynamicEQ on."""
+        if self._device.telnet_available:
+            telnet_command = self._device.telnet_commands.command_dynamiceq + "ON"
+            await self._device.telnet_api.async_send_commands(telnet_command)
+            return
+        cmd = attr.evolve(
+            AppCommands.SetAudysseyDynamicEQ,
+            param_list=(AppCommandCmdParam(name="dynamiceq", text=1),),
+        )
+        await self._async_set_audyssey(cmd)
+
+    async def async_set_multieq(self, value: str) -> None:
+        """Set MultiEQ mode."""
+        if self._device.telnet_available:
+            setting = MULTI_EQ_MAP_LABELS_TELNET.get(value)
+            if setting is None:
+                raise AvrCommandError(f"Value {value} not known for MultiEQ")
+            telnet_command = self._device.telnet_commands.command_multieq + setting
+            await self._device.telnet_api.async_send_commands(telnet_command)
+            return
+
+        setting = MULTI_EQ_MAP_LABELS_APPCOMMAND.get(value)
+        if setting is None:
+            raise AvrCommandError(f"Value {value} not known for MultiEQ")
+        cmd = attr.evolve(
+            AppCommands.SetAudysseyMultiEQ,
+            param_list=(AppCommandCmdParam(name="multeq", text=setting),),
+        )
+        await self._async_set_audyssey(cmd)
+
+    async def async_set_reflevoffset(self, value: str) -> None:
+        """Set Reference Level Offset."""
+        # Reference level offset can only be used with DynamicEQ
+        if not self._dynamiceq:
+            raise AvrCommandError(
+                "Reference level could only be set when DynamicEQ is active"
+            )
+        if self._device.telnet_available:
+            setting = REF_LVL_OFFSET_MAP_LABELS_TELNET.get(value)
+            if setting is None:
+                raise AvrCommandError(
+                    f"Value {value} not known for Reference level offset"
+                )
+            telnet_command = self._device.telnet_commands.command_reflevoffset + setting
+            await self._device.telnet_api.async_send_commands(telnet_command)
+            return
+
+        setting = REF_LVL_OFFSET_MAP_LABELS_APPCOMMAND.get(value)
+        if setting is None:
+            raise AvrCommandError(f"Value {value} not known for Reference level offset")
+        cmd = attr.evolve(
+            AppCommands.SetAudysseyReflevoffset,
+            param_list=(AppCommandCmdParam(name="reflevoffset", text=setting),),
+        )
+        await self._async_set_audyssey(cmd)
+
+    async def async_set_dynamicvol(self, value: str) -> None:
+        """Set Dynamic Volume."""
+        if self._device.telnet_available:
+            setting = DYNAMIC_VOLUME_MAP_LABELS_TELNET.get(value)
+            if setting is None:
+                raise AvrCommandError(f"Value {value} not known for Dynamic Volume")
+            telnet_command = self._device.telnet_commands.command_dynamicvol + setting
+            await self._device.telnet_api.async_send_commands(telnet_command)
+            return
+
+        setting = DYNAMIC_VOLUME_MAP_LABELS_APPCOMMAND.get(value)
+        if setting is None:
+            raise AvrCommandError(f"Value {value} not known for Dynamic Volume")
+        cmd = attr.evolve(
+            AppCommands.SetAudysseyDynamicvol,
+            param_list=(AppCommandCmdParam(name="dynamicvol", text=setting),),
+        )
+        await self._async_set_audyssey(cmd)
+
+    async def async_toggle_dynamic_eq(self) -> None:
+        """Toggle DynamicEQ."""
+        if self._dynamiceq:
+            await self.async_dynamiceq_off()
+        else:
+            await self.async_dynamiceq_on()
+
+
+def audyssey_factory(instance: DenonAVRFoundation) -> DenonAVRAudyssey:
+    """Create  DenonAVRAudyssey at receiver instances."""
+    # pylint: disable=protected-access
+    new = DenonAVRAudyssey(device=instance._device)
+    return new
---
+++ packages/denonavr/denonavr-1.0.1.tar.gz/denonavr-1.0.1/denonavr/soundmode.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module implements the handler for sound mode of Denon AVR receivers.
+
+:copyright: (c) 2021 by Oliver Goetz.
+:license: MIT, see LICENSE for more details.
+"""
+
+import asyncio
+import logging
+from collections.abc import Hashable
+from copy import deepcopy
+from typing import Dict, List, Optional
+
+import attr
+
+from .appcommand import AppCommands
+from .const import ALL_ZONE_STEREO, DENON_ATTR_SETATTR, SOUND_MODE_MAPPING
+from .exceptions import AvrCommandError, AvrIncompleteResponseError, AvrProcessingError
+from .foundation import DenonAVRFoundation
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def convert_sound_mode(value: Optional[str]) -> Optional[str]:
+    """Remove multiple spaces from value."""
+    if value is None:
+        return value
+    return " ".join(str(value).split())
+
+
+def sound_mode_map_factory() -> Dict[str, List]:
+    """Construct sound_mode map."""
+    sound_mode_map = {}
+    for matched_mode, sublist in SOUND_MODE_MAPPING.items():
+        sound_mode_map[matched_mode] = [convert_sound_mode(i) for i in sublist]
+    return sound_mode_map
+
+
+def sound_mode_rev_map_factory(instance: "DenonAVRSoundMode") -> Dict[str, str]:
+    """
+    Construct the sound_mode_rev_map.
+
+    Reverse the key value structure. The sound_mode_rev_map is bigger,
+    but allows for direct matching using a dictionary key access.
+    The sound_mode_map is uses externally to set this dictionary
+    because that has a nicer syntax.
+    """
+    mode_map = list(
+        instance._sound_mode_map.items()  # pylint: disable=protected-access
+    )
+    mode_map_rev = {}
+    for matched_mode, sublist in mode_map:
+        for raw_mode in sublist:
+            mode_map_rev[convert_sound_mode(raw_mode)] = matched_mode
+    return mode_map_rev
+
+
+@attr.s(auto_attribs=True, on_setattr=DENON_ATTR_SETATTR)
+class DenonAVRSoundMode(DenonAVRFoundation):
+    """This class implements sound mode functions of Denon AVR receiver."""
+
+    _support_sound_mode: Optional[bool] = attr.ib(
+        converter=attr.converters.optional(bool), default=None
+    )
+    _sound_mode_raw: Optional[str] = attr.ib(
+        converter=attr.converters.optional(convert_sound_mode), default=None
+    )
+    _sound_mode_map: Dict[str, list] = attr.ib(
+        validator=attr.validators.deep_mapping(
+            attr.validators.instance_of(str),
+            attr.validators.deep_iterable(
+                attr.validators.instance_of(str), attr.validators.instance_of(list)
+            ),
+            attr.validators.instance_of(dict),
+        ),
+        default=attr.Factory(sound_mode_map_factory),
+        init=False,
+    )
+    _sound_mode_map_rev: Dict[str, str] = attr.ib(
+        validator=attr.validators.deep_mapping(
+            attr.validators.instance_of(str),
+            attr.validators.instance_of(str),
+            attr.validators.instance_of(dict),
+        ),
+        default=attr.Factory(sound_mode_rev_map_factory, takes_self=True),
+        init=False,
+    )
+    _setup_lock: asyncio.Lock = attr.ib(default=attr.Factory(asyncio.Lock))
+    _appcommand_active: bool = attr.ib(converter=bool, default=True, init=False)
+
+    # Update tags for attributes
+    # AppCommand.xml interface
+    appcommand_attrs = {AppCommands.GetSurroundModeStatus: None}
+    # Status.xml interface
+    status_xml_attrs_01 = {"_sound_mode_raw": "./selectSurround/value"}
+    status_xml_attrs_02 = {"_sound_mode_raw": "./SurrMode/value"}
+
+    async def async_setup(self) -> None:
+        """Ensure that the instance is initialized."""
+        async with self._setup_lock:
+            _LOGGER.debug("Starting sound mode setup")
+
+            # The first update determines if sound mode is supported
+            await self.async_update_sound_mode()
+
+            if self._support_sound_mode and self._appcommand_active:
+                # Add tags for a potential AppCommand.xml update
+                for tag in self.appcommand_attrs:
+                    self._device.api.add_appcommand_update_tag(tag)
+
+            self._device.telnet_api.register_callback(
+                "MS", self._async_soundmode_callback
+            )
+
+            self._is_setup = True
+            _LOGGER.debug("Finished sound mode setup")
+
+    async def _async_soundmode_callback(
+        self, zone: str, event: str, parameter: str
+    ) -> None:
+        """Handle a sound mode change event."""
+        if self._device.zone != zone:
+            return
+
+        self._sound_mode_raw = parameter
+
+    async def async_update(
+        self, global_update: bool = False, cache_id: Optional[Hashable] = None
+    ) -> None:
+        """Update sound mode asynchronously."""
+        _LOGGER.debug("Starting sound mode update")
+        # Ensure instance is setup before updating
+        if not self._is_setup:
+            await self.async_setup()
+
+        # Update state
+        await self.async_update_sound_mode(
+            global_update=global_update, cache_id=cache_id
+        )
+        _LOGGER.debug("Finished sound mode update")
+
+    async def async_update_sound_mode(
+        self, global_update: bool = False, cache_id: Optional[Hashable] = None
+    ):
+        """Update sound mode status of device."""
+        if self._device.use_avr_2016_update is None:
+            raise AvrProcessingError(
+                "Device is not setup correctly, update method not set"
+            )
+
+        if self._is_setup and not self._support_sound_mode:
+            return
+
+        if self._device.use_avr_2016_update and self._appcommand_active:
+            try:
+                await self.async_update_attrs_appcommand(
+                    self.appcommand_attrs,
+                    global_update=global_update,
+                    cache_id=cache_id,
+                )
+            except (AvrProcessingError, AvrIncompleteResponseError):
+                self._appcommand_active = False
+                _LOGGER.debug(
+                    "Appcommand.xml does not support Sound mode. "
+                    "Testing status.xml interface next"
+                )
+            else:
+                if not self._is_setup:
+                    self._support_sound_mode = True
+                    _LOGGER.info("Sound mode supported")
+                return
+
+        urls = [self._device.urls.status, self._device.urls.mainzone]
+        # There are two different options of sound mode tags
+        try:
+            await self.async_update_attrs_status_xml(
+                self.status_xml_attrs_01, urls, cache_id=cache_id
+            )
+        except AvrProcessingError:
+            try:
+                await self.async_update_attrs_status_xml(
+                    self.status_xml_attrs_02, urls, cache_id=cache_id
+                )
+            except AvrProcessingError:
+                self._support_sound_mode = False
+                _LOGGER.info("Sound mode not supported")
+                return
+
+        if not self._is_setup:
+            self._support_sound_mode = True
+            _LOGGER.info("Sound mode supported")
+
+    def match_sound_mode(self) -> Optional[str]:
+        """Match the raw_sound_mode to its corresponding sound_mode."""
+        if self._sound_mode_raw is None:
+            return None
+        smr_up = self._sound_mode_raw.upper()
+        try:
+            sound_mode = self._sound_mode_map_rev[smr_up]
+        except KeyError:
+            # Estimate sound mode for unclassified input
+            if smr_up.find("DTS") != -1:
+                self._sound_mode_map["DTS SURROUND"].append(smr_up)
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', assuming 'DTS SURROUND'.",
+                    smr_up,
+                )
+            elif smr_up.find("DOLBY") != -1:
+                self._sound_mode_map["DOLBY DIGITAL"].append(smr_up)
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', assuming 'DOLBY DIGITAL'.",
+                    smr_up,
+                )
+            elif smr_up.find("MUSIC") != -1:
+                self._sound_mode_map["MUSIC"].append(smr_up)
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', assuming 'MUSIC'.", smr_up
+                )
+            elif smr_up.find("AURO") != -1:
+                self._sound_mode_map["AURO3D"].append(smr_up)
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', assuming 'AURO3D'.", smr_up
+                )
+            elif smr_up.find("MOVIE") != -1 or smr_up.find("CINEMA") != -1:
+                self._sound_mode_map["MOVIE"].append(smr_up)
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', assuming 'MOVIE'.", smr_up
+                )
+            else:
+                self._sound_mode_map[smr_up] = [smr_up]
+                _LOGGER.warning(
+                    "Not able to match sound mode: '%s', returning raw sound mode.",
+                    smr_up,
+                )
+            self._sound_mode_map_rev = sound_mode_rev_map_factory(self)
+            sound_mode = self._sound_mode_map_rev[smr_up]
+
+        return sound_mode
+
+    async def _async_set_all_zone_stereo(self, zst_on: bool) -> None:
+        """
+        Set All Zone Stereo option on the device.
+
+        Calls command to activate/deactivate the mode
+        """
+        command_url = self._device.urls.command_set_all_zone_stereo
+        telnet_command = self._device.telnet_commands.command_set_all_zone_stereo
+        if zst_on:
+            command_url += "ZST ON"
+            telnet_command += "ZST ON"
+        else:
+            command_url += "ZST OFF"
+            telnet_command += "ZST OFF"
+        if self._device.telnet_available:
+            await self._device.telnet_api.async_send_commands(telnet_command)
+        else:
+            await self._device.api.async_get_command(command_url)
+
+    ##############
+    # Properties #
+    ##############
+    @property
+    def support_sound_mode(self) -> Optional[bool]:
+        """Return True if sound mode is supported."""
+        return self._support_sound_mode
+
+    @property
+    def sound_mode(self) -> Optional[str]:
+        """Return the matched current sound mode as a string."""
+        sound_mode_matched = self.match_sound_mode()
+        return sound_mode_matched
+
+    @property
+    def sound_mode_list(self) -> List[str]:
+        """Return a list of available sound modes as string."""
+        return list(self._sound_mode_map.keys())
+
+    @property
+    def sound_mode_map(self) -> Dict[str, str]:
+        """Return a dict of available sound modes with their mapping values."""
+        return deepcopy(self._sound_mode_map)
+
+    @property
+    def sound_mode_map_rev(self) -> Dict[str, str]:
+        """Return a dict to map each sound_mode_raw to matching sound_mode."""
+        return deepcopy(self._sound_mode_map_rev)
+
+    @property
+    def sound_mode_raw(self) -> Optional[str]:
+        """Return the current sound mode as string as received from the AVR."""
+        return self._sound_mode_raw
+
+    ##########
+    # Setter #
+    ##########
+    async def async_set_sound_mode(self, sound_mode: str) -> None:
+        """
+        Set sound_mode of device.
+
+        Valid values depend on the device and should be taken from
+        "sound_mode_list".
+        """
+        if sound_mode not in self.sound_mode_list:
+            raise AvrCommandError(f"{sound_mode} is not a valid sound mode")
+
+        if sound_mode == ALL_ZONE_STEREO:
+            await self._async_set_all_zone_stereo(True)
+            return
+
+        if self.sound_mode == ALL_ZONE_STEREO:
+            await self._async_set_all_zone_stereo(False)
+        # For selection of sound mode other names then at receiving sound modes
+        # have to be used
+        # Therefore source mapping is needed to get sound_mode
+        # Create command URL and send command via HTTP GET
+        command_url = self._device.urls.command_sel_sound_mode + sound_mode
+        telnet_command = (
+            self._device.telnet_commands.command_sel_sound_mode + sound_mode
+        )
+        # sent command
+        if self._device.telnet_available:
+            await self._device.telnet_api.async_send_commands(telnet_command)
+        else:
+            await self._device.api.async_get_command(command_url)
+
+
+def sound_mode_factory(instance: DenonAVRFoundation) -> DenonAVRSoundMode:
+    """Create DenonAVRSoundMode at receiver instances."""
+    # pylint: disable=protected-access
+    new = DenonAVRSoundMode(device=instance._device)
+    return new
