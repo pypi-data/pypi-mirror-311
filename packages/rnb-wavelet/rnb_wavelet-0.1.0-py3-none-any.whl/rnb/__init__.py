fcf2d2611eb6a0279ca304a05f4e6be4c65a8a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:30 +0000

    Add package rnb-wavelet/0.1.0/rnb_wavelet-0.1.0-py3-none-any.whl

---
+++ packages/rnb-wavelet/rnb_wavelet-0.1.0-py3-none-any.whl/rnb/__init__.py
@@ -0,0 +1,7 @@
+# rnb/__init__.py
+
+# Import the rnbwavelet class from rnbun.py
+from .rnbun import rnbwavelet
+
+# Define what is accessible when using `from rnb import ...`
+__all__ = ['rnbwavelet']
