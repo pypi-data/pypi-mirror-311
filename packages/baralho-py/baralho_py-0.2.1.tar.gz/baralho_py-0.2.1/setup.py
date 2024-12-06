255068796ec17b88d174b801473266c88fe10bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:15 +0000

    Add package baralho-py/0.2.1/baralho_py-0.2.1.tar.gz

---
+++ packages/baralho-py/baralho_py-0.2.1.tar.gz/baralho_py-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baralho-py",  # Nome do pacote
+    version="0.2.1",  # Versão inicial
+    author="Arthur Felipe",
+    author_email="arthurfelipe5567@gmail.com",
+    description="Jogo simples de batalha.",
+    long_description="Jogo simples de batalha.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/arthurfaraujo/ed",  # URL do repositório (opcional)
+    packages=find_packages(),  # Encontra automaticamente subpacotes
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # Dependências do pacote (ex: 'requests', 'numpy')
+    ],
+    entry_points={
+      "console_scripts": ["baralho-py=baralho_py.main:main"]
+    }
+)
