7fb6eedbd8d0da49f14e8dcd4b0c343c544dfdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:33 +0000

    Add package truefoundry/0.5.0rc6/truefoundry-0.5.0rc6.tar.gz

---
+++ packages/truefoundry/truefoundry-0.5.0rc6.tar.gz/truefoundry-0.5.0rc6/truefoundry/ml/__init__.py
@@ -0,0 +1,76 @@
+from truefoundry.ml.autogen.client.models.library_name import LibraryName
+from truefoundry.ml.enums import (
+    DataSlice,
+    FileFormat,
+    ModelFramework,
+    ModelType,
+    ViewType,
+)
+from truefoundry.ml.exceptions import MlFoundryException
+from truefoundry.ml.log_types import Image, Plot
+from truefoundry.ml.log_types.artifacts.artifact import (
+    ArtifactPath,
+    ArtifactVersion,
+    BlobStorageDirectory,
+)
+from truefoundry.ml.log_types.artifacts.dataset import DataDirectory, DataDirectoryPath
+from truefoundry.ml.log_types.artifacts.model import (
+    ModelVersion,
+)
+from truefoundry.ml.logger import init_logger
+from truefoundry.ml.mlfoundry_api import get_client
+from truefoundry.ml.mlfoundry_run import MlFoundryRun
+from truefoundry.ml.model_framework import (
+    FastAIFramework,
+    GluonFramework,
+    H2OFramework,
+    KerasFramework,
+    LightGBMFramework,
+    ModelFrameworkType,
+    ONNXFramework,
+    PaddleFramework,
+    PyTorchFramework,
+    SklearnFramework,
+    SpaCyFramework,
+    StatsModelsFramework,
+    TensorFlowFramework,
+    TransformersFramework,
+    XGBoostFramework,
+)
+
+__all__ = [
+    "ArtifactPath",
+    "ArtifactVersion",
+    "BlobStorageDirectory",
+    "DataDirectory",
+    "DataDirectoryPath",
+    "DataSlice",
+    "FileFormat",
+    "Image",
+    "MlFoundryRun",
+    "MlFoundryException",
+    "ModelFramework",
+    "ModelType",
+    "ModelVersion",
+    "Plot",
+    "ViewType",
+    "get_client",
+    "FastAIFramework",
+    "GluonFramework",
+    "H2OFramework",
+    "KerasFramework",
+    "LightGBMFramework",
+    "ONNXFramework",
+    "PaddleFramework",
+    "PyTorchFramework",
+    "SklearnFramework",
+    "SpaCyFramework",
+    "StatsModelsFramework",
+    "TensorFlowFramework",
+    "TransformersFramework",
+    "XGBoostFramework",
+    "LibraryName",
+    "ModelFrameworkType",
+]
+
+init_logger()
