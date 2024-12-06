853fed24f064b3c42ae77a8707a900389d723c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:03 +0000

    Add package digiaaaa/0.1.0/digiaaaa-0.1.0.tar.gz

---
+++ packages/digiaaaa/digiaaaa-0.1.0.tar.gz/digiaaaa-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="digiaaaa",  # Ensure this name is unique on PyPI
+    version="0.1.0",
+    description="A simple calculator package",
+    author="Akhi",
+    author_email="akhildevops15@gmail.com",
+    packages=find_packages(),  # Automatically include the 'calku' package
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
