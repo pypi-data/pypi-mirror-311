84cd08e2d9e562e8f4618867d923bd8a9c21b7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:31 +0000

    Add package scikit-infra/0.0.2/scikit_infra-0.0.2.tar.gz

---
+++ packages/scikit-infra/scikit_infra-0.0.2.tar.gz/scikit_infra-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='叶璨铭 (Ye Canming)',
+    author_email='ycm24@mails.tsinghua.edu.cn',
+    description='所谓大学者，非谓有大楼之谓也，有大师之谓也。',
+    install_requires=['scholarly_infrastructure'],
+    long_description='This library is an alias for the `scholarly_infrastructure <https://pypi.python.org/pypi/scholarly_infrastructure/>`_ library. See `scholarly_infrastructure <https://pypi.python.org/pypi/scholarly_infrastructure/>`_ for more information.',
+    maintainer='叶璨铭 (Ye Canming)',
+    maintainer_email='ycm24@mails.tsinghua.edu.cn',
+    name='scikit-infra',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/scholarly_infrastructure/',
+    version='0.0.2',
+    zip_safe=False,
+)
