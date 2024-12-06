f3bbe1923bba0fd140d769f3821151430635d03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:22 +0000

    Add package llm-scratch/0.1.3/llm_scratch-0.1.3-py3-none-any.whl

---
+++ packages/llm-scratch/llm_scratch-0.1.3-py3-none-any.whl/llm_scratch/__init__.py
@@ -0,0 +1,9 @@
+from .llm_scratch import perplexity, haiku, opus, gpt4, llama_cpp, meta_llama, aiplayground, add_file, write_code
+
+from .raga import search, research, read_urls, find_relevant_content, get_plain_text_from_url, llm_answer
+
+from .image import image_prompt, llava_image_prompt, extract_text, kosmos, yolo, eagle, eagle_drop, flux, flux_drop
+
+from .console import interactive_table
+
+from .voice import text_to_voice
