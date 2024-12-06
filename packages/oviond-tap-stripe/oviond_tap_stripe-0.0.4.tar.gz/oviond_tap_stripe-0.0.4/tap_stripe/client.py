f7604c2204323da64e7f2f226a1087f0119b46a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:40 +0000

    Add package oviond-tap-stripe/0.0.4/oviond_tap_stripe-0.0.4.tar.gz

---
+++ packages/oviond-tap-stripe/oviond_tap_stripe-0.0.4.tar.gz/oviond_tap_stripe-0.0.4/tap_stripe/client.py
@@ -0,0 +1,79 @@
+"""REST client handling, including StripeStream base class."""
+
+from __future__ import annotations
+from typing import Any, Iterable, Optional
+from singer_sdk.streams import Stream
+from datetime import datetime, timezone
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+import stripe
+
+
+class StripeStream(Stream):
+    """Base class for Stripe streams."""
+
+    records_jsonpath = "$.data[*]"
+
+    @property
+    def stripe_client(self) -> stripe:
+        """Initialize and return a Stripe client."""
+        stripe.api_key = self.config["secret_key"]
+        return stripe
+
+    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
+        """Fetch records from the Stripe API using the client library."""
+        # Get the API key for tagging records
+        secret_key = self.config["secret_key"]
+
+        # Pagination parameters
+        params = {"limit": 100}
+
+        # Map specific resources explicitly
+        if self.path == "subscriptions":
+            params["status"] = "all"
+            api_function = self.stripe_client.Subscription
+        elif self.path == "balance_transactions":
+            params["expand"] = ["data.source"]
+            api_function = self.stripe_client.BalanceTransaction
+        elif self.path == "customers":
+            api_function = self.stripe_client.Customer
+        else:
+            raise ValueError(f"Unknown path: {self.path}")
+
+        # Include start_date from config if present
+        if "start_date" in self.config:
+            params["created"] = {
+                "gte": int(
+                    datetime.fromisoformat(self.config["start_date"]).timestamp()
+                )
+            }
+
+        # Include replication key value if present (for incremental sync)
+        if self.replication_key and self.get_starting_replication_key_value(context):
+            replication_value = self.get_starting_replication_key_value(context)
+            if isinstance(replication_value, str):
+                replication_value = int(
+                    datetime.fromisoformat(replication_value).timestamp()
+                )
+            params["created"]["gte"] = replication_value
+
+        # Fetch paginated data
+        while True:
+            response = api_function.list(**params)
+            records = response["data"]
+
+            # Sort records by `created` field
+            # sorted_records = sorted(records, key=lambda x: x["created"])
+
+            # Yield each record in the response
+            for record in records:
+                yield {**record, "profile_id": secret_key }
+
+            # Handle pagination: Check if there’s a `has_more` flag
+            if response.get("has_more"):
+                params["starting_after"] = records[-1]["id"]
+            else:
+                break
+
+    def parse_response(self, response: Any) -> Iterable[dict]:
+        """No parsing needed; data is directly handled in `get_records`."""
+        yield from extract_jsonpath(self.records_jsonpath, input=response)
---
+++ packages/oviond-tap-stripe/oviond_tap_stripe-0.0.4.tar.gz/oviond_tap_stripe-0.0.4/tap_stripe/streams.py
@@ -0,0 +1,163 @@
+"""Stream type classes for tap-stripe."""
+
+from __future__ import annotations
+from tap_stripe.client import StripeStream
+from singer_sdk import typing as th
+
+
+class SubscriptionsStream(StripeStream):
+    """Stream for fetching Stripe subscriptions."""
+
+    name = "stripe_subscriptions"
+    path = "subscriptions"
+    primary_keys = ["id"]
+    replication_key = "created"
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType),
+        th.Property("object", th.StringType),
+        th.Property("application", th.StringType),
+        th.Property("application_fee_percent", th.NumberType),
+        th.Property(
+            "automatic_tax",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property("billing_cycle_anchor", th.IntegerType),
+        th.Property("billing_thresholds", th.ObjectType(additional_properties=True)),
+        th.Property("cancel_at", th.IntegerType),
+        th.Property("cancel_at_period_end", th.BooleanType),
+        th.Property("canceled_at", th.IntegerType),
+        th.Property(
+            "cancellation_details",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property("collection_method", th.StringType),
+        th.Property("created", th.IntegerType),
+        th.Property("currency", th.StringType),
+        th.Property("current_period_end", th.IntegerType),
+        th.Property("current_period_start", th.IntegerType),
+        th.Property("customer", th.StringType),
+        th.Property("days_until_due", th.IntegerType),
+        th.Property("default_payment_method", th.StringType),
+        th.Property("default_source", th.StringType),
+        th.Property(
+            "default_tax_rates",
+            th.ArrayType(
+                th.ObjectType(additional_properties=True),
+            ),
+        ),
+        th.Property("description", th.StringType),
+        th.Property("discount", th.ObjectType(additional_properties=True)),
+        th.Property("discounts", th.ArrayType(th.StringType)),
+        th.Property("ended_at", th.IntegerType),
+        th.Property(
+            "invoice_settings",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property(
+            "items",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property("latest_invoice", th.StringType),
+        th.Property("livemode", th.BooleanType),
+        th.Property("metadata", th.ObjectType(additional_properties=True)),
+        th.Property("next_pending_invoice_item_invoice", th.StringType),
+        th.Property("on_behalf_of", th.StringType),
+        th.Property("pause_collection", th.ObjectType(additional_properties=True)),
+        th.Property(
+            "payment_settings",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property(
+            "pending_invoice_item_interval", th.ObjectType(additional_properties=True)
+        ),
+        th.Property("pending_setup_intent", th.StringType),
+        th.Property("pending_update", th.ObjectType(additional_properties=True)),
+        th.Property("schedule", th.ObjectType(additional_properties=True)),
+        th.Property("start_date", th.IntegerType),
+        th.Property("status", th.StringType),
+        th.Property("test_clock", th.ObjectType(additional_properties=True)),
+        th.Property("transfer_data", th.ObjectType(additional_properties=True)),
+        th.Property("trial_end", th.IntegerType),
+        th.Property(
+            "trial_settings",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property("trial_start", th.IntegerType),
+        th.Property("profile_id", th.StringType),
+    ).to_dict()
+
+
+class BalanceTransactionsStream(StripeStream):
+    """Stream for fetching Stripe balance transactions."""
+
+    name = "stripe_balance_transactions"
+    path = "balance_transactions"
+    primary_keys = ["id"]
+    replication_key = "created"
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType),
+        th.Property("object", th.StringType),
+        th.Property("amount", th.IntegerType),
+        th.Property("available_on", th.IntegerType),
+        th.Property("created", th.IntegerType),
+        th.Property("currency", th.StringType),
+        th.Property("description", th.StringType),
+        th.Property("exchange_rate", th.NumberType),
+        th.Property(
+            "fee_details",
+            th.ArrayType(th.ObjectType(additional_properties=True)),
+        ),
+        th.Property("fee", th.IntegerType),
+        th.Property("net", th.IntegerType),
+        th.Property(
+            "reporting_category",
+            th.StringType,
+        ),
+        th.Property("source", th.ObjectType(additional_properties=True)),
+        th.Property("status", th.StringType),
+        th.Property(
+            "type",
+            th.StringType,
+        ),
+        th.Property("profile_id", th.StringType),
+    ).to_dict()
+
+
+class CustomersStream(StripeStream):
+    """Stream for fetching Stripe customers."""
+
+    name = "stripe_customers"
+    path = "customers"
+    primary_keys = ["id"]
+    replication_key = "created"
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType),
+        th.Property("object", th.StringType),
+        th.Property("address", th.ObjectType(additional_properties=True)),
+        th.Property("balance", th.IntegerType),
+        th.Property("created", th.IntegerType),
+        th.Property("currency", th.StringType),
+        th.Property("default_source", th.StringType),
+        th.Property("delinquent", th.BooleanType),
+        th.Property("description", th.StringType),
+        th.Property("discount", th.ObjectType(additional_properties=True)),
+        th.Property("email", th.StringType),
+        th.Property("invoice_prefix", th.StringType),
+        th.Property(
+            "invoice_settings",
+            th.ObjectType(additional_properties=True),
+        ),
+        th.Property("livemode", th.BooleanType),
+        th.Property("metadata", th.ObjectType(additional_properties=True)),
+        th.Property("name", th.StringType),
+        th.Property("next_invoice_sequence", th.IntegerType),
+        th.Property("phone", th.StringType),
+        th.Property(
+            "preferred_locales",
+            th.ArrayType(th.StringType),
+        ),
+        th.Property("shipping", th.ObjectType(additional_properties=True)),
+        th.Property("tax_exempt", th.StringType),
+        th.Property("test_clock", th.StringType),
+        th.Property("profile_id", th.StringType),
+    ).to_dict()
