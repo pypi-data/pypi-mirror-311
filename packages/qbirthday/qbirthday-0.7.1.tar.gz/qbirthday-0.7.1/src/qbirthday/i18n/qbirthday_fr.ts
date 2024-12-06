7ff735e571479c6b383240983236dd1c3ba5eb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:13 +0000

    Add package qbirthday/0.7.1/qbirthday-0.7.1.tar.gz

---
+++ packages/qbirthday/qbirthday-0.7.1.tar.gz/qbirthday-0.7.1/src/qbirthday/i18n/qbirthday_fr.ts
@@ -0,0 +1,313 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../aboutdialog.py" line="32"/>
+        <source>translator-credit</source>
+        <translation>Jérôme Lafréchoux</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.py" line="34"/>
+        <source>There are no translations or the translator doesn&apos;t want to get credits for that.</source>
+        <translation>Il n&apos;y a pas de traduction, ou bien le traducteur n&apos;a pas souhaité être crédité pour cela.</translation>
+    </message>
+</context>
+<context>
+    <name>Backends</name>
+    <message>
+        <location filename="../backends/base.py" line="11"/>
+        <source>Missing {} library.</source>
+        <translation>La bibliothèque {} n&apos;est pas installée.</translation>
+    </message>
+</context>
+<context>
+    <name>CSVBackend</name>
+    <message>
+        <location filename="../backends/csv.py" line="66"/>
+        <source>Invalid row &quot;{row}&quot; in CSV file {filepath}</source>
+        <translation>Ligne invalide {row} dans le fichier CSV {filepath}</translation>
+    </message>
+    <message>
+        <location filename="../backends/csv.py" line="92"/>
+        <source>Can&apos;t open CSV file: {}</source>
+        <translation>Impossible d&apos;ouvrir le fichier CSV : {}</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="14"/>
+        <source>About QBirthday</source>
+        <translation>À propos de QBirthday</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="24"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="97"/>
+        <source>Authors</source>
+        <translation>Auteurs</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="111"/>
+        <source>Translators</source>
+        <translation>Traducteurs</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="125"/>
+        <source>Artists</source>
+        <translation>Graphistes</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="139"/>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+    <message>
+        <location filename="../ui/csvpreferencesdialog.ui" line="14"/>
+        <source>CSV database configuration</source>
+        <translation>Configuration de la base de données CSV</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="32"/>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="20"/>
+        <source>Export birthday list in iCalendar file:</source>
+        <translation>Exporter la liste d&apos;anniversaires dans le fichier iCalendar : </translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="41"/>
+        <source>Set alarms</source>
+        <translation>Créer des alarmes</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="57"/>
+        <source>days before each birthday</source>
+        <translation>jours avant chaque anniversaire</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="66"/>
+        <source>Custom ICS properties for VEVENT</source>
+        <translation>Propriétés ICS personnalisées pour VEVENT</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="76"/>
+        <source>Custom ICS properties for VALARM</source>
+        <translation>Propriétés ICS personnalisées pour VALARM</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="98"/>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="31"/>
+        <source>Host</source>
+        <translation>Hôte</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="38"/>
+        <source>Port</source>
+        <translation>Port</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="45"/>
+        <source>Username</source>
+        <translation>Nom d&apos;utilisateur</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="52"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="59"/>
+        <source>Database</source>
+        <translation>Base de données</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="81"/>
+        <source>Table</source>
+        <translation>Table</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="28"/>
+        <source>Past birthdays</source>
+        <translation>Anniversaires passés</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="72"/>
+        <source>Databases</source>
+        <translation>Bases de données</translation>
+    </message>
+    <message>
+        <location filename="../ui/icsexportpreferencesdialog.ui" line="14"/>
+        <source>iCalendar export configuration</source>
+        <translation>Configuration de l&apos;export iCalendar</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="82"/>
+        <source>iCalendar export</source>
+        <translation>Export iCalendar</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="58"/>
+        <source> days</source>
+        <translation> jours</translation>
+    </message>
+    <message>
+        <location filename="../ui/preferencesdialog.ui" line="51"/>
+        <source>Future birthdays</source>
+        <translation>Anniversaires à venir</translation>
+    </message>
+    <message>
+        <location filename="../ui/csvpreferencesdialog.ui" line="36"/>
+        <source>Delimiter</source>
+        <translation>Séparateur</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="88"/>
+        <source>&quot;Name&quot; column</source>
+        <translation>Colonne &quot;Nom&quot;</translation>
+    </message>
+    <message>
+        <location filename="../ui/mysqlpreferencesdialog.ui" line="95"/>
+        <source>&quot;Birthdate&quot; column</source>
+        <translation>Colonne &quot;Date de naissance&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>ICSExport</name>
+    <message>
+        <location filename="../ics_export.py" line="48"/>
+        <source>Birthday</source>
+        <translation>Anniversaire</translation>
+    </message>
+    <message>
+        <location filename="../ics_export.py" line="60"/>
+        <source>Birthday: </source>
+        <translation>Anniversaire : </translation>
+    </message>
+    <message>
+        <location filename="../ics_export.py" line="72"/>
+        <source>Can&apos;t write iCalendar file: {}</source>
+        <translation>Impossible d&apos;écrire le fichier iCalendar : {}</translation>
+    </message>
+</context>
+<context>
+    <name>LightningBackend</name>
+    <message>
+        <location filename="../backends/lightning.py" line="52"/>
+        <source>Cannot find Lightning profile directory</source>
+        <translation>Impossible de trouver le répertoire de profil de Lightning</translation>
+    </message>
+    <message>
+        <location filename="../backends/lightning.py" line="74"/>
+        <source>Error reading profile file {}</source>
+        <translation>Erreur à la lecture du fichier de profil {}</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="42"/>
+        <source>Birthdays</source>
+        <translation>Anniversaires</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="68"/>
+        <source>No birthdays in specified period</source>
+        <translation>Pas d&apos;anniversaire dans la période spécifiée</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="111"/>
+        <source>{} years old</source>
+        <translation>{} ans</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="130"/>
+        <source>Today</source>
+        <translation>Aujourd&apos;hui</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="137"/>
+        <source>Yesterday</source>
+        <translation>Hier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="139"/>
+        <source>{} days ago</source>
+        <translation>Il y a {} jours</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="147"/>
+        <source>Tomorrow</source>
+        <translation>Demain</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="149"/>
+        <source>{} days</source>
+        <translation>{} jours</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="20"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+</context>
+<context>
+    <name>MySQLBackend</name>
+    <message>
+        <location filename="../backends/mysql.py" line="134"/>
+        <source>Could not execute SQL query &apos;{}&apos;:
+{}</source>
+        <translation>Impossible d&apos;exécuter la requête SQL &apos;{}&apos; : {}</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../preferencesdialog.py" line="111"/>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+</context>
+<context>
+    <name>QtCore</name>
+    <message>
+        <location filename="../__about__.py" line="7"/>
+        <source>Birthday reminder</source>
+        <translation>Rappel d&apos;anniversaire</translation>
+    </message>
+</context>
+<context>
+    <name>StatusIcon</name>
+    <message>
+        <location filename="../statusicon.py" line="24"/>
+        <source>Refresh</source>
+        <translation>Recharger</translation>
+    </message>
+    <message>
+        <location filename="../statusicon.py" line="28"/>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+    <message>
+        <location filename="../statusicon.py" line="32"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../statusicon.py" line="36"/>
+        <source>Quit</source>
+        <translation>Sortir</translation>
+    </message>
+</context>
+</TS>
