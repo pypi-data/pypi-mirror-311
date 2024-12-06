96285ca58083d5da248a180c34cebe8ffc09ac35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:42 +0000

    Add package PyIndexRepo/0.1.3/pyindexrepo-0.1.3.tar.gz

---
+++ packages/PyIndexRepo/pyindexrepo-0.1.3.tar.gz/pyindexrepo-0.1.3/pyindexrepo/__init__.py
@@ -0,0 +1,13 @@
+from .pyindexrepo import (
+    RefractiveIndexLibrary,
+    Material,
+    TabulatedIndexData,
+    FormulaIndexData,
+    Specs,
+    ThermalDispersion,
+    ThermalExpansion,
+    YAMLRefractiveIndexData,
+    YAMLMaterialData,
+    YAMLLibraryData,
+    TemperatureRange,
+)
