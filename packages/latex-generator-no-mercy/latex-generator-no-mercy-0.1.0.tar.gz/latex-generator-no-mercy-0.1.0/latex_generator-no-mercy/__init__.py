1dfeadf4e8171c3a03798d1ca2bf121ae8be8e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:07 +0000

    Add package latex-generator-no-mercy/0.1.0/latex-generator-no-mercy-0.1.0.tar.gz

---
+++ packages/latex-generator-no-mercy/latex-generator-no-mercy-0.1.0.tar.gz/latex-generator-no-mercy-0.1.0/latex_generator-no-mercy/__init__.py
@@ -0,0 +1,1 @@
+from .latex_table_generator-no-mercy import generate_latex_table, generate_latex_image
---
+++ packages/latex-generator-no-mercy/latex-generator-no-mercy-0.1.0.tar.gz/latex-generator-no-mercy-0.1.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="latex-generator-no-mercy",
+    version="0.1.0",
+    author="Maxim Kirillov",
+    author_email="max79kirillov@gmail.com",
+    description="Генерация LaTeX таблиц и картинок",
+    url="https://github.com/noMercy-q/latex-generator-no-mercy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
