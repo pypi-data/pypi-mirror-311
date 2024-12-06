0f17328fe65e36de2389aae4235224ef78f4ca79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:27:03 +0000

    Add package fluentui/0.0.42/fluentui-0.0.42-py3-none-any.whl

---
+++ packages/fluentui/fluentui-0.0.42-py3-none-any.whl/fluentui/gui/__init__.py
@@ -0,0 +1,6 @@
+from .DesktopServices import DesktopServices
+from .action import Action
+from .item import StandardItem, TreeItem, ColumnField
+from .item_model import StandardModel, ColumnModel, RowModel, TreeModel
+from .painter import Font, FontDatabase, Color, Pixmap, Icon
+from .text_document import TextDocument
