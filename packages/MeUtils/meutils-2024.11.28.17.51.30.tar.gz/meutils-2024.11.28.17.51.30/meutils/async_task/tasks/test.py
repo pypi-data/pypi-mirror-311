bc4710daf88cfa29065b9f4cebfba2bb96385b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:18 +0000

    Add package MeUtils/2024.11.28.17.51.30/meutils-2024.11.28.17.51.30.tar.gz

---
+++ packages/MeUtils/meutils-2024.11.28.17.51.30.tar.gz/meutils-2024.11.28.17.51.30/meutils/async_task/tasks/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : kling
+# @Time         : 2024/11/28 16:18
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+from meutils.decorators.retry import retrying
+
+from meutils.async_task import worker, shared_task
+
+
+@shared_task
+def do_sync_task(**kwargs):
+    logger.debug("同步任务")
+    return kwargs
+
+
+@shared_task()
+async def ado_sync_task(**kwargs):
+    logger.debug("同步任务+协程")
+    return kwargs
+
+
+@shared_task
+@retrying(3)
+def proxy_task(**kwargs):
+    method = kwargs.pop('method', '')
+    url = kwargs.pop('url', '')
+
+    logger.debug(kwargs)
+
+    response = requests.request(method, url, **kwargs).json()
+
+    return response
+
+
+if __name__ == '__main__':
+    print(do_sync_task.apply_async(kwargs={'a': 1, 'b': 2}))
