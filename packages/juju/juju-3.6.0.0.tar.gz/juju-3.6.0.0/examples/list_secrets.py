3895f0714ea357b83eedc34b5714e87ef4a3a471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:45 +0000

    Add package juju/3.6.0.0/juju-3.6.0.0.tar.gz

---
+++ packages/juju/juju-3.6.0.0.tar.gz/juju-3.6.0.0/examples/list_secrets.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Canonical Ltd.
+# Licensed under the Apache V2, see LICENCE file for details.
+
+from juju import jasyncio
+from juju.model import Model
+
+
+async def main():
+    m = Model()
+    await m.connect()
+
+    secrets = await m.list_secrets()
+    print(secrets)
+    await m.disconnect()
+
+
+if __name__ == "__main__":
+    jasyncio.run(main())
