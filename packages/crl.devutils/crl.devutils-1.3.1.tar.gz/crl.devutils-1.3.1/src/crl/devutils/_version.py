4bc80debd66959e087a04dc994e848ab75b2fe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:11:00 +0000

    Add package crl.devutils/1.3.1/crl.devutils-1.3.1.tar.gz

---
+++ packages/crl.devutils/crl.devutils-1.3.1.tar.gz/crl.devutils-1.3.1/src/crl/devutils/_version.py
@@ -0,0 +1,11 @@
+__copyright__ = 'Copyright (C) 2019-2024, Nokia'
+VERSION = '1.3.1'
+GITHASH = '30bbb9b7217145ca5ffd4009b45349f89c97cb0a'
+
+
+def get_version():
+    return VERSION
+
+
+def get_githash():
+    return GITHASH
