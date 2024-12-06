8f05bf1d2e3d37b3f21be11c9017354f751496be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:47 +0000

    Add package Security-Layer/1.0.12/security_layer-1.0.12.tar.gz

---
+++ packages/Security-Layer/security_layer-1.0.12.tar.gz/security_layer-1.0.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='Security_Layer',
+    version='1.0.12',
+    author='Rawaa Ahmed',
+    author_email='rawaa.ahmed@ejada.com',
+    description='Details about the package',
+    packages=['Security_Layer_Package'],#, 'module_name.config'],
+    package_data={
+        # 'module_name': ['config/rules.yml'],
+    },
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests',
+        # 'dependency2',
+        # List any other dependencies your module requires
+    ],
+)
