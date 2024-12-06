89a5d987042c873400454bc5478f27e7a92a1e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:14 +0000

    Add package hyrex/0.7.0/hyrex-0.7.0-py3-none-any.whl

---
+++ packages/hyrex/hyrex-0.7.0-py3-none-any.whl/hyrex/dispatcher/postgres_lite_dispatcher.py
@@ -0,0 +1,154 @@
+from datetime import datetime
+from uuid import UUID
+
+from psycopg import RawCursor
+from psycopg.types.json import Json
+from psycopg_pool import ConnectionPool
+from uuid_extensions import uuid7
+
+from hyrex import constants, sql
+from hyrex.dispatcher.dispatcher import DequeuedTask, Dispatcher
+from hyrex.models import HyrexTask, StatusEnum
+
+
+# Single-threaded variant of Postgres dispatcher. (Slower enqueuing.)
+class PostgresLiteDispatcher(Dispatcher):
+    def __init__(self, conn_string: str):
+        super().__init__()
+        self.conn_string = conn_string
+        self.pool = ConnectionPool(
+            conn_string + "?keepalives=1&keepalives_idle=60&keepalives_interval=10",
+            open=True,
+            max_idle=300,
+        )
+
+    def mark_success(self, task_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.MARK_TASK_SUCCESS, [task_id])
+            conn.commit()
+
+    def mark_failed(self, task_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.MARK_TASK_FAILED, [task_id])
+            conn.commit()
+
+    def attempt_retry(self, task_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.CONDITIONALLY_RETRY_TASK, [task_id, uuid7()])
+            conn.commit()
+
+    def try_to_cancel_task(self, task_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.TRY_TO_CANCEL_TASK, [task_id])
+            conn.commit()
+
+    def task_canceled(self, task_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.TASK_CANCELED, [task_id])
+            conn.commit()
+
+    def dequeue(
+        self,
+        executor_id: UUID,
+        queue: str = constants.ANY_QUEUE,
+        num_tasks: int = 1,
+    ) -> list[DequeuedTask]:
+        dequeued_tasks = []
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                if queue == constants.ANY_QUEUE:
+                    cur.execute(sql.FETCH_TASK_FROM_ANY_QUEUE, [executor_id])
+                else:
+                    cur.execute(sql.FETCH_TASK, [queue, executor_id])
+                row = cur.fetchone()
+                if row:
+                    task_id, task_name, task_args = row
+                    dequeued_tasks.append(
+                        DequeuedTask(id=task_id, name=task_name, args=task_args)
+                    )
+        return dequeued_tasks
+
+    def enqueue(
+        self,
+        task: HyrexTask,
+    ):
+        task_data = (
+            task.id,
+            task.root_id,
+            task.task_name,
+            Json(task.args),
+            task.queue,
+            task.max_retries,
+            task.priority,
+        )
+
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(
+                    sql.ENQUEUE_TASK,
+                    task_data,
+                )
+            conn.commit()
+
+    def stop(self):
+        """
+        Stops the batching process and flushes remaining tasks.
+        """
+        self.logger.debug("Stopping dispatcher...")
+        self.pool.close()
+        self.logger.debug("Dispatcher stopped successfully!")
+
+    def get_task_status(self, task_id: UUID) -> StatusEnum:
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.GET_TASK_STATUS, [task_id])
+                result = cur.fetchone()
+                if result is None:
+                    raise ValueError(f"Task id {task_id} not found in DB.")
+                return result[0]
+
+    def register_executor(self, executor_id: UUID, executor_name: str, queue: str):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.REGISTER_EXECUTOR, [executor_id, executor_name, queue])
+            conn.commit()
+
+    def disconnect_executor(self, executor_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.DISCONNECT_EXECUTOR, [executor_id])
+            conn.commit()
+
+    def executor_heartbeat(self, executor_ids: list[UUID], timestamp: datetime):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.EXECUTOR_HEARTBEAT, [timestamp, executor_ids])
+            conn.commit()
+
+    def task_heartbeat(self, task_ids: list[UUID], timestamp: datetime):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.TASK_HEARTBEAT, [timestamp, task_ids])
+            conn.commit()
+
+    def get_tasks_up_for_cancel(self):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.GET_TASKS_UP_FOR_CANCEL)
+                return [row[0] for row in cur.fetchall()]
+
+    def mark_running_tasks_lost(self, executor_id: UUID):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.MARK_RUNNING_TASKS_LOST, [executor_id])
+
+    def save_result(self, task_id: UUID, result: str):
+        with self.pool.connection() as conn:
+            with RawCursor(conn) as cur:
+                cur.execute(sql.SAVE_RESULT, [task_id, result])
+            conn.commit()
---
+++ packages/hyrex/hyrex-0.7.0-py3-none-any.whl/hyrex/sql.py
@@ -0,0 +1,239 @@
+from hyrex import constants
+
+CREATE_HYREX_TASK_TABLE = """
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'statusenum' AND typnamespace = 'public'::regnamespace) THEN
+CREATE TYPE statusenum AS ENUM ('success', 'failed', 'running', 'queued', 'up_for_cancel', 'canceled', 'lost', 'waiting');
+END IF;
+END $$;
+
+create table if not exists hyrextask
+(
+    id              uuid       not null
+primary key,
+    root_id         uuid       not null,
+    parent_id       uuid,
+    task_name       varchar    not null,
+    args            json       not null,
+    queue           varchar    not null,
+    max_retries     smallint   not null,
+    priority        smallint   not null,
+    status          statusenum not null default 'queued'::statusenum,
+    attempt_number  smallint   not null default 0,
+    scheduled_start timestamp with time zone,
+    executor_id     uuid,
+    queued          timestamp with time zone default CURRENT_TIMESTAMP,
+    started         timestamp with time zone,
+    last_heartbeat  timestamp with time zone,
+    finished        timestamp with time zone
+);
+
+create index if not exists ix_hyrextask_task_name
+on hyrextask (task_name);
+
+create index if not exists ix_hyrextask_status
+on hyrextask (status);
+
+create index if not exists ix_hyrextask_queue
+on hyrextask (queue);
+
+create index if not exists ix_hyrextask_scheduled_start
+on hyrextask (scheduled_start);
+
+create index if not exists index_queue_status
+on hyrextask (status, queue, scheduled_start, task_name);
+"""
+
+CREATE_HYREX_RESULT_TABLE = """
+CREATE TABLE IF NOT EXISTS hyrextaskresult (
+    id SERIAL PRIMARY KEY,
+    task_id UUID REFERENCES hyrextask(id),
+    result JSON DEFAULT '{}'
+);
+"""
+
+CREATE_HYREX_EXECUTOR_TABLE = """
+create table if not exists hyrexexecutor
+(
+    id      uuid    not null
+primary key,
+    name    varchar not null,
+    queue   varchar not null,
+    started timestamp with time zone default CURRENT_TIMESTAMP,
+    last_heartbeat timestamp with time zone,
+    stopped timestamp with time zone
+);
+"""
+
+FETCH_TASK = """
+WITH next_task AS (
+    SELECT id 
+    FROM hyrextask
+    WHERE
+        queue = $1 AND
+        status = 'queued'
+    ORDER BY priority DESC, id
+    FOR UPDATE SKIP LOCKED
+    LIMIT 1
+)
+UPDATE hyrextask
+SET status = 'running', started = CURRENT_TIMESTAMP, last_heartbeat = CURRENT_TIMESTAMP, worker_id = $2
+FROM next_task
+WHERE hyrextask.id = next_task.id
+RETURNING hyrextask.id, hyrextask.task_name, hyrextask.args;
+"""
+
+FETCH_TASK_FROM_ANY_QUEUE = """
+WITH next_task AS (
+    SELECT id
+    FROM hyrextask
+    WHERE status = 'queued'
+    ORDER BY priority DESC, id
+    FOR UPDATE SKIP LOCKED
+    LIMIT 1
+)
+UPDATE hyrextask
+SET status = 'running', started = CURRENT_TIMESTAMP, last_heartbeat = CURRENT_TIMESTAMP, executor_id = $1
+FROM next_task
+WHERE hyrextask.id = next_task.id
+RETURNING hyrextask.id, hyrextask.task_name, hyrextask.args;
+"""
+
+CONDITIONALLY_RETRY_TASK = """
+WITH existing_task AS (
+    SELECT
+        root_id,
+        task_name,
+        args,
+        queue,
+        attempt_number,
+        max_retries,
+        priority
+    FROM hyrextask
+    WHERE id = $1
+      AND attempt_number < max_retries
+)
+INSERT INTO hyrextask (
+    id,
+    root_id,
+    queued,
+    status,
+    task_name,
+    args,
+    queue,
+    attempt_number,
+    max_retries,
+    priority
+)
+SELECT
+    $2 AS id,
+    root_id,
+    CURRENT_TIMESTAMP as queued,
+    'queued' AS status,
+    task_name,
+    args,
+    queue,
+    attempt_number + 1 AS attempt_number,
+    max_retries,
+    priority
+FROM existing_task;
+"""
+
+ENQUEUE_TASK = """
+INSERT INTO hyrextask (
+    id,
+    root_id,
+    task_name,
+    args,
+    queue,
+    max_retries,
+    priority
+) VALUES ($1, $2, $3, $4, $5, $6, $7);
+"""
+
+MARK_TASK_SUCCESS = """
+    UPDATE hyrextask 
+    SET status = 'success', finished = CURRENT_TIMESTAMP
+    WHERE id = $1 AND status = 'running'
+"""
+
+MARK_TASK_FAILED = """
+    UPDATE hyrextask 
+    SET status = 'failed', finished = CURRENT_TIMESTAMP
+    WHERE id = $1 AND status = 'failed'
+"""
+
+RESET_OR_CANCEL_TASK = """
+   UPDATE hyrextask 
+   SET status = CASE 
+                   WHEN status = 'up_for_cancel' THEN 'canceled'::statusenum 
+                   ELSE 'queued'::statusenum 
+               END, 
+       executor_id = CASE 
+                     WHEN status = 'up_for_cancel' THEN executor_id  -- Keep the current value
+                     ELSE NULL
+                   END,
+       started = CASE 
+                   WHEN status = 'up_for_cancel' THEN started  -- Keep the current value
+                   ELSE NULL
+                 END
+   WHERE id = $1
+"""
+
+TRY_TO_CANCEL_TASK = """
+    UPDATE hyrextask
+    SET status = CASE 
+                WHEN status = 'running' THEN 'up_for_cancel'::statusenum 
+                WHEN status = 'queued' THEN 'canceled'::statusenum
+                END
+    WHERE id = $1 AND status IN ('running', 'queued');
+"""
+
+TASK_CANCELED = """
+    UPDATE hyrextask
+    SET status = 'canceled'
+    WHERE id = $1 AND status = 'up_for_cancel';
+"""
+
+GET_TASKS_UP_FOR_CANCEL = """
+    SELECT id FROM hyrextask WHERE status = 'up_for_cancel'
+"""
+
+GET_TASK_STATUS = """
+    SELECT status FROM hyrextask WHERE id = $1
+"""
+
+TASK_HEARTBEAT = """
+    UPDATE hyrextask 
+    SET last_heartbeat = $1 
+    WHERE id = ANY($2)
+"""
+
+EXECUTOR_HEARTBEAT = """
+    UPDATE hyrexexecutor 
+    SET last_heartbeat = $1 
+    WHERE id = ANY($2)
+"""
+
+REGISTER_EXECUTOR = """
+    INSERT INTO hyrexexecutor (id, name, queue, started, last_heartbeat)
+    VALUES ($1, $2, $3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+"""
+
+DISCONNECT_EXECUTOR = """
+    UPDATE hyrexexecutor
+    SET stopped = CURRENT_TIMESTAMP
+    WHERE id = $1 AND stopped IS NULL
+"""
+
+MARK_RUNNING_TASKS_LOST = """
+    UPDATE hyrextask
+    SET status = 'lost'
+    WHERE status = 'running' AND executor_id = $1
+"""
+
+SAVE_RESULT = """
+    INSERT INTO hyrextaskresult (task_id, result)
+    VALUES ($1, $2);
+"""
