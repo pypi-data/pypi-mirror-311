97a6a38a965ee43fc0b5cb21cc69a97fb410d492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:08 +0000

    Add package importutil/0.1.1/importutil-0.1.1.tar.gz

---
+++ packages/importutil/importutil-0.1.1.tar.gz/importutil-0.1.1/importutil/__init__.py
@@ -0,0 +1,40 @@
+import importlib.util
+import sys
+from pathlib import Path
+
+
+def is_package_installed(package_name: str) -> bool:
+    """
+    Check if a Python package is installed.
+
+    Args:
+        package_name (str): The name of the package to check.
+
+    Returns:
+        bool: True if the package is installed, False otherwise.
+    """
+    return importlib.util.find_spec(package_name) is not None
+
+
+def import_file(file: str):
+    file = Path(file).resolve()
+    path = str(file.parent)
+
+    original_sys_path = None
+
+    if path not in sys.path:
+        original_sys_path = sys.path.copy()
+        # append to front
+        sys.path = [path, *sys.path]
+
+    module = importlib.import_module(file.stem)
+
+    # restore sys path
+    if original_sys_path is not None:
+        sys.path = original_sys_path
+
+    return module
+
+
+def import_module(module_name: str):
+    return importlib.import_module(module_name)
