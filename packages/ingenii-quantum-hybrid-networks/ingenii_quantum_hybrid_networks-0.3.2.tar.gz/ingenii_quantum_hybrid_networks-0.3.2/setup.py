8f4ca7dac274c80e2443a1edf8a5dd698ec95044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:55 +0000

    Add package ingenii-quantum-hybrid-networks/0.3.2/ingenii_quantum_hybrid_networks-0.3.2.tar.gz

---
+++ packages/ingenii-quantum-hybrid-networks/ingenii_quantum_hybrid_networks-0.3.2.tar.gz/ingenii_quantum_hybrid_networks-0.3.2/setup.py
@@ -0,0 +1,26 @@
+import re
+from setuptools import setup, find_packages
+
+
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+regex = r"^Version:\s(\d+.\d+.\d+)$"
+match = re.search(regex, readme, re.MULTILINE)
+
+if match:
+    current_version = match.group(1)
+else:
+    raise Exception("Current version is missing from the README.md")
+
+with open("requirements.txt", "r") as r:
+    requirements = [p for p in r.read().split("\n") if p]
+    dependencies = [p.split("=")[0] for p in requirements]
+
+setup(
+    version=current_version,
+    packages=find_packages(include=["ingenii_quantum", "ingenii_quantum.*", "tests"]),
+    install_requires=requirements,
+    dependency_links=dependencies
+)
