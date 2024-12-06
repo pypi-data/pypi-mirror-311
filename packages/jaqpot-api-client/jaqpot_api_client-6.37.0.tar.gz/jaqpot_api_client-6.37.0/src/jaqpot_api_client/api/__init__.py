98135d425db4b5828c1ecb167da2311a404473c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:58 +0000

    Add package jaqpot-api-client/6.37.0/jaqpot_api_client-6.37.0.tar.gz

---
+++ packages/jaqpot-api-client/jaqpot_api_client-6.37.0.tar.gz/jaqpot_api_client-6.37.0/src/jaqpot_api_client/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from src.jaqpot_api_client.api.user_settings_api import UserSettingsApi
+from src.jaqpot_api_client.api.api_keys_api import ApiKeysApi
+from src.jaqpot_api_client.api.auth_api import AuthApi
+from src.jaqpot_api_client.api.dataset_api import DatasetApi
+from src.jaqpot_api_client.api.feature_api import FeatureApi
+from src.jaqpot_api_client.api.lead_api import LeadApi
+from src.jaqpot_api_client.api.model_api import ModelApi
+from src.jaqpot_api_client.api.organization_api import OrganizationApi
+from src.jaqpot_api_client.api.organization_invitation_api import OrganizationInvitationApi
+from src.jaqpot_api_client.api.user_api import UserApi
+from src.jaqpot_api_client.api.user_settings_api import UserSettingsApi
+
