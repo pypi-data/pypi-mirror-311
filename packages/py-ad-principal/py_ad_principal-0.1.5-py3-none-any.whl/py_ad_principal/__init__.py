7fd0f4be22aef3685076c15ec61a4f03f4586808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:30 +0000

    Add package py-ad-principal/0.1.5/py_ad_principal-0.1.5-py3-none-any.whl

---
+++ packages/py-ad-principal/py_ad_principal-0.1.5-py3-none-any.whl/py_ad_principal/__init__.py
@@ -0,0 +1,16 @@
+from .py_ad_principal import (
+    AuthenticationContextConfig,
+    ActiveDirectoryError,
+    ActiveDirectoryPrincipal,
+    AuthenticationResult,
+    AuthenticationContext
+)
+
+__version__ = "0.1.5"
+__all__ = [
+    "AuthenticationContextConfig",
+    "ActiveDirectoryError",
+    "ActiveDirectoryPrincipal",
+    "AuthenticationResult",
+    "AuthenticationContext",
+]
