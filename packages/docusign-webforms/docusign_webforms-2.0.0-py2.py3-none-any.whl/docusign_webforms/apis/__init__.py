8cff0c9fdd02cc05f2624a8b793864e3eef645f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:44 +0000

    Add package docusign-webforms/2.0.0/docusign_webforms-2.0.0-py2.py3-none-any.whl

---
+++ packages/docusign-webforms/docusign_webforms-2.0.0-py2.py3-none-any.whl/docusign_webforms/apis/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .form_instance_management_api import FormInstanceManagementApi
+from .form_management_api import FormManagementApi
