99dd90a470fbb41d598a9a3f55d23d059c83e940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:48 +0000

    Add package framelib/0.0.6/framelib-0.0.6-py3-none-any.whl

---
+++ packages/framelib/framelib-0.0.6-py3-none-any.whl/framelib/models.py
@@ -0,0 +1,175 @@
+"""
+data models
+"""
+
+import datetime
+from typing import Optional, Literal
+from pydantic import BaseModel
+
+
+# ---- frame message ----
+
+class CastId(BaseModel):
+    fid: int
+    hash: str
+
+
+class FrameAction(BaseModel):
+    url: str
+    buttonIndex: int
+    inputText: Optional[str] = None
+    state: Optional[str] = None
+    transactionId: Optional[str] = None
+    address: Optional[str] = None
+    castId: CastId
+
+
+class UntrustedData(FrameAction):
+    # note: this untrusted message seems to collapse the ValidatedDate and FrameAction fields
+    fid: int
+    messageHash: str
+    timestamp: int
+    network: int
+
+
+class TrustedData(BaseModel):
+    messageBytes: str
+
+
+class FrameMessage(BaseModel):
+    untrustedData: UntrustedData
+    trustedData: TrustedData
+
+
+# ---- frame transaction ----
+
+class EthTransactionParams(BaseModel):
+    abi: list[dict]
+    to: str
+    value: Optional[str]
+    data: Optional[str]
+
+
+class Transaction(BaseModel):
+    chainId: str
+    method: Literal['eth_sendTransaction']
+    params: EthTransactionParams
+
+
+# ---- frame error ----
+
+class FrameError(BaseModel):
+    message: str
+
+
+# ---- hub ----
+
+class ValidatedData(BaseModel):
+    type: str
+    fid: int
+    timestamp: datetime.datetime
+    network: str
+    frameActionBody: FrameAction
+
+
+class ValidatedMessage(BaseModel):
+    data: ValidatedData
+    hash: str
+    hashScheme: str
+    signature: str
+    signatureScheme: str
+    signer: str
+
+
+# ---- neynar ----
+
+class NeynarViewer(BaseModel):
+    following: bool
+    followed_by: bool
+
+
+class NeynarBio(BaseModel):
+    text: str
+    mentioned_profiles: Optional[list[str]] = []
+
+
+class NeynarProfile(BaseModel):
+    bio: NeynarBio
+
+
+class NeynarInteractor(BaseModel):
+    object: str
+    fid: int
+    username: str
+    display_name: str
+    custody_address: Optional[str] = None
+    pfp_url: str
+    profile: NeynarProfile
+    follower_count: int
+    following_count: int
+    verifications: list[str]
+    viewer_context: Optional[NeynarViewer] = None
+
+
+class NeynarButton(BaseModel):
+    title: Optional[str] = None
+    index: int
+    action_type: Optional[str] = None
+
+
+class NeynarInput(BaseModel):
+    text: str
+
+
+class NeynarState(BaseModel):
+    serialized: str
+
+
+class NeynarTransaction(BaseModel):
+    hash: str
+
+
+class NeynarValidatedMessage(BaseModel):
+    object: str
+    interactor: NeynarInteractor
+    tapped_button: NeynarButton
+    input: Optional[NeynarInput] = None
+    state: Optional[NeynarState] = None
+    url: str
+    cast: dict
+    timestamp: datetime.datetime
+    transaction: Optional[NeynarTransaction] = None
+
+
+# ---- warpcast ----
+
+class Pfp(BaseModel):
+    url: str
+    verified: bool
+
+
+class WarpBio(BaseModel):
+    text: str
+    mentions: Optional[list[str]] = []
+    channelMentions: Optional[list[str]] = []
+
+
+class WarpLocation(BaseModel):
+    placeId: str
+    description: str
+
+
+class WarpProfile(BaseModel):
+    bio: WarpBio
+    location: WarpLocation
+
+
+class User(BaseModel):
+    fid: int
+    username: Optional[str] = None
+    displayName: str
+    pfp: Optional[Pfp] = None
+    profile: WarpProfile
+    followerCount: int
+    followingCount: int
+    activeOnFcNetwork: bool
---
+++ packages/framelib/framelib-0.0.6-py3-none-any.whl/test/test_mint.py
@@ -0,0 +1,22 @@
+"""
+test cases for frame minting logic
+"""
+
+# lib
+import json
+import pytest
+from flask import Flask
+
+# src
+from framelib import mint
+
+
+class TestTransaction(object):
+
+    def test_mint(self):
+        target = mint(7777777, '0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df')
+        assert target == 'eip155:7777777:0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df'
+
+    def test_mint_with_id(self):
+        target = mint(7777777, '0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df', token_id=1234)
+        assert target == 'eip155:7777777:0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df:1234'
