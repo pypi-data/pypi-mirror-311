9f934a345bc000f5d78a8c3703e40df0de0d48e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:59 +0000

    Add package care-sm-toolkit/0.1.2/CARE-SM-Toolkit-0.1.2.tar.gz

---
+++ packages/care-sm-toolkit/CARE-SM-Toolkit-0.1.2.tar.gz/CARE-SM-Toolkit-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="CARE-SM-Toolkit",
+    version="0.1.2",
+    packages=["toolkit"],
+    author="Pablo Alarcón Moreno",
+    author_email="pabloalarconmoreno@gmail.com",
+    url="https://github.com/CARE-SM/CARE-SM-Toolkit",
+    description="CARE-SM Toolkit",
+    license="MIT",
+    keywords=["EJP-RD","EJP","FAIR-in-a-box","Fiab","CARE-SM"]
+)
