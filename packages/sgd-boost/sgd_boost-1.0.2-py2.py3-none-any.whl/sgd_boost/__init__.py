14a6db1f3cc52fee50ec3539fd589a9e56d1c915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:51 +0000

    Add package sgd-boost/1.0.2/sgd_boost-1.0.2-py2.py3-none-any.whl

---
+++ packages/sgd-boost/sgd_boost-1.0.2-py2.py3-none-any.whl/sgd_boost/__init__.py
@@ -0,0 +1,11 @@
+'''
+Author: 
+Date: 2024-11-22 01:11:52
+LastEditTime: 2024-11-22 01:11:54
+LastEditors: 
+Description: 
+FilePath: /SGD-Boost/sgd_boost/__init__.py
+'''
+from .sgd_boost import SGD_boost
+
+__all__ = ['SGD_boost']
