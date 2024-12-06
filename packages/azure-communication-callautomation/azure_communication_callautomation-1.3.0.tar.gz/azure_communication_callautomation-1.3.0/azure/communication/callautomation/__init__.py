8f94c06a36e881cc26bcfd4dbcc9547256993dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:16 +0000

    Add package azure-communication-callautomation/1.3.0/azure_communication_callautomation-1.3.0.tar.gz

---
+++ packages/azure-communication-callautomation/azure_communication_callautomation-1.3.0.tar.gz/azure_communication_callautomation-1.3.0/azure/communication/callautomation/__init__.py
@@ -0,0 +1,120 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import warnings
+
+from ._version import VERSION
+from ._call_automation_client import CallAutomationClient
+from ._call_connection_client import CallConnectionClient
+from ._models import (
+    CallConnectionProperties,
+    FileSource,
+    TextSource,
+    SsmlSource,
+    RecognitionChoice,
+    CallParticipant,
+    RecordingProperties,
+    AddParticipantResult,
+    RemoveParticipantResult,
+    TransferCallResult,
+    ChannelAffinity,
+    MuteParticipantResult,
+    SendDtmfTonesResult,
+    CancelAddParticipantOperationResult,
+    CallInvite,
+    ServerCallLocator,
+    GroupCallLocator,
+    RoomCallLocator,
+    AzureBlobContainerRecordingStorage,
+    AzureCommunicationsRecordingStorage
+)
+from ._shared.models import (
+    CommunicationIdentifier,
+    PhoneNumberIdentifier,
+    MicrosoftTeamsAppIdentifier,
+    MicrosoftTeamsUserIdentifier,
+    CommunicationUserIdentifier,
+    CommunicationIdentifierKind,
+    CommunicationCloudEnvironment,
+    UnknownIdentifier,
+)
+from ._generated.models._enums import (
+    CallRejectReason,
+    RecordingContent,
+    RecordingChannel,
+    RecordingFormat,
+    RecordingStorageKind,
+    RecordingKind,
+    RecognizeInputType,
+    DtmfTone,
+    CallConnectionState,
+    RecordingState,
+    VoiceKind
+)
+
+__all__ = [
+    # clients
+    "CallAutomationClient",
+    "CallConnectionClient",
+
+    # models for input
+    "FileSource",
+    "TextSource",
+    "SsmlSource",
+    "RecognitionChoice",
+    "ChannelAffinity",
+    "AzureBlobContainerRecordingStorage",
+    "AzureCommunicationsRecordingStorage",
+
+    # models for output
+    "CallConnectionProperties",
+    "CallParticipant",
+    "RecordingProperties",
+    "AddParticipantResult",
+    "RemoveParticipantResult",
+    "TransferCallResult",
+    "MuteParticipantResult",
+    "SendDtmfTonesResult",
+    "CancelAddParticipantOperationResult",
+
+    # common ACS communication identifier
+    "CommunicationIdentifier",
+    "PhoneNumberIdentifier",
+    "MicrosoftTeamsAppIdentifier",
+    "MicrosoftTeamsUserIdentifier",
+    "CommunicationUserIdentifier",
+    "CommunicationIdentifierKind",
+    "CommunicationCloudEnvironment",
+    "UnknownIdentifier",
+
+    # enums
+    "CallRejectReason",
+    "RecordingContent",
+    "RecordingChannel",
+    "RecordingFormat",
+    "RecordingStorageKind",
+    "RecordingKind",
+    "RecognizeInputType",
+    "DtmfTone",
+    "CallConnectionState",
+    "RecordingState",
+    "VoiceKind",
+
+    # deprecated models
+    "CallInvite",
+    "ServerCallLocator",
+    "GroupCallLocator",
+    "RoomCallLocator",
+]
+__version__ = VERSION
+
+
+def __getattr__(name):
+    if name == 'MicrosoftBotIdentifier':
+        warnings.warn(f"{name} is deprecated and should not be used. Please use 'MicrosoftTeamsAppIdentifier' instead.",
+                       DeprecationWarning)
+        from ._shared.models import _MicrosoftBotIdentifier
+        return _MicrosoftBotIdentifier
+    raise AttributeError(f"module 'azure.communication.callautomation' has no attribute {name}")
