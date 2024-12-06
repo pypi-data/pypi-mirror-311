e01868a752c82d0c03703a689ad9b4cb6ea38b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:02 +0000

    Add package hypatia/0.5/hypatia-0.5-pp310-pypy310_pp73-macosx_10_15_x86_64.whl

---
+++ packages/hypatia/hypatia-0.5-pp310-pypy310_pp73-macosx_10_15_x86_64.whl/hypatia/text/tests/test_okapiindex.py
@@ -0,0 +1,179 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Okapi text index tests
+"""
+import unittest
+from unittest import mock
+
+
+class OkapiIndexTestBase:
+    # Subclasses must define '_getBTreesFamily'
+    def _getTargetClass(self):
+        from ..okapiindex import OkapiIndex
+        return OkapiIndex
+
+    def _makeOne(self):
+        from ..lexicon import Lexicon
+        from ..lexicon import Splitter
+        lexicon = Lexicon(Splitter())
+        return self._getTargetClass()(lexicon, family=self._getBTreesFamily())
+
+    def test_class_conforms_to_IIndexInjection(self):
+        from zope.interface.verify import verifyClass
+        from hypatia.interfaces import IIndexInjection
+        verifyClass(IIndexInjection, self._getTargetClass())
+
+    def test_instance_conforms_to_IIndexInjection(self):
+        from zope.interface.verify import verifyObject
+        from hypatia.interfaces import IIndexInjection
+        verifyObject(IIndexInjection, self._makeOne())
+
+    def test_class_conforms_to_IIndexStatistics(self):
+        from zope.interface.verify import verifyClass
+        from hypatia.interfaces import IIndexStatistics
+        verifyClass(IIndexStatistics, self._getTargetClass())
+
+    def test_instance_conforms_to_IIndexStatistics(self):
+        from zope.interface.verify import verifyObject
+        from hypatia.interfaces import IIndexStatistics
+        verifyObject(IIndexStatistics, self._makeOne())
+
+    def test_class_conforms_to_ILexiconBasedIndex(self):
+        from zope.interface.verify import verifyClass
+        from ..interfaces import ILexiconBasedIndex
+        verifyClass(ILexiconBasedIndex, self._getTargetClass())
+
+    def test_instance_conforms_to_ILexiconBasedIndex(self):
+        from zope.interface.verify import verifyObject
+        from ..interfaces import ILexiconBasedIndex
+        verifyObject(ILexiconBasedIndex, self._makeOne())
+
+    def test_class_conforms_to_IExtendedQuerying(self):
+        from zope.interface.verify import verifyClass
+        from ..interfaces import IExtendedQuerying
+        verifyClass(IExtendedQuerying, self._getTargetClass())
+
+    def test_instance_conforms_to_IExtendedQuerying(self):
+        from zope.interface.verify import verifyObject
+        from ..interfaces import IExtendedQuerying
+        verifyObject(IExtendedQuerying, self._makeOne())
+
+    def test_empty(self):
+        index = self._makeOne()
+        self.assertEqual(index._totaldoclen(), 0)
+
+    def test_index_doc_updates_totaldoclen(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+        index.index_doc(2, 'two three four')
+        self.assertEqual(index._totaldoclen(), 6)
+
+    def test_index_doc_existing_updates_totaldoclen(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+        index.index_doc(1, 'two three four')
+        self.assertEqual(index._totaldoclen(), 3)
+
+    def test_index_doc_upgrades_totaldoclen(self):
+        index = self._makeOne()
+
+        # Simulate old instances which didn't have Length attributes
+        index._totaldoclen = 0
+
+        index.index_doc(1, 'one two three')
+
+        self.assertEqual(index._totaldoclen(), 3)
+
+    def test__search_wids_non_empty_wids(self):
+        TEXT = 'one two three'
+        index = self._makeOne()
+        index.index_doc(1, TEXT )
+        wids = [index._lexicon._wids[x] for x in TEXT.split()]
+        relevances = index._search_wids(wids)
+        self.assertEqual(len(relevances), len(wids))
+        for relevance in relevances:
+            self.assertTrue(isinstance(relevance[0], index.family.IF.Bucket))
+            self.assertEqual(len(relevance[0]), 1)
+            self.assertTrue(isinstance(relevance[0][1], float))
+            self.assertTrue(isinstance(relevance[1], int))
+
+    def test__search_wids_non_empty_wids_multiple_docs(self):
+
+        NUM_DOCS = 23   # get past the DICT_CUTOFF size
+        TEXT = 'one two three'
+        family = self._getBTreesFamily()
+        index = self._makeOne()
+
+        for i in range(NUM_DOCS):
+            texts = [TEXT] * ((i % 5) + 1)
+            index.index_doc(i, " ".join(texts) )
+
+        d2f = index._wordinfo[1]
+        assert isinstance(d2f, family.IF.BTree)
+
+        wids = [index._lexicon._wids[x] for x in TEXT.split()]
+
+        relevances = index._search_wids(wids)
+
+        self.assertEqual(len(relevances), len(wids))
+        for relevance in relevances:
+            self.assertTrue(isinstance(relevance[0], index.family.IF.Bucket))
+            self.assertEqual(len(relevance[0]), NUM_DOCS)
+            self.assertTrue(isinstance(relevance[0][1], float))
+            self.assertTrue(isinstance(relevance[1], int))
+
+    def test__search_wids_old_totaldoclen_no_write_on_read(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+
+        # Simulate old instances which didn't have Length attributes
+        index._totaldoclen = 3
+
+        relevances = index._search_wids([1])
+
+        self.assertTrue(isinstance(index._totaldoclen, int))
+
+    def test_query_weight_empty_wids(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+        self.assertEqual(index.query_weight(()), 0.0)
+
+    def test_query_weight_oov_wids(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+        self.assertEqual(index.query_weight(['nonesuch']), 0.0)
+
+    def test_query_weight_hit_single_occurence(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one two three')
+        self.assertTrue(0.0 < index.query_weight(['one']))
+
+    def test_query_weight_hit_multiple_occurences(self):
+        index = self._makeOne()
+        index.index_doc(1, 'one one two three one')
+        self.assertTrue(0.0 < index.query_weight(['one']))
+
+
+class OkapiIndexTest32(OkapiIndexTestBase, unittest.TestCase):
+
+    def _getBTreesFamily(self):
+        import BTrees
+        return BTrees.family32
+
+
+class OkapiIndexTest64(OkapiIndexTestBase, unittest.TestCase):
+
+    def _getBTreesFamily(self):
+        import BTrees
+        return BTrees.family64
