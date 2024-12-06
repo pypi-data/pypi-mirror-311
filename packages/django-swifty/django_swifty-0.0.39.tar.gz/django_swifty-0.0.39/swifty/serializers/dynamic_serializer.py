f2fb74b970273de673fd6c3f66728dff5ac6e161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:06:20 +0000

    Add package django-swifty/0.0.39/django_swifty-0.0.39.tar.gz

---
+++ packages/django-swifty/django_swifty-0.0.39.tar.gz/django_swifty-0.0.39/swifty/serializers/dynamic_serializer.py
@@ -0,0 +1,71 @@
+"""Module for dynamic serializers using Django REST Framework."""
+
+from copy import deepcopy
+from typing import List, Dict, Any, Type
+from rest_framework import serializers
+from .fields import FIELD_TYPE_MAP, BasicFieldSerializer
+
+
+class DynamicSerializer(serializers.Serializer):
+    """Base class for dynamic serializers."""
+
+    def create(self, *args, **kwargs) -> Any:
+        """Create an instance based on validated data."""
+
+    def update(self, *args, **kwargs) -> Any:
+        """Update an instance based on validated data."""
+
+    def get_fields_config(self) -> Dict[str, Any]:
+        """Return the fields configuration as a dictionary.
+
+        Returns:
+            Dict[str, Any]: A dictionary representation of the fields configuration.
+        """
+        fields_config = {}
+        for field_name, field in self.fields.items():
+            allowed_args = getattr(field, "static_args", ()) + getattr(
+                field, "extra_args", ()
+            )
+            allowed_kwargs = {
+                k: v for k, v in field.__dict__.items() if k in allowed_args
+            }
+            fields_config[field_name] = {
+                "field_name": field_name,
+                "type": field.__class__.__name__,
+                **allowed_kwargs,
+                **getattr(field, "extra_kwargs", {}),
+            }
+
+        return fields_config
+
+
+def create_dynamic_serializer(
+    fields_config: List[Dict[str, Any]],
+    base_class: Type[DynamicSerializer] = DynamicSerializer,
+    field_type_map: Dict[str, BasicFieldSerializer] = deepcopy(FIELD_TYPE_MAP),
+) -> Type[DynamicSerializer]:
+    """Create a dynamic serializer class based on field configuration.
+
+    Args:
+        field_config (List[Dict[str, Any]]): A list of dictionaries defining the fields.
+
+    Returns:
+        Type[DynamicSerializer]: A dynamically created serializer class.
+    """
+    fields: Dict[str, serializers.Field] = {}
+
+    for field_info in fields_config:
+        field_base = field_type_map.get(field_info.get("type", "CharField"))
+        allowed_args = field_base.static_args + field_base.extra_args
+        allowed_kwargs = {}
+        extra_kwargs = {}
+        for k, v in field_info.items():
+            if k in allowed_args:
+                allowed_kwargs.update({k: v})
+            else:
+                extra_kwargs.update({k: v})
+        field: BasicFieldSerializer = field_base(**allowed_kwargs)
+        field.extra_kwargs = extra_kwargs
+        fields[field_info["field_name"]] = field
+
+    return type("DynamicSerializer", (base_class,), fields)
