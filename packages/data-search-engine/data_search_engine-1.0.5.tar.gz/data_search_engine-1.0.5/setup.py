962d873bcee625808b94ba5479293a3487c9b102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:59 +0000

    Add package data-search-engine/1.0.5/data_search_engine-1.0.5.tar.gz

---
+++ packages/data-search-engine/data_search_engine-1.0.5.tar.gz/data_search_engine-1.0.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+try:
+    import tkinter
+except ImportError:
+    raise RuntimeError("tkinter is required to run this package, but it is not available in your Python environment.")
+
+
+setup(
+    name="data_search_engine",
+    version="1.0.5",  # Nowa wersja
+    description="A Python-based data search and visualization engine.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Siatek98",
+    url="https://github.com/Siatek98/data_search_engine",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "fredapi",
+        "pyperclip",
+        "yfinance",
+        "requests",
+        "reportlab",
+        "openpyxl",
+        "eurostat",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "gui": ["tk"],  # Użytkownicy mogą to dodać ręcznie, jeśli zajdzie potrzeba
+    },
+)
