96dbce839acfe1b19227392863f10958af695aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:27 +0000

    Add package beaker-kernel/1.8.0/beaker_kernel-1.8.0.tar.gz

---
+++ packages/beaker-kernel/beaker_kernel-1.8.0.tar.gz/beaker_kernel-1.8.0/beaker_kernel/lib/subkernels/julia.py
@@ -0,0 +1,44 @@
+import ast
+import json
+import logging
+from typing import Any
+
+from .base import BeakerSubkernel
+
+logger = logging.getLogger(__name__)
+
+
+def get_kernel_name():
+    from jupyter_client.kernelspec import KernelSpecManager
+    ksm = KernelSpecManager()
+    kernel_specs = ksm.get_all_specs()
+    for kernel, specs in kernel_specs.items():
+        if specs.get("spec", {}).get("language", None) == "julia":
+            return kernel
+    return None
+
+
+class JuliaSubkernel(BeakerSubkernel):
+    """
+    Beaker subkernel for the Julia language, using the IJulia kernel from the IJulia.jl package.
+
+    More information at:
+    https://julialang.github.io/IJulia.jl/stable/
+    https://github.com/JuliaLang/IJulia.jl
+    """
+    DISPLAY_NAME = "Julia"
+    SLUG = "julia"
+    KERNEL_NAME = get_kernel_name()
+
+    WEIGHT = 30
+
+    @classmethod
+    def parse_subkernel_return(cls, execution_result) -> Any:
+        return_raw = execution_result.get("return")
+        if return_raw:
+            return_str = ast.literal_eval(return_raw)
+            try:
+                python_obj = json.loads(return_str)
+            except json.JSONDecodeError:
+                raise
+            return python_obj
