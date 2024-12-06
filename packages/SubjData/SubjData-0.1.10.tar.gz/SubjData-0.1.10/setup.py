9be6a3f6931ab2aaa8e84cf6180da933c1f90f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:01 +0000

    Add package SubjData/0.1.10/SubjData-0.1.10.tar.gz

---
+++ packages/SubjData/SubjData-0.1.10.tar.gz/SubjData-0.1.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SubjData',  # Replace with your package’s name
+    version='0.1.10',
+    packages=find_packages(),
+    package_data={'resources': ['*']},
+    install_requires=[ # List your dependencies here
+        'pandas',
+        'numpy',
+        'huggingface_hub'
+    ],
+    author='Leon Fröhling',  
+    author_email='leon.froehling@gesis.org',
+    description='A library for automatically creating targeted hate speech datasets.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
