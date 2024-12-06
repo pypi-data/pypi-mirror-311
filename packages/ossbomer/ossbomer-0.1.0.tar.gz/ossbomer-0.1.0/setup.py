f3d5bd89168e9e9965f8eb0b7f35a296813ffbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:04:52 +0000

    Add package ossbomer/0.1.0/ossbomer-0.1.0.tar.gz

---
+++ packages/ossbomer/ossbomer-0.1.0.tar.gz/ossbomer-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+def get_version():
+    version_file = os.path.join(os.path.dirname(__file__), "ossbomer", "__init__.py")
+    with open(version_file, "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Version not found in __init__.py")
+
+setup(
+    name="ossbomer",
+    version=get_version(),
+    description="SBOMs quality validator for Open Source License Compliance.",
+    author="Oscar Valenzuela",
+    author_email="oscar.valenzuela.b@gmail.com",
+    license='Apache 2.0',
+    url='https://github.com/Xpertians/xmonkey-ossbomer',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "jsonschema",
+        "requests"
+    ],
+    package_data={
+        "ossbomer": [
+            "datasets/*.json",
+            "datasets/package_signatures/*.json"
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "ossbomer=ossbomer.cli:main",
+        ],
+    },
+    python_requires='>=3.6',
+)
