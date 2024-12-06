646adb8f22694be3356f20f9adf688c019d8b808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:21 +0000

    Add package unikit/1.0.0/unikit-1.0.0-py3-none-any.whl

---
+++ packages/unikit/unikit-1.0.0-py3-none-any.whl/unikit/contrib/taskiq/di.py
@@ -0,0 +1,33 @@
+#
+#  Copyright 2024 by Dmitry Berezovsky, MIT License
+#
+import injector
+
+from unikit.contrib.taskiq.dto import TaskiqTask
+from unikit.di import DiModule, DiProxy
+from unikit.worker import ContextVarWorkerTaskHolder, ContextVarWorkerTaskProvider, PostedTask
+
+
+class BaseTaskiqDiModule(DiModule):
+    """Base DI module for Taskiq."""
+
+    def configure(self, binder: injector.Binder) -> None:
+        """Configure DI module."""
+        super().configure(binder)
+
+        provider = injector.InstanceProvider(
+            PostedTaskProvider(
+                ContextVarWorkerTaskProvider(default_current_task_holder), binder.injector  # type: ignore
+            )
+        )
+        binder.bind(PostedTask, provider)
+        binder.bind(TaskiqTask, provider)
+
+
+default_current_task_holder = ContextVarWorkerTaskHolder()
+
+
+class PostedTaskProvider(DiProxy[TaskiqTask]):
+    """Provider for TaskiqPostedTask."""
+
+    pass
