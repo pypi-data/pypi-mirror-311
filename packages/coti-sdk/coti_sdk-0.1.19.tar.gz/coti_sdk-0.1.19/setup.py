ff6b47c2b6994d2f1924e153574b19ff1a867182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:47 +0000

    Add package coti-sdk/0.1.19/coti_sdk-0.1.19.tar.gz

---
+++ packages/coti-sdk/coti_sdk-0.1.19.tar.gz/coti_sdk-0.1.19/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='coti_sdk',
+    description='COTI SDK for Privacy Preserving Network',
+    version='v0.1.19',
+    license='Apache2.0',
+    author="gmesika-coti@COTI",
+    author_email='support@coti.io',
+    package_dir={'': '.'},
+    url='https://github.com/coti-io/coti-sdk-python',
+    keywords='COTI SDK Privacy',
+    install_requires=[
+        'pycryptodome==3.19.0', 'cryptography==3.4.8', 'eth-keys>=0.4.0', 'eth-account>=0.13.1'
+    ],
+    python_requires=">=3.9",
+)
