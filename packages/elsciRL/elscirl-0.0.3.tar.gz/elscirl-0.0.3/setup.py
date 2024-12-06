b0d7ca4580cdf58adfa0642418f44b67a3bf6e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:31 +0000

    Add package elsciRL/0.0.3/elscirl-0.0.3.tar.gz

---
+++ packages/elsciRL/elscirl-0.0.3.tar.gz/elscirl-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+
+setup(
+    name='elsciRL',
+    version='0.0.3',
+    packages=[
+        'elsciRL', 
+        'elsciRL.adapters',
+        'elsciRL.agents',
+        'elsciRL.agents.stable_baselines',
+        'elsciRL.analysis',
+        'elsciRL.encoders', 
+        'elsciRL.environment_setup',
+        'elsciRL.evaluation',
+        'elsciRL.examples',
+        'elsciRL.experiments',
+        'elsciRL.instruction_following',
+        'elsciRL.interaction_loops',
+        'elsciRL.benchmarking_suite',
+        ],
+    # TODO: Add benchmark to exclusion of wheel
+    url='https://github.com/pdfosborne/elsciRL',
+    license='GNU Public License v3',
+    author='Philip Osborne',
+    author_email='pdfosborne@gmail.com',
+    description='Applying the elsciRL architecture to Reinforcement Learning problems.',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy>=1.10.1',
+        'torch',
+        'tqdm',
+        'httpimport',
+        'sentence-transformers',
+        'gymnasium',
+        'stable-baselines3'
+
+    ] 
+)
