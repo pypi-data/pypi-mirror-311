451fee74b1cb6e6ab0afd39956c0c87712aaf740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:30 +0000

    Add package textual-tree-sitter-languages/0.0.0a0/textual_tree_sitter_languages-0.0.0a0-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/textual-tree-sitter-languages/textual_tree_sitter_languages-0.0.0a0-cp311-cp311-macosx_13_0_arm64.whl/tree_sitter_languages/__init__.py
@@ -0,0 +1,9 @@
+"""Tree Sitter with Languages"""
+
+from .core import get_language, get_parser
+
+__version__ = "0.0.0a0"
+__title__ = "textual_tree_sitter_languages"
+__author__ = "Grant Jenks"
+__license__ = "Apache 2.0"
+__copyright__ = "2022-2024, Grant Jenks"
