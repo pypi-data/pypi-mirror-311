6aa420a452594385ac338754e34fd4c7c36f20a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:37 +0000

    Add package EHT/0.0.8/eht-0.0.8.tar.gz

---
+++ packages/EHT/eht-0.0.8.tar.gz/eht-0.0.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import os
+
+# 확장 모듈 설정
+extensions = [
+    Extension("EHT", ["EHT/EHT.pyx"], include_dirs=["/opt/sagemath-9.3/local/include"], library_dirs=["/opt/sagemath-9.3/local/lib"])
+]
+
+setup(
+    name="EHT",
+    version="0.0.8",
+    description="EHT",
+    long_description_content_type="text/markdown",
+    author='fourchains_R&D',
+    author_email='fourchainsrd@gmail.com',
+    package_dir={"EHT": "EHT"},
+    packages=["EHT"],
+    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "EHT": ["*.pxd", "*.c", "*.h", "*.pyd"],
+    },
+    exclude_package_data={
+        "EHT": ["*.py", "*.pyx"],  # .py와 .pyx 파일 제외
+    },
+    python_requires=">=3.6",
+    install_requires=["pandas", "random"],  # 추가 종속성
+)
