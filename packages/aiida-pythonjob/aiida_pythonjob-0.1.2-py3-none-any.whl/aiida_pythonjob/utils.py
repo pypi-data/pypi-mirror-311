46c65684d78cf9a17bd8cc66fdbba967e3775d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:44 +0000

    Add package aiida-pythonjob/0.1.2/aiida_pythonjob-0.1.2-py3-none-any.whl

---
+++ packages/aiida-pythonjob/aiida_pythonjob-0.1.2-py3-none-any.whl/aiida_pythonjob/utils.py
@@ -0,0 +1,31 @@
+from typing import Optional, Union
+
+from aiida.common.exceptions import NotExistent
+from aiida.orm import Computer, InstalledCode, load_code, load_computer
+
+
+def get_or_create_code(
+    label: str = "python3",
+    computer: Optional[Union[str, "Computer"]] = "localhost",
+    filepath_executable: Optional[str] = None,
+    prepend_text: str = "",
+) -> InstalledCode:
+    """Try to load code, create if not exit."""
+
+    try:
+        return load_code(f"{label}@{computer}")
+    except NotExistent:
+        description = f"Code on computer: {computer}"
+        computer = load_computer(computer)
+        filepath_executable = filepath_executable or label
+        code = InstalledCode(
+            computer=computer,
+            label=label,
+            description=description,
+            filepath_executable=filepath_executable,
+            default_calc_job_plugin="pythonjob.pythonjob",
+            prepend_text=prepend_text,
+        )
+
+        code.store()
+        return code
