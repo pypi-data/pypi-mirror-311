c4e7f2eb78c58fa93aa33170605fed4660ad7a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:44 +0000

    Add package MeUtils/2024.11.28.16.49.49/MeUtils-2024.11.28.16.49.49-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.11.28.16.49.49-py3-none-any.whl/meutils/async_task/tasks/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : kling
+# @Time         : 2024/11/28 16:18
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+from meutils.decorators.retry import retrying
+
+from meutils.async_task import worker, shared_task
+
+
+@shared_task()
+def ado_task(**kwargs):
+    return kwargs
+
+
+@shared_task
+@retrying(3)
+def aproxy_task(**kwargs):
+    method = kwargs.pop('method', '')
+    url = kwargs.pop('url', '')
+
+    logger.debug(kwargs)
+
+    response = requests.request(method, url, **kwargs).json()
+
+    return response
