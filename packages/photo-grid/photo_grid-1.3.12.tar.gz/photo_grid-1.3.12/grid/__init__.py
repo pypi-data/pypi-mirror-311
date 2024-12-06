f2fe9ed2090e45d42b9e508b027f7889edc58ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:23 +0000

    Add package photo-grid/1.3.12/photo_grid-1.3.12.tar.gz

---
+++ packages/photo-grid/photo_grid-1.3.12.tar.gz/photo_grid-1.3.12/grid/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "Chun-Peng James Chen <niche@vt.edu>"
+__version__ = "1.3.11"
+__update__ = "March 30, 2024"
---
+++ packages/photo-grid/photo_grid-1.3.12.tar.gz/photo_grid-1.3.12/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="photo_grid",
+    version="1.3.12",
+    description="A GUI for field segmentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Poissonfish/GRID",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    author="James Chen",
+    author_email="niche@vt.edu",
+    license="GPLv3",
+    packages=["grid", "grid.gui"],
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas>=0.19.2",
+        # data processing
+        "h5py",
+        "pyshp",
+        # math, models
+        "scikit-learn",
+        "scipy",
+        "matplotlib",
+        # image processing
+        "image",
+        "opencv-python",
+        "rasterio",
+        # GUI
+        "PyQt6",
+        "qdarkstyle",
+        # misc
+        "tqdm",
+    ],
+    entry_points={"console_scripts": ["GRID=grid.__main__:main"]},
+)
