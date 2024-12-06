24e3d0d0d8ed29ca491bd078958d55bb85b0efd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:50 +0000

    Add package chinus-emotions/3.1.3/chinus_emotions-3.1.3.tar.gz

---
+++ packages/chinus-emotions/chinus_emotions-3.1.3.tar.gz/chinus_emotions-3.1.3/README.md
@@ -0,0 +1,13 @@
+# Chinu9653's package
+
+This is a sample Python package created by Chinu9653.
+
+## Features
+- Feature 1
+- Feature 2
+
+## Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install `chinus-emotions`.
+
+```bash
+pip install chinus-emotions
---
+++ packages/chinus-emotions/chinus_emotions-3.1.3.tar.gz/chinus_emotions-3.1.3/chinus_emotions/src/main/main.py
@@ -0,0 +1,44 @@
+from chinus_emotions.src.main.emotions.emotion import Emotion
+
+
+class Minds(Emotion):
+    """
+    emotions.db의 minds 테이블의 랜덤 row
+
+    생성자 매개변수:
+        - num: 갯수
+        - positive_percent: 긍적적인 row일 확률
+
+    :ivar words: 단어 리스트
+    :ivar means: 단어의 뜻 리스트
+    """
+
+
+class Feelings(Emotion):
+    """
+    emotions.db의 feelings 테이블의 랜덤 row
+
+    생성자 매개변수:
+        - num: 갯수
+        - positive_percent: 긍적적인 row일 확률
+
+    :ivar words: 단어 리스트
+    :ivar means: 단어의 뜻 리스트
+    """
+
+
+class Senses(Emotion):
+    """
+    emotions.db의 senses 테이블의 랜덤 row
+
+    생성자 매개변수:
+        - num: 갯수
+        - positive_percent: 긍적적인 row일 확률
+
+    :ivar words: 단어 리스트
+    :ivar means: 단어의 뜻 리스트
+    """
+
+
+if __name__ == '__main__':
+    Minds(num=2).print()
