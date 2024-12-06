8f96b0a7c03b01e5ca972ae5392e9bdff6209954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:16 +0000

    Add package debounce-redis-guard/0.0.3/debounce-redis-guard-0.0.3.tar.gz

---
+++ packages/debounce-redis-guard/debounce-redis-guard-0.0.3.tar.gz/debounce-redis-guard-0.0.3/tests/http_module.py
@@ -0,0 +1,9 @@
+# # http_module.py
+# import requests
+#
+# async def fetch_data(url):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         return None
