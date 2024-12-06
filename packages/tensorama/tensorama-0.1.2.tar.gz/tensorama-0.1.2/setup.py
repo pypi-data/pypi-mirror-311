8dd4443d3197916856572904fe1a07d5d8e4730b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:14 +0000

    Add package tensorama/0.1.2/tensorama-0.1.2.tar.gz

---
+++ packages/tensorama/tensorama-0.1.2.tar.gz/tensorama-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tensorama",  # Your library name
+    version="0.1.2",  # Version number
+    description="A simple library to help people",
+    author="Clonitty",
+    author_email="teamclonitty@gmail.com",
+    packages=find_packages(),  # Automatically find sub-packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7, <4',
+)
