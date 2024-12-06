4a2cf31135eeed41f2e0a36ddecf233ba54636fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:02 +0000

    Add package bayex/0.2.1rc0/bayex-0.2.1rc0-py3-none-any.whl

---
+++ packages/bayex/bayex-0.2.1rc0-py3-none-any.whl/bayex/__init__.py
@@ -0,0 +1,11 @@
+from .optimizer import Optimizer, OptimizerState
+from . import domain
+
+
+__version__ = "0.2.1c"
+
+__all__ = [
+        "Optimizer",
+        "OptimizerState",
+        "domain",
+]
