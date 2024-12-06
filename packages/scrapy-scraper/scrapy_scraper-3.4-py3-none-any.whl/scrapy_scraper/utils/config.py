e339776f4ad0375aff98b59bd903f2c35409c15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:01 +0000

    Add package scrapy-scraper/3.4/scrapy_scraper-3.4-py3-none-any.whl

---
+++ packages/scrapy-scraper/scrapy_scraper-3.4-py3-none-any.whl/scrapy_scraper/utils/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+APP_VERSION = "v3.4"
+
+USER_AGENT = "Scrapy Scraper/3.4"
+
+def banner():
+	"""
+	Display the banner.
+	"""
+	print("#########################################################################")
+	print("#                                                                       #")
+	print("#                          Scrapy Scraper v3.4                          #")
+	print("#                                     by Ivan Sincek                    #")
+	print("#                                                                       #")
+	print("# Crawl and scrape websites.                                            #")
+	print("# GitHub repository at github.com/ivan-sincek/scrapy-scraper.           #")
+	print("#                                                                       #")
+	print("#########################################################################")
