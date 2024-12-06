e62ebe562f286d09e8a2931cd5d508e53baadb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:54 +0000

    Add package octo-pipeline-backend-git-python/1.3.2/octo_pipeline_backend_git_python-1.3.2-py3-none-any.whl

---
+++ packages/octo-pipeline-backend-git-python/octo_pipeline_backend_git_python-1.3.2-py3-none-any.whl/octo_pipeline_python/backends/git/models/git_backend_model.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class GitModel(BaseModel):
+    head: Optional[str] = Field(description="Head branch or tag", default="master")
+    mirror: Optional[str] = Field(default=None, description="Mirror remote to add")
+    shallow: bool = Field(description="Shallow clone", default=False)
+    recursive: bool = Field(description="Recursive clone", default=False)
+    submodule_init: bool = Field(description="Try to run submodule init", default=False)
