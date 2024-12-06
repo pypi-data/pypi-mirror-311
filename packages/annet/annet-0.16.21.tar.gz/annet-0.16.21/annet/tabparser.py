b7d05c2aab8def9f57e8cd049ee2c02a2fc402d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:37 +0000

    Add package annet/0.16.21/annet-0.16.21.tar.gz

---
+++ packages/annet/annet-0.16.21.tar.gz/annet-0.16.21/annet/tabparser.py
@@ -0,0 +1,44 @@
+from annet.annlib.tabparser import (  # pylint: disable=unused-import
+    AsrFormatter,
+    CiscoFormatter,
+    CommonFormatter,
+    HuaweiFormatter,
+    OptixtransFormatter,
+    JuniperFormatter,
+    JuniperList,
+    NokiaFormatter,
+    ParserError,
+    RibbonFormatter,
+    RosFormatter,
+    parse_to_tree,
+    AristaFormatter,
+)
+
+
+def make_formatter(hw, **kwargs):
+    if hw.OptiXtrans:
+        cls = OptixtransFormatter
+    elif hw.Huawei:
+        cls = HuaweiFormatter
+    elif hw.Cisco.ASR or hw.Cisco.XRV:
+        cls = AsrFormatter
+    elif hw.Nexus or hw.Cisco or hw.Aruba or hw.B4com:
+        cls = CiscoFormatter
+    elif hw.Juniper:
+        cls = JuniperFormatter
+    elif hw.Nokia:
+        cls = NokiaFormatter
+    elif hw.RouterOS:
+        cls = RosFormatter
+    elif hw.PC:
+        cls = CommonFormatter
+    elif hw.Ribbon:
+        cls = RibbonFormatter
+    elif hw.H3C:
+        cls = HuaweiFormatter
+    elif hw.Arista:
+        cls = AristaFormatter
+    else:
+        raise NotImplementedError("Unknown formatter for hw '%s'" % hw)
+
+    return cls(**kwargs)
