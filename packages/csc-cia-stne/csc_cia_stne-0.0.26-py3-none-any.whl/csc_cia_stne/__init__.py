1bd873abdacc98cb663047e4c377a512d8add7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:39 +0000

    Add package csc-cia-stne/0.0.26/csc_cia_stne-0.0.26-py3-none-any.whl

---
+++ packages/csc-cia-stne/csc_cia_stne-0.0.26-py3-none-any.whl/csc_cia_stne/__init__.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+# Carrega .env
+load_dotenv()
+
+if os.getenv('ambiente_de_execucao') is not None and os.getenv('ambiente_de_execucao') == "karavela":
+    from .logger_json import logger
+else:
+    from .logger_rich import logger
+from .karavela import Karavela
+#from .utilitarios.classes import Util
+from .servicenow import ServiceNow
+from .stne_admin import StoneAdmin
+from .bc_sta import BC_STA
+from .bc_correios import BC_Correios
+from .gcp_bigquery import BigQuery
+from .email import Email
+#from .utilitarios.functions import titulo
+# Define os itens disponíveis para importação
+__all__ = [
+    "titulo",
+    "BigQuery",
+    "BC_Correios",
+    "BC_STA",
+    "StoneAdmin",
+    "ServiceNow",
+    "Util",
+    "logger",
+    "Email"
+]
