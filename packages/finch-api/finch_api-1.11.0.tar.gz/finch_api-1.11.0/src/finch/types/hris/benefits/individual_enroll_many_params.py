a5802658d77bf9e71532aea8acc15657695230e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:57 +0000

    Add package finch-api/1.11.0/finch_api-1.11.0.tar.gz

---
+++ packages/finch-api/finch_api-1.11.0.tar.gz/finch_api-1.11.0/src/finch/types/hris/benefits/individual_enroll_many_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Required, TypedDict
+
+__all__ = ["IndividualEnrollManyParams", "Individual"]
+
+
+class IndividualEnrollManyParams(TypedDict, total=False):
+    individuals: Required[Iterable[Individual]]
+    """Array of the individual_id to enroll and a configuration object."""
+
+
+class Individual(TypedDict, total=False):
+    configuration: object
+
+    individual_id: str
+    """Finch id (uuidv4) for the individual to enroll"""
