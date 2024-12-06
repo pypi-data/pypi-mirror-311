953ce3f3f0acf52c6f6e1b1d6545afb11242eba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:20 +0000

    Add package vectocore/0.1.1/vectocore-0.1.1.tar.gz

---
+++ packages/vectocore/vectocore-0.1.1.tar.gz/vectocore-0.1.1/vectocore/vectocore.py
@@ -0,0 +1,11 @@
+import os
+import json
+import requests
+
+
+class Vectocore:
+    def __init__(self, tenantKey=""):
+        self.tenant_key = os.environ.get("VECTOCORE_TENANT_KEY", tenantKey)
+
+    def test(self, input: str) -> str:
+        return "here is {} comes. tenant key : {}".format(input, self.tenant_key)
