7abaa9ff30c6f6e7c46f9a6ce74b76a3b9dca6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:48 +0000

    Add package pretix-pages/1.6.3/pretix_pages-1.6.3-py3-none-any.whl

---
+++ packages/pretix-pages/pretix_pages-1.6.3-py3-none-any.whl/pretix_pages/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/",
+        views.PageList.as_view(),
+        name="index",
+    ),
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/create",
+        views.PageCreate.as_view(),
+        name="create",
+    ),
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/<int:page>/",
+        views.PageUpdate.as_view(),
+        name="edit",
+    ),
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/<int:page>/delete",
+        views.PageDelete.as_view(),
+        name="delete",
+    ),
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/<int:page>/up",
+        views.page_move_up,
+        name="up",
+    ),
+    path(
+        "control/event/<str:organizer>/<str:event>/pages/<int:page>/down",
+        views.page_move_down,
+        name="down",
+    ),
+]
+
+event_patterns = [
+    path("page/<str:slug>/", views.ShowPageView.as_view(), name="show"),
+]
