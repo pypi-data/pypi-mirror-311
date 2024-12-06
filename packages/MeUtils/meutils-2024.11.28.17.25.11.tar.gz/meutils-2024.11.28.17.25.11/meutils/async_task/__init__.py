d0471b65e73c9c1ab6cfb13aea694d0b49905aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:19 +0000

    Add package MeUtils/2024.11.28.17.25.11/meutils-2024.11.28.17.25.11.tar.gz

---
+++ packages/MeUtils/meutils-2024.11.28.17.25.11.tar.gz/meutils-2024.11.28.17.25.11/meutils/async_task/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2024/11/28 15:28
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.async_task.common import worker, shared_task
