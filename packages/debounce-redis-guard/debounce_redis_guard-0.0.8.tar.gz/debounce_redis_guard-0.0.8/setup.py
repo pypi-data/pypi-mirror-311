3f94ce3256120439c3f0e3d6f3e0bd7590cc7824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:04 +0000

    Add package debounce-redis-guard/0.0.8/debounce_redis_guard-0.0.8.tar.gz

---
+++ packages/debounce-redis-guard/debounce_redis_guard-0.0.8.tar.gz/debounce_redis_guard-0.0.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debounce_redis_guard",
+    version="0.0.8",
+    packages=find_packages(),
+    install_requires=[],
+    author="Chathura Deepana Herath",
+    author_email="chathuradeepana7@gmail.com",
+    description="A Python library for debouncing requests with support for multiple workers.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cdeepana/debounce_redis_guard",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Fixed classifier here
+        "Development Status :: 4 - Beta"
+    ],
+    python_requires='>=3.6',
+)
