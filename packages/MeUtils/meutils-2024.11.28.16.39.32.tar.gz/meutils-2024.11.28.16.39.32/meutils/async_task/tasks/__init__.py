552484a6bfce0e45f6ed58f6d895473885206c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:16 +0000

    Add package MeUtils/2024.11.28.16.39.32/meutils-2024.11.28.16.39.32.tar.gz

---
+++ packages/MeUtils/meutils-2024.11.28.16.39.32.tar.gz/meutils-2024.11.28.16.39.32/meutils/async_task/tasks/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2024/11/28 16:03
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+# 所有异步任务
+from meutils.async_task.tasks import *
