71425125648144d8c029b6e263f63cfd81c1425c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:34 +0000

    Add package django-otp-rest-auth/0.2.3/django_otp_rest_auth-0.2.3.tar.gz

---
+++ packages/django-otp-rest-auth/django_otp_rest_auth-0.2.3.tar.gz/django_otp_rest_auth-0.2.3/otp_rest_auth/urls.py
@@ -0,0 +1,69 @@
+from django.urls import path, include
+
+from .views import (
+    RegisterView,
+    ResendOTPView,
+    ResetPasswordView,
+    LoginView,
+    LogoutView,
+    UserDetailsView,
+    VerifyAccountView,
+    VerifyEmailView,
+    VerifyPhoneView,
+    PasswordChangeView,
+    PasswordResetConfirmView,
+    InvokeChangeEmailOTPView,
+    InvokeChangePhoneOTPView,
+    ChangeEmailConfrimationView,
+    ChangePhoneConfirmationView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
+from otp_rest_auth.jwt_auth import get_refresh_view
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="otp_rest_login"),
+    path("logout/", LogoutView.as_view(), name="otp_rest_logout"),
+    path("register/", RegisterView.as_view(), name="otp_rest_register"),
+    path("user/", UserDetailsView.as_view(), name="otp_rest_user_details"),
+    path("resend_otp/", ResendOTPView.as_view(), name="otp_rest_resend_otp"),
+    path(
+        "phone/change/",
+        InvokeChangePhoneOTPView.as_view(),
+        name="otp_rest_change_phone",
+    ),
+    path(
+        "email/change/",
+        InvokeChangeEmailOTPView.as_view(),
+        name="otp_rest_change_email",
+    ),
+    path(
+        "phone/change/confirm/",
+        ChangePhoneConfirmationView.as_view(),
+        name="otp_rest_change_phone_set",
+    ),
+    path(
+        "email/change/confirm/",
+        ChangeEmailConfrimationView.as_view(),
+        name="otp_rest_change_email_set",
+    ),
+    path("verify/phone/", VerifyPhoneView.as_view(), name="otp_rest_verify_phone"),
+    path("verify/email/", VerifyEmailView.as_view(), name="otp_rest_verify_email"),
+    path(
+        "verify/account/", VerifyAccountView.as_view(), name="otp_rest_verify_account"
+    ),
+    path(
+        "password/reset/", ResetPasswordView.as_view(), name="otp_rest_password_reset"
+    ),
+    path(
+        "password/reset/confirm/",
+        PasswordResetConfirmView.as_view(),
+        name="otp_rest_password_reset_confirm",
+    ),
+    path(
+        "password/change/",
+        PasswordChangeView.as_view(),
+        name="otp_rest_password_change",
+    ),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+]
