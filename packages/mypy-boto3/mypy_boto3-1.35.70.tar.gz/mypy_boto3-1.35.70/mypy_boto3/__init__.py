b291f85781e658a3407451b35477a16c1320c1bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:12 +0000

    Add package mypy-boto3/1.35.70/mypy_boto3-1.35.70.tar.gz

---
+++ packages/mypy-boto3/mypy_boto3-1.35.70.tar.gz/mypy_boto3-1.35.70/mypy_boto3/__init__.py
@@ -0,0 +1,5 @@
+"""
+mypy-boto3 main entrypoint.
+
+Copyright 2024 Vlad Emelianov
+"""
---
+++ packages/mypy-boto3/mypy_boto3-1.35.70.tar.gz/mypy_boto3-1.35.70/mypy_boto3/__main__.py
@@ -0,0 +1,9 @@
+"""
+mypy-boto3 module entrypoint.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from mypy_boto3.main import main
+
+main()
