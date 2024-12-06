71c51416af10f9a4f711343829daa5d05611feb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:50 +0000

    Add package ansys-fluent-core/0.27.0/ansys_fluent_core-0.27.0-py3-none-any.whl

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.27.0-py3-none-any.whl/ansys/fluent/core/journaling.py
@@ -0,0 +1,17 @@
+"""A module for controlling the writing of Fluent Python journals."""
+
+
+class Journal:
+    """Control the writing of Fluent Python journals."""
+
+    def __init__(self, scheme_eval):
+        """__init__ method of Journal class."""
+        self.scheme_eval = scheme_eval
+
+    def start(self, file_name: str):
+        """Start writing a Fluent Python journal at the specified file_name."""
+        self.scheme_eval.exec([f'(api-start-python-journal "{file_name}")'])
+
+    def stop(self):
+        """Stop writing the Fluent Python journal."""
+        self.scheme_eval.exec(["(api-stop-python-journal)"])
