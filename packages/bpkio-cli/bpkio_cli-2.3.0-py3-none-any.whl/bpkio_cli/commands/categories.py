cf9fbd99ef13e87b39d271e85ad7d5197e06dbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:05 +0000

    Add package bpkio-cli/2.3.0/bpkio_cli-2.3.0-py3-none-any.whl

---
+++ packages/bpkio-cli/bpkio_cli-2.3.0-py3-none-any.whl/bpkio_cli/commands/categories.py
@@ -0,0 +1,14 @@
+from bpkio_api.models import Category
+from bpkio_cli.commands.creators.categories import create_category_command
+from bpkio_cli.commands.template_crud import create_resource_group
+
+
+def add_categories_commands():
+    return create_resource_group(
+        "category",
+        resource_type=Category,
+        endpoint_path=["categories"],
+        aliases=["cat", "categories"],
+        default_fields=["id", "name", "subcategories_list"],
+        extra_commands=[create_category_command()],
+    )
