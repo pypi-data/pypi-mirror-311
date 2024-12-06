ba1ef6219f9dacac095de9d68a8454e4c1215ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:13 +0000

    Add package redshift_client/8.0.1/redshift_client-8.0.1-py3-none-any.whl

---
+++ packages/redshift_client/redshift_client-8.0.1-py3-none-any.whl/redshift_client/client/_factory.py
@@ -0,0 +1,38 @@
+from . import (
+    _schema,
+    _table,
+)
+from ._core import (
+    SchemaClient,
+    TableClient,
+)
+from dataclasses import (
+    dataclass,
+)
+from redshift_client.sql_client import (
+    SqlCursor,
+)
+
+
+@dataclass(frozen=True)
+class ClientFactory:
+    @staticmethod
+    def new_table_client(sql: SqlCursor) -> TableClient:
+        """
+        @move_data:
+        This method moves data from source to target.
+        - After the operation source will be empty.
+        - Both tables must exists.
+        @move:
+        - create target if not exist
+        - move_data (append) data from source into target
+        - delete source table (that will be empty)
+        @migrate:
+        - delete target if exist
+        - move source into target (see move method)
+        """
+        return _table.new_table_client(sql)
+
+    @staticmethod
+    def new_schema_client(sql: SqlCursor) -> SchemaClient:
+        return _schema.new_schema_client(sql)
---
+++ packages/redshift_client/redshift_client-8.0.1-py3-none-any.whl/redshift_client/sql_client/_factory/__init__.py
@@ -0,0 +1,57 @@
+from . import (
+    _temp_creds,
+)
+from ._connection import (
+    RedshiftConnection,
+)
+from ._primitive import (
+    DbPrimitiveFactory,
+)
+from ._temp_creds import (
+    TempCredsUser,
+)
+from dataclasses import (
+    dataclass,
+)
+from fa_purity import (
+    Cmd,
+    ResultE,
+)
+from redshift_client.sql_client._core.connection import (
+    Credentials,
+    DatabaseId,
+    DbConnection,
+    IsolationLvl,
+)
+
+
+@dataclass(frozen=True)
+class ConnectionFactory:
+    @staticmethod
+    def redshift_connection(
+        db_id: DatabaseId,
+        creds: Credentials,
+        readonly: bool,
+        isolation: IsolationLvl,
+        autocommit: bool,
+    ) -> Cmd[DbConnection]:
+        return RedshiftConnection.connect(
+            db_id,
+            creds,
+            readonly,
+            isolation,
+            autocommit,
+        )
+
+
+@dataclass(frozen=True)
+class LoginUtils:
+    @staticmethod
+    def get_temp_creds(user: TempCredsUser) -> Cmd[ResultE[Credentials]]:
+        return _temp_creds.get_temp_creds(user)
+
+
+__all__ = [
+    "TempCredsUser",
+    "DbPrimitiveFactory",
+]
