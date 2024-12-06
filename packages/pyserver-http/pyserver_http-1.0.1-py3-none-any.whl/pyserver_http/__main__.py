84c4d4448dae0a542bc8bc6737e3cc8286fd45c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:33 +0000

    Add package pyserver-http/1.0.1/pyserver_http-1.0.1-py3-none-any.whl

---
+++ packages/pyserver-http/pyserver_http-1.0.1-py3-none-any.whl/pyserver_http/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+from .server import start_server
+# from .utils import get_local_ip # i may need it later
+
+def main():
+    parser = argparse.ArgumentParser(description="simple http server in python")
+    subparsers = parser.add_subparsers(dest="command")
+
+    server_parser = subparsers.add_parser("serve", help="Start the sendfile server")
+    server_parser.add_argument("--host", default="0.0.0.0", help="Host to bind the server (default: 0.0.0.0)")
+    server_parser.add_argument("--port", type=int, default=80, help="Port to bind the server (default: 80)")
+
+    args = parser.parse_args()
+
+    start_server(host=args.host, port=args.port)
+
+
+if __name__ == "__main__":
+    main()
