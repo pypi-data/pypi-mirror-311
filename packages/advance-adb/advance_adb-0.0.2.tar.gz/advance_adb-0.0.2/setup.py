0e9aae5f68e74b98d7c9aeefcf9108fbe560118b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:04 +0000

    Add package advance-adb/0.0.2/advance_adb-0.0.2.tar.gz

---
+++ packages/advance-adb/advance_adb-0.0.2.tar.gz/advance_adb-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+    
+setuptools.setup(
+    name="advance_adb",
+    version="0.0.2",
+    author="Sudhir Kumar",
+    author_email="sudhir9122kumar@gmail.com",
+    description="Advance adb toolkit in one place",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
