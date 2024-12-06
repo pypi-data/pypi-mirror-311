2064d30f12f47f9d788082df05fd32a70b78473a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:00 +0000

    Add package udyndns/0.0.3/udyndns-0.0.3.tar.gz

---
+++ packages/udyndns/udyndns-0.0.3.tar.gz/udyndns-0.0.3/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+import setuptools
+
+from udyndns.version import __version__
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="udyndns",
+    version=__version__,
+    author="KurisuD",
+    author_email="KurisuD@pypi.darnand.net",
+    description="A very simple OVH dyndns client to workaround difficulties with Ubiquity unadyn...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KurisuD/udyndns",
+    packages=setuptools.find_packages(),
+    install_requires=['aiounifi', 'pap_logger'],
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "License :: Public Domain",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Home Automation",
+        "Topic :: Internet :: Name Service (DNS)"
+    ],
+)
