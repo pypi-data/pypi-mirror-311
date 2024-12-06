ff4736dc8a6684219e5b10b79e5ad1e3cfcc89ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:16 +0000

    Add package bx-django-utils/83/bx_django_utils-83.tar.gz

---
+++ packages/bx-django-utils/bx_django_utils-83.tar.gz/bx_django_utils-83/bx_django_utils_tests/test_project/publish.py
@@ -0,0 +1,36 @@
+"""
+    Helper to publish this Project to PyPi
+"""
+
+import subprocess
+from pathlib import Path
+
+from bx_py_utils.path import assert_is_file
+from cli_base.cli_tools.subprocess_utils import verbose_check_call
+from manageprojects.utilities.publish import publish_package
+
+import bx_django_utils
+
+
+def publish():
+    """
+    Publish to PyPi
+    Call this via:
+        $ .venv/bin/publish
+    """
+    package_path = Path(bx_django_utils.__file__).parent.parent
+    assert_is_file(package_path / 'pyproject.toml')
+
+    subprocess.check_call(['pipenv', 'check'])
+
+    verbose_check_call('make', 'test')  # don't publish if tests fail
+    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
+
+    publish_package(
+        module=bx_django_utils,
+        package_path=package_path,
+    )
+
+
+if __name__ == '__main__':
+    publish()
