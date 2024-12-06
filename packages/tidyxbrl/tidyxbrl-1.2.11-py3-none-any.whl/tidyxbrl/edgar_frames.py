4dd3fd2f5f37a7f294d3661a59eadd27364f2f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:54 +0000

    Add package tidyxbrl/1.2.11/tidyxbrl-1.2.11-py3-none-any.whl

---
+++ packages/tidyxbrl/tidyxbrl-1.2.11-py3-none-any.whl/tidyxbrl/edgar_frames.py
@@ -0,0 +1,85 @@
+"""
+https://www.sec.gov/edgar/sec-api-documentation
+
+data.sec.gov/api/xbrl/frames/
+The xbrl/frames API aggregates one fact for each reporting entity that is last filed
+that most closely fits the calendrical period requested. This API supports for annual,
+quarterly and instantaneous data:
+
+https://data.sec.gov/api/xbrl/frames/us-gaap/AccountsPayableCurrent/USD/CY2019Q1I.json
+Where the units of measure specified in the XBRL contains a numerator and a denominator,
+these are separated by “-per-” such as “USD-per-shares”. Note that the default unit in
+XBRL is “pure”.
+
+The period format is CY#### for annual data (duration 365 days +/- 30 days), CY####Q# for
+quarterly data (duration 91 days +/- 30 days), and CY####Q#I for instantaneous data.
+Because company financial calendars can start and end on any month or day and even change
+in length from quarter to quarter to according to the day of the week, the frame data is
+assembled by the dates that best align with a calendar quarter or year. Data users should
+be mindful different reporting start and end dates for facts contained in a frame.
+"""
+
+import pandas
+import requests
+from tqdm import tqdm
+from src.config.default_headers import con_headers_default
+
+
+def edgar_frames(urldescriptor="", timeout_sec = 15, con_headers = con_headers_default):
+    """
+    The edgar_frames function aggregates one fact for each reporting entity
+    that is last filed that most closely fits the calendrical period requested.
+    This API supports for annual, quarterly and instantaneous data
+
+    Args:
+        urldescriptor: URL description of the frame query
+        (everything after https://data.sec.gov/api/xbrl/frames/***)
+        The data is structured as "finstandard/reporttype/denomination/dateholder"
+        
+            - finstandard: The financial reporting standard
+                * us-gaap
+                * ifrs-full
+                * dei
+                * srt
+            - reporttype: Type of report. See datacode in xbrl_parse (i.e. NonoperatingIncomeExpense)
+            - denomination: Report currency (i.e. USD)
+            - dateholder: Date of evaluation (i.e. CY2019Q1I)
+        timeout_sec: The time in seconds to wait for the server to respond
+        con_headers (dict): The headers to be sent with the initial request.
+
+    Outputs:
+        return companies: Return a pandas DataFrame of reporting companies and their associated CIK
+
+    Examples:
+        - edgar_frames(urldescriptor = 'us-gaap/NonoperatingIncomeExpense/USD/CY2019Q1I.json')
+    """
+
+    # Specify the query url and parameters
+    dataquery = (
+        "https://data.sec.gov/api/xbrl/frames/"
+        + str(urldescriptor).replace(".json", "")
+        + ".json"
+    )
+    # Pull the data, check the response, and convert to a long format
+    dataresponse = requests.get(url=dataquery, headers=con_headers, timeout=timeout_sec)
+    if dataresponse.status_code == 200:
+        try:
+            result = pandas.DataFrame(pandas.json_normalize(dataresponse.json()))
+            longdata = pandas.melt(result, id_vars=["taxonomy"])
+        except Exception as exc:
+            raise ValueError(f"Error in parsing JSON response: {str(dataresponse.json())}") from exc
+    else:
+        xbrl_queryoutput = dataresponse.status_code
+        print(dataresponse.text)
+        raise ValueError(str(xbrl_queryoutput) + ": Error in Response")
+
+    # If the data is in a list form, then convert into a nested dataframe
+    for valueholder in tqdm(range(1, len(longdata))):
+        loadvalue = longdata.iloc[valueholder].value
+        if str(type(loadvalue)) == "<class 'list'>" and len(loadvalue) > 1:
+            try:
+                longdata.at[valueholder, "value"] = pandas.json_normalize(loadvalue)
+            except ValueError:
+                pass
+
+    return longdata
---
+++ packages/tidyxbrl/tidyxbrl-1.2.11-py3-none-any.whl/tidyxbrl/edgar_query.py
@@ -0,0 +1,114 @@
+"""
+https://www.sec.gov/edgar/sec-api-documentation
+
+data.sec.gov/submissions/
+Each entity’s current filing history is available at the following URL:
+https://data.sec.gov/submissions/CIK##########.json
+Where the ########## is the entity’s 10-digit Central Index Key (CIK), including leading
+zeros.
+
+This JSON data structure contains metadata such as current name, former name, and stock
+exchanges and ticker symbols of publicly-traded companies. The object’s property path
+contains at least one year’s of filing or to 1,000 (whichever is more) of the most recent
+filings in a compact columnar data array. If the entity has additional filings, files will
+contain an array of additional JSON files and the date range for the filings each one
+contains.
+
+------
+
+data.sec.gov/api/xbrl/companyconcept/
+The company-concept API returns all the XBRL disclosures from a single company (CIK) and
+concept (a taxonomy and tag) into a single JSON file, with a separate array of facts for
+each units on measure that the company has chosen to disclose (e.g. net profits reported
+in U.S. dollars and in Canadian dollars).
+
+https://data.sec.gov/api/xbrl/companyconcept/CIK##########/us-gaap/AccountsPayableCurrent.json
+
+-----
+
+data.sec.gov/api/xbrl/companyfacts/
+This API returns all the company concepts data for a company into a single API call:
+
+https://data.sec.gov/api/xbrl/companyfacts/CIK##########.json
+"""
+
+import pandas
+import requests
+from tqdm import tqdm
+import numpy
+from src.config.default_headers import con_headers_default
+
+
+def edgar_query(companycik, query_type, queryextension="", parse_pandas = True, timeout_sec = 15, con_headers = con_headers_default):
+    """
+    Query SEC data using the Central Index Key (CIK).
+
+    The edgar_query function is used to query SEC data using the Central Index Key (CIK)
+    determined in the edgar_cik function.
+
+    Args:
+        companycik (str): Unique company CIK value pulled in edgar_cik. Note that the CIK is
+        converted to 10 digits with leading 0s.
+        query_type (str): The type of API query. Can be 'submissions', 'companyconcept', or
+        'companyfacts'.
+        queryextension (str, optional): Extension required for the "companyconcept"
+        query_type to specify the report type. Defaults to "".
+        parse_pandas (bool, optional): If True, the data is converted to a pandas DataFrame.
+        timeout_sec: The time in seconds to wait for the server to respond
+        con_headers (dict): The headers to be sent with the initial request.
+
+    Returns:
+        pandas.DataFrame: Tidy dataframe housing the report data.
+
+    Raises:
+        ValueError: If query_type is not one of 'submissions', 'companyconcept', or
+        'companyfacts'.
+
+    Examples:
+        edgar_query('0000789019', query_type = 'submissions')
+        edgar_query('0000789019', query_type = 'companyconcept',
+        queryextension = '/us-gaap/AccountsPayableCurrent.json')
+        edgar_query('0000789019', query_type = 'companyfacts')
+    """
+
+    # Pull one of the urls associated with the query types
+    querydict = {
+        "submissions": "https://data.sec.gov/submissions/CIK",
+        "companyconcept": "https://data.sec.gov/api/xbrl/companyconcept/CIK",
+        "companyfacts": "https://data.sec.gov/api/xbrl/companyfacts/CIK",
+    }
+    # Raise error if the data does not exist
+    if query_type not in querydict:
+        raise ValueError("parse_type must be in: " + str(querydict))
+    # Specify the query url and parameters
+    urlquery = querydict[query_type]
+    dataquery = (
+        urlquery + str(companycik) + str(queryextension).replace(".json", "") + ".json"
+    )
+    # Pull the data, check the response, and convert to a long format
+    dataresponse = requests.get(url=dataquery, headers=con_headers, timeout=timeout_sec)
+    if dataresponse.status_code == 200:
+        try:
+            result = pandas.DataFrame(pandas.json_normalize(dataresponse.json()))
+            longdata = pandas.melt(result, id_vars=["cik"])
+        except Exception as exc:
+            raise ValueError(str(dataresponse.json())) from exc
+    else:
+        xbrl_queryoutput = dataresponse.status_code
+        # print(dataresponse.text)
+        raise ValueError(str(xbrl_queryoutput) + ": " + str(dataresponse.content))
+
+    # If the data is in a list form, then convert it into a nested dataframe
+    if parse_pandas:
+        for valueholder in tqdm(range(1, len(longdata.value))):
+            loadvalue = longdata.iloc[valueholder].value
+            if isinstance(loadvalue, list):
+                try:
+                    loadvalue_df = pandas.DataFrame(loadvalue)
+                    if len(loadvalue_df) > 1:
+                        loadvalue_df = loadvalue_df.transpose().explode("value")
+                    longdata.at[valueholder, "value"] = loadvalue_df
+                except Exception:
+                    longdata.at[valueholder, "value"] = pandas.DataFrame(loadvalue)
+    longdata = longdata.assign(cik = lambda x: pandas.to_numeric(x.cik, errors='coerce'))
+    return longdata
