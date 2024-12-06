198acdd28a5f7327519ad7044f2ad61d18380218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:44 +0000

    Add package VineCopulas/2.0.2/vinecopulas-2.0.2.tar.gz

---
+++ packages/VineCopulas/vinecopulas-2.0.2.tar.gz/vinecopulas-2.0.2/src/vinecopulas/marginals.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 17:13:20 2024
+
+
+"""
+
+
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+from scipy.stats import rankdata
+from scipy.optimize import newton
+from scipy.optimize import minimize_scalar
+from scipy.special import gammaln
+from scipy.linalg import cholesky
+from itertools import product
+import sys
+
+# %% best fit discrete distribution
+
+def best_fit_distributiondiscrete(data, bound=False, criterion="BIC"):
+    """
+    Fits the best discrete distribution to data.
+
+    Arguments:
+        *data* : The data which has to be fit as a 1-d numpy array.
+
+        *bounds* : whether the data is bounded
+
+        *criterion* : Either BIC, AIC or ML is used to choose the best distribution
+
+    Returns:
+     *bestdist* : the best distribution and its parameters.
+    """
+
+    # distributions
+    distributions = {
+        "Betabinomial": st.betabinom,
+        "Binomial": st.binom,
+        "Poisson": st.poisson,
+        "Geometric": st.geom,
+        "Hypergeometric": st.hypergeom,
+        "Negative Binomial": st.nbinom,
+        "Negative Hypergeometric": st.nhypergeom,
+        "Zipfian": st.zipfian,
+        "Log-Series": st.logser,
+        "Laplacian": st.dlaplace,
+
+    }
+    if bound == True:
+        distributions = {
+            "Betabinomial": st.betabinom,
+            "Binomial": st.binom,
+            "Poisson": st.poisson,
+            "Geometric": st.geom,
+            "Hypergeometric": st.hypergeom,
+            "Negative Binomial": st.nbinom,
+        }
+
+    # Best holders
+    best_distributions = []
+
+    # Estimate distribution parameters from data
+    for name, distribution in distributions.items():
+
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                if name == "Betabinomial":
+                    bounds = (
+                        (0, len(np.unique(data))),
+                        (0, len(np.unique(data))),
+                        (0, len(np.unique(data))),
+                    )
+                elif name == "Hypergeometric" or name == "Negative Hypergeometric":
+                    bounds = (
+                        (0, len((data))),
+                        (0, len(np.unique(data))),
+                        (0, len((data))),
+                    )
+                elif (
+                    name == "Binomial"
+                    or name == "Negative Binomial"
+                    or name == "Zipfian"
+                    or name == "Zipf"
+                    or name == "Boltzmann"
+                    or name == "Planck"
+                ):
+                    bounds = ((0, len(data)), (0, len(data)))
+                elif (
+                    name == "Poisson"
+                    or name == "Geom"
+                    or name == "Log-Series"
+                    or name == "Laplacian"
+                    or name == "Yule-Simon"
+                ):
+                    bounds = ((0, max(data)), (0, max(data)))
+                elif name == "Fisher" or name == "Wallenius":
+                    bounds = (
+                        (0, len(data)),
+                        (0, len(data)),
+                        (0, len(data)),
+                        (0, len(data)),
+                    )
+                elif name == "Bernoulli":
+                    bounds = None
+                # fit dist to data
+                params = st.fit(distribution, data, bounds).params[:]
+
+                # Separate parts of parameters
+
+                # Calculate fitted PDF and error with fit in distribution
+
+                n = len(data)
+                k = len(params)
+
+                log_likelihood = distribution(*params).logpmf(data).sum()
+
+                if criterion == "BIC":
+                    criterion_value = -2 * log_likelihood + np.log(n) * k
+                elif criterion == "AIC":
+                    criterion_value = 2 * k - 2 * log_likelihood
+                elif criterion == "ML":
+                    criterion_value = log_likelihood
+
+                # identify if this distribution is better
+                best_distributions.append((distribution, params, criterion_value))
+
+        except Exception:
+            pass
+
+        bestdist = sorted(best_distributions, key=lambda x: x[2])[0]
+    return bestdist
+
+
+# %% best fit distribution
+def best_fit_distribution(data, criterion="BIC", dists = []):
+    """
+    Fits the best continuous distribution to data.
+
+    Arguments:
+        *data* : The data which has to be fit as a 1-d numpy array.
+
+        *criterion* : Either BIC, AIC or ML is used to choose the best distribution
+        
+        *dists* : Specify specific distributions if only specific distributions need to be tested, provided as a list.
+
+    Returns:
+     *bestdist* : the best distribution and its parameters.
+    """
+    # distributions
+    distributions = {
+        "Beta": st.beta,
+        "Birnbaum-Saunders": st.burr,
+        "Exponential": st.expon,
+        "Extreme value": st.genextreme,
+        "Gamma": st.gamma,
+        "Generalized extreme value": st.genextreme,
+        "Generalized Pareto": st.genpareto,
+        "Inverse Gaussian": st.invgauss,
+        "Logistic": st.logistic,
+        "Log-logistic": st.fisk,
+        "Lognormal": st.lognorm,
+        "Nakagami": st.nakagami,
+        "Normal": st.norm,
+        "Rayleigh": st.rayleigh,
+        "Rician": st.rice,
+        "t location-scale": st.t,
+        "Weibull": st.weibull_min,
+    }
+    
+    if len(dists)> 0:
+        keys_list = list(distributions.keys())
+    
+        keys_list2 = []
+        for i in dists:
+            keys_list2.append(keys_list[i])
+            
+        distributions = dict((k, distributions[k]) for k in keys_list2
+               if k in distributions)
+    
+   
+        
+    
+    
+    
+
+    # Best holders
+    best_distributions = []
+
+    # Estimate distribution parameters from data
+    for name, distribution in distributions.items():
+
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+
+                n = len(data)
+                k = len(params)
+
+                log_likelihood = (
+                    distribution(loc=loc, scale=scale, *arg).logpdf(data).sum()
+                )
+
+                if criterion == "BIC":
+                    criterion_value = -2 * log_likelihood + np.log(n) * k
+                elif criterion == "AIC":
+                    criterion_value = 2 * k - 2 * log_likelihood
+                elif criterion == "ML":
+                    criterion_value = log_likelihood
+
+                # identify if this distribution is better
+                best_distributions.append((distribution, params, criterion_value))
+
+        except Exception:
+            pass
+
+    return sorted(best_distributions, key=lambda x: x[2])[0]
+
+
+# %% pseudodata
+def pseudodata(data):
+    """
+    Compute the pseudo-observations for the given data (transforms data to standard uniform margins)
+
+    Arguments:
+        *data* : The data which has to be converted into pseudo data, provided as a numpy array where each column contains a separate variable (eg. x1,x2,...,xn)
+
+
+    Returns:
+     *u* : Pseudo data, provided as a numpy array where each column contains a separate variable (eg. u1,u2,...,un)
+    """
+
+    ranks = np.apply_along_axis(rankdata, axis=0, arr=data)
+    n = data.shape[0]
+    u = (ranks - 1) / (n - 1)
+
+    return u
+
+
+# %%
+def pseudodiscr(xcdf, xpmf):
+    """
+    Compute the pseudo-observations for the given variable that is discrete.
+
+    Arguments:
+        *xcdf* : The cumulative distribution function of the variable, calculated based on the best fit discrete distribution, provided as a 1-d numpy array.
+
+        *xpmf* : The probability mass function of the variable, calculated based on the best fit discrete distribution, provided as a 1-d numpy array.
+
+    Returns:
+     *ui* : Pseudo data of a given variable  provided as a 1-d numpy array.
+    """
+
+    # Reference: Mitskopoulos et al. 2022
+    u113 = xcdf - xpmf
+    ru = np.random.uniform(0, 1, len(xcdf))
+    ui = u113 + ru * (xcdf - u113)
+    return ui
---
+++ packages/VineCopulas/vinecopulas-2.0.2.tar.gz/vinecopulas-2.0.2/tests/test_vinecopula.py
@@ -0,0 +1,331 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 17:49:04 2024
+
+"""
+import warnings
+
+warnings.filterwarnings("ignore")
+from vinecopulas.vinecopula import *
+from vinecopulas.marginals import *
+import numpy as np
+import unittest
+import pandas as pd
+
+#%%
+class Testvinecopula(unittest.TestCase):
+    def setUp(self):
+        datapath = "https://raw.githubusercontent.com/VU-IVM/vinecopula/develop/doc/sample_data.csv"  # path to data
+        df = pd.read_csv(datapath)
+        self.U = pseudodata(np.array(df)[:, :-1])
+        nan = np.nan
+        self.M_R = np.array(
+            [
+                [0.0, 0.0, 0.0, 5.0, 5.0, 5.0],
+                [3.0, 2.0, 5.0, 4.0, 4.0, nan],
+                [2.0, 5.0, 4.0, 0.0, nan, nan],
+                [5.0, 4.0, 2.0, nan, nan, nan],
+                [4.0, 3.0, nan, nan, nan, nan],
+                [1.0, nan, nan, nan, nan, nan],
+            ]
+        )
+        self.P_R = np.array(
+            [
+                [
+                    5.667415060610504,
+                    4.809887154028018,
+                    4.742610409927173,
+                    5.329052634720821,
+                    6.365954831464865,
+                    nan,
+                ],
+                [
+                    list([-0.6512734168403267, 5.953996539895858]),
+                    1.155319323794073,
+                    0.1056255559540575,
+                    list([0.3900965721838106, 6.344879386256914]),
+                    nan,
+                    nan,
+                ],
+                [
+                    list([-0.40578880593225847, 5.521243696841605]),
+                    1.0500821290068711,
+                    0.08990469323285986,
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [
+                    list([0.2172541619061329, 18.453691725212337]),
+                    1.587083876681365,
+                    nan,
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [
+                    list([0.05131396326384129, 30.722615332724796]),
+                    nan,
+                    nan,
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [nan, nan, nan, nan, nan, nan],
+            ],
+            dtype=object,
+        )
+        self.C_R = np.array(
+            [
+                [4.0, 4.0, 4.0, 4.0, 4.0, nan],
+                [15.0, 3.0, 1.0, 15.0, nan, nan],
+                [15.0, 4.0, 9.0, nan, nan, nan],
+                [15.0, 10.0, nan, nan, nan, nan],
+                [15.0, nan, nan, nan, nan, nan],
+                [nan, nan, nan, nan, nan, nan],
+            ]
+        )
+        self.M_D = np.array(
+            [
+                [2.0, 0.0, 5.0, 4.0, 5.0, 5.0],
+                [0.0, 5.0, 4.0, 5.0, 4.0, nan],
+                [5.0, 4.0, 3.0, 3.0, nan, nan],
+                [4.0, 3.0, 0.0, nan, nan, nan],
+                [3.0, 2.0, nan, nan, nan, nan],
+                [1.0, nan, nan, nan, nan, nan],
+            ]
+        )
+        self.P_D = np.array(
+            [
+                [
+                    3.5133699681513915,
+                    4.742610409927173,
+                    5.329052634720821,
+                    4.093431936485096,
+                    6.365954831464865,
+                    nan,
+                ],
+                [
+                    list([0.7761434164482084, 6.701427960238227]),
+                    0.1056255559540575,
+                    list([0.3900965721838106, 6.344879386256914]),
+                    list([0.16733909434684788, 10.7269854361358]),
+                    nan,
+                    nan,
+                ],
+                [
+                    0.12978185414142449,
+                    0.08990469323285986,
+                    list([0.5353314452187353, 16.347989651517267]),
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [-0.8757255275146373, 1.1429781801124308, nan, nan, nan, nan],
+                [
+                    list([-0.6825574708843178, 5.342550524934684]),
+                    nan,
+                    nan,
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [nan, nan, nan, nan, nan, nan],
+            ],
+            dtype=object,
+        )
+        self.C_D = np.array(
+            [
+                [4.0, 4.0, 4.0, 4.0, 4.0, nan],
+                [15.0, 1.0, 15.0, 15.0, nan, nan],
+                [6.0, 9.0, 15.0, nan, nan, nan],
+                [10.0, 5.0, nan, nan, nan, nan],
+                [15.0, nan, nan, nan, nan, nan],
+                [nan, nan, nan, nan, nan, nan],
+            ]
+        )
+        self.M_C = np.array(
+            [
+                [0.0, 0.0, 0.0, 0.0, 5.0, 5.0],
+                [3.0, 3.0, 3.0, 5.0, 0.0, nan],
+                [5.0, 5.0, 5.0, 3.0, nan, nan],
+                [1.0, 4.0, 4.0, nan, nan, nan],
+                [4.0, 1.0, nan, nan, nan, nan],
+                [2.0, nan, nan, nan, nan, nan],
+            ]
+        )
+        self.P_C = np.array(
+            [
+                [
+                    4.742610409927173,
+                    5.667415060610504,
+                    5.186696320701982,
+                    4.809887154028018,
+                    5.329052634720821,
+                    nan,
+                ],
+                [
+                    1.155319323794073,
+                    list([-0.6512734168403267, 5.953996539895858]),
+                    1.6047944400112235,
+                    1.0501700414768074,
+                    nan,
+                    nan,
+                ],
+                [
+                    0.10074381405975691,
+                    list([0.1637157864473392, 18.148998646970544]),
+                    list([0.6321675900633315, 6.631581061709736]),
+                    nan,
+                    nan,
+                    nan,
+                ],
+                [list([-0.41682583386878064, 5.365613356884372]), 0.07785323645884598, nan, nan, nan, nan],
+                [-0.037650974919099515, nan, nan, nan, nan, nan],
+                [nan, nan, nan, nan, nan, nan],
+            ],
+            dtype=object,
+        )
+        self.C_C = np.array(
+            [
+                [4.0, 4.0, 4.0, 4.0, 4.0, nan],
+                [3.0, 15.0, 10.0, 4.0, nan, nan],
+                [1.0, 15.0, 15.0, nan, nan, nan],
+                [15.0, 6.0, nan, nan, nan, nan],
+                [9.0, nan, nan, nan, nan, nan],
+                [nan, nan, nan, nan, nan, nan],
+            ]
+        )
+
+    def test_vinecopR(self):
+        cops = list(range(1, 16))  # all copulas
+        M, P, C = fit_vinecop(self.U, cops, vine="R", printing=False)
+        assert np.isclose(self.M_R, M, rtol=0.05, equal_nan=True).all()
+        assert np.isclose(self.C_R, C, rtol=0.05, equal_nan=True).all()
+        assert self.P_R in P
+
+    def test_vinecopD(self):
+        cops = list(range(1, 16))  # all copulas
+        M, P, C = fit_vinecop(self.U, cops, vine="D", printing=False)
+        assert np.isclose(self.M_D, M, rtol=0.05, equal_nan=True).all()
+        assert np.isclose(self.C_D, C, rtol=0.05, equal_nan=True).all()
+        assert self.P_D in P
+
+    def test_vinecopC(self):
+        cops = list(range(1, 16))  # all copulas
+        M, P, C = fit_vinecop(self.U, cops, vine="C", printing=False)
+        assert np.isclose(self.M_C, M, rtol=0.05, equal_nan=True).all()
+        assert np.isclose(self.C_C, C, rtol=0.05, equal_nan=True).all()
+        assert self.P_C in P
+
+    def test_vinecopstructure(self):
+        cops = list(range(1, 16))  # all copulas
+        P, C = fit_vinecopstructure(self.U, cops, self.M_C)
+        assert np.isclose(self.C_C, C, rtol=0.05, equal_nan=True).all()
+        assert self.P_C in P
+
+    def test_samplecop(self):
+        np.random.seed(10)
+        ur_expected = np.array(
+            [
+                [
+                    0.27309758, 0.459142  , 0.30827003, 0.14359465, 0.23025509,
+                            0.22479665,
+                ],
+                [
+                    0.87293178, 0.78494858, 0.76770503, 0.93681427, 0.95934152,
+                     0.95339335,
+                ],
+                [
+                    0.34006522, 0.18519631, 0.43717586, 0.35082454, 0.33830778,
+                     0.29187607,
+                ],
+                [
+                   0.55197026, 0.60908594, 0.58327573, 0.63115738, 0.64226717,
+                    0.67413362,
+                ],
+                [
+                    0.62078409, 0.60270035, 0.65357772, 0.61645537, 0.63675281,
+                     0.60103895,
+                ],
+                [0.23607129, 0.28591365, 0.39315093, 0.20390582, 0.2238174 ,
+                 0.30070006],
+            ]
+        )
+        ur_result = sample_vinecop(self.M_R, self.P_R, self.C_R, 6)
+        assert np.isclose(ur_expected, ur_result, rtol=0.05, equal_nan=True).all()
+
+    def test_vincopconditionalsample(self):
+        np.random.seed(10)
+        ur_expected = np.array(
+            [
+                [0.92455016, 0.98003593, 0.92675129, 0.69882321, 0.9, 0.9],
+                [0.92623137, 0.94499478, 0.83619152, 0.86607092, 0.9, 0.9],
+                [0.9635065, 0.95650604, 0.9643929, 0.83547459, 0.9, 0.9],
+                [0.90597386, 0.66753704, 0.94219826, 0.88281989, 0.9, 0.9],
+                [0.91975774, 0.93327197, 0.96929214, 0.8292089, 0.9, 0.9],
+                [0.9142449, 0.94302835, 0.87124292, 0.82148805, 0.9, 0.9],
+            ]
+        )
+        ur_result = sample_vinecopconditional(self.M_R, self.P_R, self.C_R, 6, [0.9, 0.9])
+        assert np.isclose(ur_expected, ur_result, rtol=0.05, equal_nan=True).all()
+
+    def test_samplingorder(self):
+        so = samplingorder(self.M_R)
+        expected_last_so = [0.0, 1.0, 3.0, 2.0, 5.0, 4.0]
+        assert np.isclose(so[-1], expected_last_so, rtol=0.05, equal_nan=True).all()
+        assert len(so) == 32
+
+    def test_samplingmatrix(self):
+        sorder = [0.0, 1.0, 3.0, 2.0, 5.0, 4.0]
+        M, P, C = samplingmatrix(self.M_R, self.C_R, self.P_R, sorder)
+        so = list(np.diag(M[::-1])[::-1])
+        assert sorder == so
+
+    def test_conditionalvine1(self):
+        cops = list(range(1, 16))  # all copulas
+        vint = [2, 0]
+        MR, PR, CR = fit_conditionalvine(
+            self.U, vint, cops, vine="R", condition=1, printing=False
+        )
+        soR = list(np.diag(MR[::-1])[::-1])
+        MD, PD, CD = fit_conditionalvine(
+            self.U, vint, cops, vine="D", condition=1, printing=False
+        )
+        soD = list(np.diag(MD[::-1])[::-1])
+        MC, PC, CC = fit_conditionalvine(
+            self.U, vint, cops, vine="C", condition=1, printing=False
+        )
+        soC = list(np.diag(MC[::-1])[::-1])
+        assert 2 in soR[-2:]
+        assert 0 in soR[-2:]
+        assert 2 in soD[-2:]
+        assert 0 in soD[-2:]
+        assert 2 in soC[-2:]
+        assert 0 in soC[-2:]
+
+    def test_conditionalvine2(self):
+        cops = list(range(1, 16))  # all copulas
+        vint = [2, 0]
+        MR, PR, CR = fit_conditionalvine(
+            self.U, vint, cops, vine="R", condition=2, printing=False
+        )
+        soR = list(np.diag(MR[::-1])[::-1])
+        MD, PD, CD = fit_conditionalvine(
+            self.U, vint, cops, vine="D", condition=2, printing=False
+        )
+        soD = list(np.diag(MD[::-1])[::-1])
+        MC, PC, CC = fit_conditionalvine(
+            self.U, vint, cops, vine="C", condition=2, printing=False
+        )
+        soC = list(np.diag(MC[::-1])[::-1])
+        assert 2 in soR[:2]
+        assert 0 in soR[:2]
+        assert 2 in soD[:2]
+        assert 0 in soD[:2]
+        assert 2 in soC[:2]
+        assert 0 in soC[:2]
+
+#%%
+if __name__ == "__main__":
+    unittest.main()
