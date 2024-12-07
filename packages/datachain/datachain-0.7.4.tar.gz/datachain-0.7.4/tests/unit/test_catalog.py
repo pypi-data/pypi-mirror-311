53abc6094a9d1f16eb72c758e0e0768f3d269091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:56 +0000

    Add package datachain/0.7.4/datachain-0.7.4.tar.gz

---
+++ packages/datachain/datachain-0.7.4.tar.gz/datachain-0.7.4/tests/unit/test_catalog.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING
+
+from datachain.catalog import Catalog
+
+if TYPE_CHECKING:
+    from datachain.data_storage import AbstractWarehouse
+
+
+def test_catalog_warehouse_ready_callback(mocker, warehouse, id_generator, metastore):
+    spy = mocker.spy(warehouse, "is_ready")
+
+    def callback(warehouse: "AbstractWarehouse"):
+        assert warehouse.is_ready()
+
+    catalog = Catalog(
+        id_generator, metastore, warehouse, warehouse_ready_callback=callback
+    )
+
+    spy.assert_not_called()
+
+    _ = catalog.warehouse
+
+    spy.assert_called_once()
+    spy.reset_mock()
+
+    _ = catalog.warehouse
+
+    spy.assert_not_called()
