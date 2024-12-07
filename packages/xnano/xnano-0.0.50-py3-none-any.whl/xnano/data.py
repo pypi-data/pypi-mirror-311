f27706e74b83efbf094b030f8f6f57f647e8cc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:14 +0000

    Add package xnano/0.0.50/xnano-0.0.50-py3-none-any.whl

---
+++ packages/xnano/xnano-0.0.50-py3-none-any.whl/xnano/data.py
@@ -0,0 +1,9 @@
+# xnano . data
+
+__all__ = ["Database", "VectorStore"]
+
+# database and vector store handlers
+# located at
+# xnano.resources.data/
+
+from .resources.data._routing import Database, VectorStore
