c42b37a31b4f801894435df9f6504d3b44a59bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:18 +0000

    Add package smart-steganography-lib/0.1.1/smart_steganography_lib-0.1.1-py3-none-any.whl

---
+++ packages/smart-steganography-lib/smart_steganography_lib-0.1.1-py3-none-any.whl/smart_steganography_lib/__init__.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2024, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""Smart Steganography Lib - This is a steganography library
+ that allows you to hide and extract text messages in images."""
+from .steganography_tool import SteganographyTool
+__version__ = '0.1.1'
+__author__ = 'A.A Suvorov'
