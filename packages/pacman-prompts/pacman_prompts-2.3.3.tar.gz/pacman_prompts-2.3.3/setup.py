7484430b7a5088ad7a85cf4da4290bbe4a896ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:03 +0000

    Add package pacman-prompts/2.3.3/pacman_prompts-2.3.3.tar.gz

---
+++ packages/pacman-prompts/pacman_prompts-2.3.3.tar.gz/pacman_prompts-2.3.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'pacman-prompts',
+  packages = find_packages(exclude=['examples']),
+  version = '2.3.3',
+  license='MIT',
+  description = 'Prompts As Code, man',
+  long_description_content_type = 'text/markdown',
+  author = 'Anish Thite',
+  author_email = 'anishthite@gmail.com',
+  url = 'https://github.com/anishthite/pacman',
+  keywords = [
+    'artificial intelligence',
+    'prompt engineering',
+    'prompt management'
+  ],
+  install_requires=[
+      'openai',
+      'groq',
+      'anthropic',
+      'instructor',
+      'pyyaml',
+      'posthog',
+      'pydantic'
+  ],
+  setup_requires=[
+  ],
+  tests_require=[
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
