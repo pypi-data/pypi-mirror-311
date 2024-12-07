dec449d16b2a37f5d16dd579dc9e34f561011719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:20 +0000

    Add package simexp/0.1.5/simexp-0.1.5-py3-none-any.whl

---
+++ packages/simexp/simexp-0.1.5-py3-none-any.whl/simexp/simex.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from datetime import datetime
+from simexp.simfetcher import fetch_content
+from processor import process_content
+from archiver import save_as_markdown
+import yaml
+from imp_clip import update_sources_from_clipboard, is_clipboard_content_valid  # Import the function
+
+def main():
+    # Update sources from clipboard
+    update_sources_from_clipboard()
+
+    # Load configuration from YAML file
+    with open('config.yaml', 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+    # Check if clipboard content is valid
+    if not is_clipboard_content_valid():
+        print("Invalid clipboard content. Proceeding with existing websites from configuration.")
+        sources = config['SOURCES']
+    else:
+        sources = config['CLIPBOARD_SOURCES']
+
+    base_path = config['BASE_PATH']
+
+    # Create a folder for the current date
+    current_date = datetime.now().strftime('%Y%m%d')
+    daily_folder = os.path.join(base_path, current_date)
+    os.makedirs(daily_folder, exist_ok=True)
+
+    # Fetch, process, and save content for each source
+    for source in sources:
+        url = source['url']
+        filename = source['filename']
+        raw_content = fetch_content(url)
+        title, cleaned_content = process_content(raw_content)
+        save_as_markdown(title, cleaned_content, filename)
+
+if __name__ == "__main__":
+    main()
