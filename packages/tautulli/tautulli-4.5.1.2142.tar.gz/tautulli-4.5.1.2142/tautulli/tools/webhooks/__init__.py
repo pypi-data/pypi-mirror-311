1e88488dfe107d46d2924cfb5a5890fe241f2ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:39 +0000

    Add package tautulli/4.5.1.2142/tautulli-4.5.1.2142.tar.gz

---
+++ packages/tautulli/tautulli-4.5.1.2142.tar.gz/tautulli-4.5.1.2142/tautulli/tools/webhooks/__init__.py
@@ -0,0 +1,11 @@
+from tautulli.tools.webhooks.base import (
+    TautulliWebhookTrigger,
+    _TautulliWebhook,
+)
+from tautulli.tools.webhooks.discord import (
+    DiscordWebhook,
+    _DiscordWebhookData,
+    DiscordWebhookAttachment,
+    PlaybackStateChangeDiscordWebhookData,
+    RecentlyAddedDiscordWebhookData,
+)
