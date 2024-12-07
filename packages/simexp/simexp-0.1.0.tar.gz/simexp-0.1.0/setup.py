fae47e1977025fa42ffc9861db8c44e30bc07165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:12 +0000

    Add package simexp/0.1.0/simexp-0.1.0.tar.gz

---
+++ packages/simexp/simexp-0.1.0.tar.gz/simexp-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simexp',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pyperclip',
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'simexp=simex:main',
+        ],
+    },
+    author='gerico1007',
+    author_email='gerico@jgwill.com',
+    description='A web content extractor and archiver for simplenote',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gerico1007/simexp',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
