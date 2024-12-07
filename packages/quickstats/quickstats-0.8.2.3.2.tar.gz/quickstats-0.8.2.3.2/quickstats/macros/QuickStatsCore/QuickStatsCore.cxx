4346a736b52f738ad98b491d9a0ff7c93cdb2b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:22 +0000

    Add package quickstats/0.8.2.3.2/quickstats-0.8.2.3.2.tar.gz

---
+++ packages/quickstats/quickstats-0.8.2.3.2.tar.gz/quickstats-0.8.2.3.2/quickstats/macros/QuickStatsCore/QuickStatsCore.cxx
@@ -0,0 +1,39 @@
+#ifndef __QuickStatsCore_CXX__
+#define __QuickStatsCore_CXX__
+
+#include <map>
+#include <string>
+
+#include "RooDataSet.h"
+#include "RooAbsPdf.h"
+
+#include "RooFitExt.cxx"
+#include "QStringUtils.cxx"
+
+#ifdef __CINT__
+
+#pragma link off all functions;
+#pragma link off all globals;
+#pragma link off all classes;
+
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+
+#pragma link C++ namespace RooFitExt;
+
+#pragma link C++ class std::map<std::string, RooDataSet*>+;
+//#pragma link C++ class std::map<std::string, RooDataSet*>::*;
+#pragma link C++ class std::pair<std::map<string,RooDataSet*>::iterator, bool>+;
+//#pragma link C++ class std::pair<std::map<string,RooDataSet*>::iterator, bool>::*+;
+#pragma link C++ class std::pair<std::string, RooDataSet*>+;
+//#pragma link C++ class std::pair<std::string, RooDataSet*>::*+;
+#pragma link C++ class std::map<std::string, RooAbsPdf*>+;
+//#pragma link C++ class std::map<std::string, RooAbsPdf*>::*;
+#pragma link C++ class std::pair<std::map<string,RooAbsPdf*>::iterator, bool>+;
+//#pragma link C++ class std::pair<std::map<string,RooAbsPdf*>::iterator, bool>::*+;
+#pragma link C++ class std::pair<std::string, RooAbsPdf*>+;
+//#pragma link C++ class std::pair<std::string, RooAbsPdf*>::*+;
+
+#endif
+
+#endif
