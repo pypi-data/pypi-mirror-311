2b0ca93911037543faaee5e79fcbbade03d9601b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:34 +0000

    Add package componentsDjangoType/2.0.19/componentsdjangotype-2.0.19.tar.gz

---
+++ packages/componentsDjangoType/componentsdjangotype-2.0.19.tar.gz/componentsdjangotype-2.0.19/services/home/views_page.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from Home.services.authentication import Authentication
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    return Authentication.get_signup(request)
+
+def signout(request):
+    return Authentication.get_signout(request)
+
+def signing(request):
+    return Authentication.get_signing(request)
+
+@login_required
+def logged(request):
+    return Authentication.get_logged(request)
+
+def custom_dispatch(request, *args, **kwargs):
+    return Authentication.dispatch(request, *args, **kwargs)
