a237b23d65cdcfafce08a06fa6581902cc265c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:46 +0000

    Add package cprint-jianjun/1.7.0/cprint_jianjun-1.7.0.tar.gz

---
+++ packages/cprint-jianjun/cprint_jianjun-1.7.0.tar.gz/cprint_jianjun-1.7.0/cprint/__init__.py
@@ -0,0 +1,15 @@
+import cprint
+import cprint.main
+from cprint.tools import demo as _demo
+from cprint.tools import demoid as _demoid
+from cprint.tools import getshow_config as _showconfig
+
+__version__ = "1.7.0"
+
+cprint.custom_style = {}
+
+_cpshow = lambda : _showconfig(2)
+_cpshoww = lambda : _showconfig(3)
+
+config = cprint = main = tools = None
+del config, cprint, main, tools
